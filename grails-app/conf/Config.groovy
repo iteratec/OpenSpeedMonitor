8a45b1a795dae7809afefd2b0d7a4e90a9fdf227
Nils Kuhn <nils.kuhn@iteratec.de>
2014-12-01 13:57:00 +0100

    changed external configuration file determination

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -24,13 +24,14 @@ import de.iteratec.osm.report.chart.ChartingLibrary
  * 	config files can be ConfigSlurper scripts, Java properties files, or classes 
  * 	in the classpath in ConfigSlurper format
  */
-grails.config.locations = [ "classpath:${appName}-config.properties",
-                             "classpath:${appName}-config.groovy",
-                             "file:${userHome}/.grails/${appName}-config.properties",
-                             "file:${userHome}/.grails/${appName}-config.groovy"]
-
- if (System.properties["${appName}.config.location"]) {
-    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
+if (System.properties["${appName}.config.location"]) {
+    grails.config.locations = ["file:" + System.properties["${appName}.config.location"]]
+ } else {
+	 grails.config.locations = [
+		 "classpath:${appName}-config.properties",
+		 "classpath:${appName}-config.groovy",
+		 "file:${userHome}/.grails/${appName}-config.properties",
+		 "file:${userHome}/.grails/${appName}-config.groovy"]
  }
 
 // config for all environments //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
