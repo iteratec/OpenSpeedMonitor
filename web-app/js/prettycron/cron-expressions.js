305474c93ea1aff978c32ff2a3213c3fde16fc2d
Rainer Krauß <rainer.krauss@iteratec.de>
2015-04-02 11:35:52 +0200

    IT-534

--- web-app/js/prettycron/cron-expressions.js
+++ web-app/js/prettycron/cron-expressions.js
@@ -27,7 +27,10 @@ function updatePrettyCrons() {
 	$.each($('abbr.cronExpression'), function(index, elem) {
 		var cronExpression = elem.innerHTML;
     $(elem).attr('title', getPrettyCron(cronExpression));
-    $(elem).html(cronExpression.substr(cronExpression.indexOf(' ') + 1));
+    if(!$(elem).attr('data-seconds-cutted') ) {
+      $(elem).attr('data-seconds-cutted', 'true');
+      $(elem).html(cronExpression.substr(cronExpression.indexOf(' ') + 1));
+    }
 	});
 }
 
