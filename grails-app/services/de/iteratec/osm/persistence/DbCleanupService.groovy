e380b568aa207133ddaf9582503f56644ee9aa08
Benjamin Wollmer <benjamin.wollmer@iteratec.de>
2015-04-14 11:20:59 +0200

    temporarily disabled batchactivity update for nightly cleanup

--- grails-app/services/de/iteratec/osm/persistence/DbCleanupService.groovy
+++ grails-app/services/de/iteratec/osm/persistence/DbCleanupService.groovy
@@ -122,7 +122,7 @@ class DbCleanupService {
             //batch size -> hibernate doc recommends 10..50
             int batchSize = 50
             0.step(count, batchSize) { int offset ->
-                batchActivityService.updateStatus(batchActivity, ['progress': batchActivityService.calculateProgress(count, offset)])
+//                batchActivityService.updateStatus(batchActivity, ['progress': batchActivityService.calculateProgress(count, offset)])
                 JobResult.withNewTransaction {
                     dc.list(max: batchSize).each { JobResult jobResult ->
                         try {
@@ -181,7 +181,7 @@ class DbCleanupService {
 
             //First clean MeasuredValueUpdateEvents
             0.step(measuredValueUpdateEventsCount, batchSize){ int offset ->
-                batchActivityService.updateStatus(batchActivity, ['progress': batchActivityService.calculateProgress(globalCount, offset), 'stage': 'delete MeasuredValueUpdateEvents'])
+//                batchActivityService.updateStatus(batchActivity, ['progress': batchActivityService.calculateProgress(globalCount, offset), 'stage': 'delete MeasuredValueUpdateEvents'])
                 MeasuredValueUpdateEvent.withNewTransaction {
                     measuredValueUpdateEventDetachedCriteria.list(max: batchSize).each{ MeasuredValueUpdateEvent measuredValueUpdateEvent ->
                         try {
@@ -197,7 +197,7 @@ class DbCleanupService {
 
             //After then clean MeasuredValues
             0.step(measuredValueCount, batchSize) { int offset ->
-                batchActivityService.updateStatus(batchActivity, ['progress': batchActivityService.calculateProgress(measuredValueCount, offset+measuredValueUpdateEventsCount), 'stage': 'delete MeasuredValues'])
+//                batchActivityService.updateStatus(batchActivity, ['progress': batchActivityService.calculateProgress(measuredValueCount, offset+measuredValueUpdateEventsCount), 'stage': 'delete MeasuredValues'])
                 MeasuredValue.withNewTransaction {
                     measuredValueDetachedCriteria.list(max: batchSize).each { MeasuredValue measuredValue ->
                         try {
