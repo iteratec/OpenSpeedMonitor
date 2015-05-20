cf8a5f27121c1895b127a4e0c55d6eda1918b205
Rainer Krauß <rainer.krauss@iteratec.de>
2015-05-20 14:09:51 +0200

    ADDED spinner to RICKSHAW graph export, FIXED X-axis label generation

--- web-app/js/rickshaw/rickshawChartCreation.js
+++ web-app/js/rickshaw/rickshawChartCreation.js
@@ -446,10 +446,9 @@ function XAxis(args) {
           return dayName + "_nl_" + dateLabel;
         }
       }
-//    aggregate daily (if applicable) of if aggregate is weekly and tickValues empty
+//    aggregate daily (if applicable) or if aggregate is weekly and tickValues empty
       if ((resultsSelectedInterval[1] == 1440) || ((resultsSelectedInterval[1] == 10080) && (tickValues.length == 0))) {// daily
         tickValues = self.getDaysInRange(minDate, maxDate);
-        tickValues = self.getHoursInRange(minDate, maxDate);
         format = function(n) {
           var date = new Date(n * 1000);
           var time = self.getTimeString(date);
@@ -457,7 +456,7 @@ function XAxis(args) {
           return time + "_nl_" + dateLabel;
         }
       }
-//    aggregate hourly (if applicable) of if aggregate is daily/weekly and tickValues empty
+//    aggregate hourly (if applicable) or if aggregate is daily/weekly and tickValues empty
       if ((resultsSelectedInterval[1] == 60) || (tickValues.length == 0)) {
         tickValues = self.getHoursInRange(minDate, maxDate);
         format = function(n) {
@@ -1143,6 +1142,27 @@ function ChartExporter(args) {
   
   this.initialize = function(args) {
     d3.select("#dia-save-chart-as-png").on("click", function(){
+      
+      var opts = {
+          lines: 15, // The number of lines to draw
+          length: 20, // The length of each line
+          width: 10, // The line thickness
+          radius: 30, // The radius of the inner circle
+          corners: 1, // Corner roundness (0..1)
+          rotate: 0, // The rotation offset
+          direction: 1, // 1: clockwise, -1: counterclockwise
+          color: '#000', // #rgb or #rrggbb or array of colors
+          speed: 1, // Rounds per second
+          trail: 60, // Afterglow percentage
+          shadow: true, // Whether to render a shadow
+          hwaccel: false, // Whether to use hardware acceleration
+          className: 'spinner', // The CSS class to assign to the spinner
+          zIndex: 2e9, // The z-index (defaults to 2000000000)
+          top: 'auto', // Top position relative to parent in px
+          left: '50%' // Left position relative to parent in px
+      };
+
+      var spinner = new Spinner(opts).spin(document.getElementsByClassName('graph')[0]);
 
       deferrerCollection = new Array();
       
@@ -1293,8 +1313,11 @@ function ChartExporter(args) {
             deferrerCollection.push($.Deferred());
             self.resizeGraphTo(previousWidth, previousHeight, deferrerCollection[deferrerCollection.length - 1]);
           }
+          spinner.stop();
         } 
-        catch(err) {} // handle IE        
+        catch(err) {
+          spinner.stop();
+        } // handle IE        
       });
     });
   }  
