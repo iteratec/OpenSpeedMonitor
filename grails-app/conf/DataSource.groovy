9fd0d59baa257b6427596b2e6fcaa4dba5339e11
Rainer Krauß <rainer.krauss@iteratec.de>
2015-05-26 16:55:48 +0200

    REVERTING changes

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,28 +1,28 @@
-/*
+/* 
 * OpenSpeedMonitor (OSM)
 * Copyright 2014 iteratec GmbH
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); 
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-*
+* 
 * 	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
 * limitations under the License.
 */
 
 /**
  * PLEASE NEVER COMMIT CHANGED VERSIONS OF THIS FILE.
- *
+ * 
  * The datasources defined in this file are default datasources to be used only when running the app out of the box via run-app.
- * Datasources different from default can and should be defined in separate external config files.
+ * Datasources different from default can and should be defined in separate external config files. 
  * Config param grails.config.locations in Config.groovy contains a list of possible locations for such additional config files.
  * In addition you can add an own location for an external config file via system property "osm_config_location"
- *
+ * 
  * @author nkuhn
  * @see OpenSpeedMonitor-config.groovy.sample
  * @see Config.groovy
