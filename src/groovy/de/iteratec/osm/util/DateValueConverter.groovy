a51168f405cc78c90a08a1275c821df353cff17c
Rainer Krauß <rainer.krauss@iteratec.de>
2015-05-27 16:54:57 +0200

    MODIFIED conversion to not fail on null

--- src/groovy/de/iteratec/osm/util/DateValueConverter.groovy
+++ src/groovy/de/iteratec/osm/util/DateValueConverter.groovy
@@ -18,10 +18,14 @@ class DateValueConverter implements ValueConverter{
         value instanceof String
     }
     def convert(value) {
-        String strDate = SIMPLE_DATE_FORMAT.format(value);
-        Date parsedDate = SIMPLE_DATE_FORMAT.parse(strDate);
+        if (value) {
+            String strDate = SIMPLE_DATE_FORMAT.format(value);
+            Date parsedDate = SIMPLE_DATE_FORMAT.parse(strDate);
 //        return SIMPLE_DATE_FORMAT.parse(value)
-        return parsedDate
+            return parsedDate
+        } else {
+            return null;
+        }
     }
     Class<?> getTargetType() {
         Date.class
