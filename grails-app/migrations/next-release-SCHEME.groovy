048965a809f63fff16fb9b409aa0822aeafcc35f
BirgerK <birger.kamp@iteratec.de>
2016-01-12 10:44:25 +0100

    move single db-migration to next-release-file

--- grails-app/migrations/next-release-SCHEME.groovy
+++ grails-app/migrations/next-release-SCHEME.groovy
@@ -3,4 +3,13 @@ databaseChangeLog = {
     changeSet(author: "marcus (generated)", id: "1452076094323-1") {
         dropIndex(indexName: "full_hour", tableName: "hour_of_day")
     }
+
+// nightlyDatabaseCleanups can be de-/activated by REST-Call
+    changeSet(author: "bka (generated)", id: "1452546683118-1") {
+        addColumn(tableName: "api_key") {
+            column(name: "allowed_for_nightly_database_cleanup_activation", type: "bit") {
+                constraints(nullable: "false")
+            }
+        }
+    }
 }
