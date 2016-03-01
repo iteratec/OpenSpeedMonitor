92ee061097c7384e329f797393caf723d82b94d6
BirgerK <birger.kamp@iteratec.de>
2016-03-01 17:38:28 +0100

    [ci] add local karma-dependencies to repo

--- test/unit/javascript/JavaScriptUnitTestKarmaSuite.groovy
+++ test/unit/javascript/JavaScriptUnitTestKarmaSuite.groovy
@@ -5,5 +5,6 @@ import de.is24.util.karmatestrunner.junit.KarmaTestSuiteRunner;
 
 @RunWith(KarmaTestSuiteRunner.class)
 @KarmaTestSuiteRunner.KarmaConfigPath("test/unit/javascript/karma.conf.js")
+@KarmaTestSuiteRunner.KarmaProcessName("./node_modules/karma/bin/karma")
 public class JavaScriptUnitTestKarmaSuite {
 }
