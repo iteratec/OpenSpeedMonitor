cc2ce86daed0cae578f9b97726f3436282a0ab35
BirgerK <birger.kamp@iteratec.de>
2016-03-07 18:03:29 +0100

    [ci] fix username

--- scripts/ci/deploy/blue-green_switch.sh
+++ scripts/ci/deploy/blue-green_switch.sh
@@ -14,7 +14,7 @@ echo "PORT_TO_UNDEPLOY=$bamboo_tomcat_PORT_TO_UNDEPLOY"
 
 
 # run blue/green switch on target machine
-ssh -p $bamboo_tomcat_SSH_PORT_TO_HOST_TO_DEPLOY_TO -o "StrictHostKeyChecking no" $bamboo_tomcat_TOMCAT_ADMIN_USERNAME@$bamboo_tomcat_HOST_TO_DEPLOY_TO \
+ssh -p $bamboo_tomcat_SSH_PORT_TO_HOST_TO_DEPLOY_TO -o "StrictHostKeyChecking no" $bamboo_tomcat_JENKINS_USER_NAME@$bamboo_tomcat_HOST_TO_DEPLOY_TO \
 	"sudo /opt/osm_blue_green_switch.sh $PORT_TO_SWITCH_FROM $PORT_TO_SWITCH_TO $bamboo_tomcat_OSM_REVERSE_PROXY_URL $bamboo_tomcat_OSM_REVERSE_PROXY_IP OpenSpeedMonitor 2>&1" \
     > .\combined_output_on_local_host.txt
 cat .\combined_output_on_local_host.txt
