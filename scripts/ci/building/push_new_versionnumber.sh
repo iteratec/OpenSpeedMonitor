d09cc97df3f53ace552fd6b6d7e99fa93abead77
BirgerK <birger.kamp@iteratec.de>
2016-03-09 11:57:31 +0100

    [ci] add username to git-push

--- scripts/ci/building/push_new_versionnumber.sh
+++ scripts/ci/building/push_new_versionnumber.sh
@@ -5,6 +5,8 @@ if [ -z $bamboo_ci_app_version ]; then
   exit 1
 else
   echo "Found version-number: ${bamboo_ci_app_version}"
+  git config user.mail "jenkins@seu.hh.iteratec.de"
+  git config user.name "jenkins@seu"
   git add --all
   git commit -m "[${bamboo_ci_app_version}] version update"
   git tag $bamboo_ci_app_version
