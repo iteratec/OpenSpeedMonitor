584c763030226cbe903e9e9f4584f633efebac63
Nils Kuhn <nils.kuhn@iteratec.de>
2016-03-15 17:02:25 +0100

    [ci] updated push version number script

--- scripts/ci/building/push_new_versionnumber.sh
+++ scripts/ci/building/push_new_versionnumber.sh
@@ -9,15 +9,21 @@ else
 
   git config user.email "jenkins@seu.hh.iteratec.de"
   git config user.name "jenkins@seu"
+  git config push.default simple
 
-  git add --all
+  git add --all :/
   git commit -m "[${bamboo_ci_app_version}] version update"
 
   git status
+  git pull --rebase
+  git push
 
   git tag $bamboo_ci_app_version
-  git remote add github $bamboo_planRepository_repositoryUrl
-  GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull --rebase github $bamboo_planRepository_branch
-  git config push.default simple
-  GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push github $bamboo_planRepository_branch
+  git pull --rebase
+  git push --tags
+
+
+  # git remote add github $bamboo_planRepository_repositoryUrl
+  # GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull --rebase github $bamboo_planRepository_branch
+  # GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push github $bamboo_planRepository_branch
 fi
