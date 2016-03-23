018794c07eb62edc5972a8912efe141a163a3ce7
BirgerK <birger.kamp@iteratec.de>
2016-03-23 14:32:24 +0100

    [ci] fix push-version script

--- scripts/ci/building/push_new_versionnumber.sh
+++ scripts/ci/building/push_new_versionnumber.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
-
 set -e
 
 if [ -z $bamboo_ci_app_version ]; then
   echo "No version-number found: ${bamboo_ci_app_version}"
   exit 1
 else
-  GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
   echo "Found version-number: ${bamboo_ci_app_version}"
+
+  remote=origin
+
+  git remote remove $remote
+
+  remote_url=https://$bamboo_git_USER_NAME:$bamboo_git_PASSWORD@github.com/IteraSpeed/OpenSpeedMonitor.git
+  echo "set remote $remote to '$remote_url'"
+  git remote add -f $remote $remote_url
 
   git config user.email "jenkins@seu.hh.iteratec.de"
   git config user.name "jenkins@seu"
-  git config push.default simple
 
-  git add --all :/
-  git commit -m "[${bamboo_ci_app_version}] version update"
+  git commit -am "[${bamboo_ci_app_version}] version update"
+  git tag "${bamboo_ci_app_version}"
 
-  git status
-  git pull --rebase
-  git push
+  git pull --rebase $remote $bamboo_planRepository_branch
+  git push --tags $remote HEAD:refs/heads/$bamboo_repository_git_branch
 
-  git tag $bamboo_ci_app_version
-  git pull --rebase
-  git push --tags
-
-
-  # git remote add github $bamboo_planRepository_repositoryUrl
-  # GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull --rebase github $bamboo_planRepository_branch
-  # GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push github $bamboo_planRepository_branch
 fi
