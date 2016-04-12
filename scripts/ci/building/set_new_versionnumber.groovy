118616d418301b2fa0e14c73603e0b44130ad075
BirgerK <birger.kamp@iteratec.de>
2016-04-12 17:18:10 +0200

    [IT-1027] fix build-script at reading and writing version-number

--- scripts/ci/building/set_new_versionnumber.groovy
+++ scripts/ci/building/set_new_versionnumber.groovy
@@ -2,11 +2,9 @@
 // set new osm version in application.properties
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-Properties props = new Properties()
-File propsFile = new File('./application.properties')
-props.load(propsFile.newDataInputStream())
+File buildFile = new File('./build.gradle')
 
-String appVersion = props.getProperty('app.version')
+String appVersion = getVersionFromFile(buildFile)
 println "AppVersion from application.properties is ${appVersion}"
 
 List tokenizedVersion = appVersion.tokenize('.')
@@ -14,14 +12,14 @@ Integer major = Integer.valueOf(tokenizedVersion[0])
 Integer minor = Integer.valueOf(tokenizedVersion[1])
 
 String lastVersionPart = tokenizedVersion[2]
-List tokenizedLastVersionPart = tokenizedVersion[2].tokenize('-')
+List tokenizedLastVersionPart = lastVersionPart.tokenize('-')
 Integer patch = Integer.valueOf(tokenizedLastVersionPart[0])
 
 String oldVersion = "${major}.${minor}.${patch}"
 println "OldVersion is ${oldVersion}"
 
 def bamboo_jira_version = System.getenv("bamboo_jira_version") ?: ""
-String newVersion = ""
+String newVersion
 if("${bamboo_jira_version}") {
   println 'Given release-version from jira: ' + bamboo_jira_version + '...'
   newVersion = "${bamboo_jira_version}"
@@ -34,8 +32,42 @@ println "... and adding build-number ${bamboo_build_number} to version-number"
 
 newVersion += "-build${bamboo_build_number}"
 
-props.setProperty('app.version', newVersion)
-props = props.sort()
-props.store(propsFile.newWriter(), null)
+writeVersionToFile(buildFile,newVersion)
 
 println "Updated version from ${oldVersion} to ${newVersion}"
+
+
+def String getVersionFromFile(File file) {
+  String result
+  String line
+  int n = file.length()
+  file.withReader { r ->
+    while( n-- > 0) {
+      line = r.readLine()
+      if(line.startsWith("version ")) {
+        break
+      }
+    }
+
+  }
+  result = line.replace("version ","")
+  result = result.replaceAll('\"',"")
+
+  if(result == null || result.empty) {
+    println "Found String is: " + result
+    println "Could not find version-number in build.gradle"
+    System.exit(1)
+  }
+
+  return result
+}
+
+def String writeVersionToFile(File file, String versionNumber) {
+  String writeToFile = 'version "' + versionNumber + '"\n'
+  def text = file.text
+  def pattern = ~/version "\d\.\d\.\d\-build(\d+)"\n/
+
+  file.withWriter { w ->
+    w << text.replaceAll(pattern, writeToFile)
+  }
+}
