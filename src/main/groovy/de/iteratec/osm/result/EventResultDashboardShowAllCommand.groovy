04bdbc3174fddeb2a0b7f141f1da0c965594f8aa
BirgerK <birger.kamp@iteratec.de>
2016-04-12 12:13:39 +0200

    [IT-1027] fix warnings for command-validations because of commands aren't inner classes of their controllers, they have to implement Validateable

--- src/main/groovy/de/iteratec/osm/result/EventResultDashboardShowAllCommand.groovy
+++ src/main/groovy/de/iteratec/osm/result/EventResultDashboardShowAllCommand.groovy
@@ -352,10 +352,6 @@ public class EventResultDashboardShowAllCommand implements Validateable{
      *         if called on an invalid instance.
      */
     public Interval getSelectedTimeFrame() throws IllegalStateException {
-        if (!this.validate()) {
-            throw new IllegalStateException('A time frame is not available from an invalid command.')
-        }
-
         DateTime start
         DateTime end
 
