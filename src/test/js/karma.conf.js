e59431ff24ea6989f2a869fbc4b7464f2bf86e2a
BirgerK <birger.kamp@iteratec.de>
2016-04-18 15:58:08 +0200

    [IT-1034] fix karma/jasmine-tests for running with grails 3.x

--- src/test/js/karma.conf.js
+++ src/test/js/karma.conf.js
@@ -2,6 +2,10 @@
 // Generated on Mon Dec 14 2015 12:27:49 GMT+0100 (CET)
 
 module.exports = function (config) {
+
+    var ASSET_PATH = 'grails-app/assets';
+    var TEST_PATH = 'src/test/js';
+
     config.set({
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -15,19 +19,16 @@ module.exports = function (config) {
         // list of files / patterns to load in the browser
         files: [
             { pattern: 'target/plugins/jquery-*/web-app/js/jquery/jquery-*.js', watched: false },
-            { pattern: 'grails-app/assets/javascripts/application.js', watched: true },
-            { pattern: 'grails-app/assets/javascripts/csi/defaultMappingCsvValidator.js', watched: true },
-            { pattern: 'test/unit/javascript/specs/**/*.js', watched: true },
+            { pattern: ASSET_PATH + '/javascripts/application.js', watched: true },
+            { pattern: ASSET_PATH + '/javascripts/csi/defaultMappingCsvValidator.js', watched: true },
+            { pattern: TEST_PATH + '/specs/**/*.js', watched: true }
         ],
 
         // test results reporter to use
-        // possible values: 'dots', 'progress'
+        // possible values: 'dots', 'progress', 'remote'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['remote'],
-        remoteReporter: {
-            host: 'localhost',
-            port: '9889'
-        },
+        reporters: ['progress'],
+        port: 9876,
 
 
         // enable / disable colors in the output (reporters and logs)
@@ -36,7 +37,7 @@ module.exports = function (config) {
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+        logLevel: config.LOG_ERROR,
 
 
         // enable / disable watching file and executing tests whenever any file changes
