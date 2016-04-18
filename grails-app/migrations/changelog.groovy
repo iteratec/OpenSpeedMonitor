96dad30d6a6ff7fa2b1457312ebd16393877d89c
BirgerK <birger.kamp@iteratec.de>
2016-04-18 13:51:46 +0200

    [IT-1037] fix double primaryKeyName in Migrations MySQL doesn't even seems to be interested into this names. Looks like they're only for liquibase or other DBMS.

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -22,4 +22,5 @@ databaseChangeLog = {
 	include file: '2016-03-08-SCHEME-DetailAnalysisStart.groovy'
     include file: '2016-03-23-SCHEME-v34.groovy'
     include file: '2016-03-30-SCHEME-v348.groovy'
+    include file: '2016-04-18-SCHEME-v35.groovy'
 }
