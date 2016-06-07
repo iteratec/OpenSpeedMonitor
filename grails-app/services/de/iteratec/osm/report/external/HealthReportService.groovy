3390a67f35ca3a8316622676489e800b5e1a911e
Marko Schnecke <marko.schnecke@iteratec.de>
2016-06-07 11:12:55 +0200

    [IT-1029] adding extra metrics

--- grails-app/services/de/iteratec/osm/report/external/HealthReportService.groovy
+++ grails-app/services/de/iteratec/osm/report/external/HealthReportService.groovy
@@ -1,6 +1,7 @@
 package de.iteratec.osm.report.external
 
 import com.codahale.metrics.Gauge
+import com.codahale.metrics.MetricFilter
 import com.codahale.metrics.MetricRegistry
 import com.codahale.metrics.graphite.Graphite
 import com.codahale.metrics.graphite.GraphiteReporter
@@ -8,6 +9,7 @@ import com.codahale.metrics.jvm.GarbageCollectorMetricSet
 import com.codahale.metrics.jvm.MemoryUsageGaugeSet
 import com.codahale.metrics.jvm.ThreadStatesGaugeSet
 import org.springframework.boot.actuate.endpoint.SystemPublicMetrics
+import org.springframework.boot.actuate.metrics.Metric
 
 import java.lang.management.ManagementFactory
 import java.lang.management.OperatingSystemMXBean
@@ -43,19 +45,34 @@ class HealthReportService {
         metricRegistry.register(graphiteServer.garbageCollectorPrefix, new GarbageCollectorMetricSet())
         metricRegistry.register(graphiteServer.memoryReportPrefix, new MemoryUsageGaugeSet())
         metricRegistry.register(graphiteServer.threadStatesReportPrefix, new ThreadStatesGaugeSet())
-        metricRegistry.register(graphiteServer.garbageCollectorPrefix, new Gauge() {
+        metricRegistry.register(graphiteServer.processCpuLoadPrefix, new Gauge() {
             @Override
             Object getValue() {
                 OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
                 bean.getProcessCpuTime()
             }
         })
-//        systemPublicMetrics.metrics().each{m ->
-//                metricRegistry.register(m.getName(), m)
-//        }
+        systemPublicMetrics.metrics().each{  m->
+
+            metricRegistry.register("systen."+m.getName(), new Gauge() {
+
+                @Override
+                Object getValue() {
+                    long result
+                    systemPublicMetrics.metrics().each{
+                        if (it.getName() == m.getName()) result = it.getValue().longValue()
+                    }
+                    return result
+                }
+            })
+        }
         final Graphite graphite = new Graphite(new InetSocketAddress(graphiteServer.getServerInetAddress(),graphiteServer.port))
         GraphiteReporter reporter = GraphiteReporter
-                .forRegistry(metricRegistry).prefixedWith(graphiteServer.healthMetricsReportPrefix)
+                .forRegistry(metricRegistry)
+                .prefixedWith(graphiteServer.healthMetricsReportPrefix)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .filter(MetricFilter.ALL)
                 .build(graphite)
         reporter.start(graphiteServer.timeBetweenReportsInSeconds, TimeUnit.SECONDS)
         reporterMap[graphiteServer.id]= reporter
