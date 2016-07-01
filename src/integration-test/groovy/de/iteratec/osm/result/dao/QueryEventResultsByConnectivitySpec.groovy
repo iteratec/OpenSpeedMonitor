be72bef63e1675ec5bb27673a2db3948f6d224a3
Marcus Meier <marcus.meier@iteratec.de>
2016-07-01 15:32:05 +0200

    added missing import statement

--- src/integration-test/groovy/de/iteratec/osm/result/dao/QueryEventResultsByConnectivitySpec.groovy
+++ src/integration-test/groovy/de/iteratec/osm/result/dao/QueryEventResultsByConnectivitySpec.groovy
@@ -1,5 +1,6 @@
 package de.iteratec.osm.result.dao
 
+import de.iteratec.osm.csi.NonTransactionalIntegrationSpec
 import de.iteratec.osm.csi.Page
 import de.iteratec.osm.csi.TestDataUtil
 import de.iteratec.osm.measurement.environment.Browser
