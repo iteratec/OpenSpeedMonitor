c34ad7a521ce340cc55583225f1cdd2d13ee9271
Marko Schnecke <marko.schnecke@iteratec.de>
2016-07-06 10:57:26 +0200

    Only commiting new version numbers from jira from now on

--- scripts/ci/building/push_new_versionnumber.sh
+++ scripts/ci/building/push_new_versionnumber.sh
@@ -5,27 +5,28 @@ if [ -z $bamboo_ci_app_version ]; then
   echo "No version-number found: ${bamboo_ci_app_version}"
   exit 1
 else
-  echo "Found version-number: ${bamboo_ci_app_version}"
-
-  remote=origin
-
-  git remote remove $remote
-
-  remote_url=https://$bamboo_git_USER_NAME:$bamboo_git_PASSWORD@github.com/IteraSpeed/OpenSpeedMonitor.git
-  echo "set remote $remote to '$remote_url'"
-  git remote add -f $remote $remote_url
-
-  git config user.email "wpt@iteratec.de"
-  git config user.name "bamboo iteratec"
-
-  # the following commit message is referenced by regex in bamboo to exclude these commits
-  # while picking up changes (configured in bamboo repositories advanced settings)
-  git commit -am "[${bamboo_ci_app_version}] version update"
-
-  git pull --rebase $remote release
   if [ -z $bamboo_jira_version ]; then
     echo "Nothing to commit since we are not pushing the build numbers anymore"
   else
+    echo "Found version-number: ${bamboo_ci_app_version}"
+
+    remote=origin
+
+    git remote remove $remote
+
+    remote_url=https://$bamboo_git_USER_NAME:$bamboo_git_PASSWORD@github.com/IteraSpeed/OpenSpeedMonitor.git
+    echo "set remote $remote to '$remote_url'"
+    git remote add -f $remote $remote_url
+
+    git config user.email "wpt@iteratec.de"
+    git config user.name "bamboo iteratec"
+
+    # the following commit message is referenced by regex in bamboo to exclude these commits
+    # while picking up changes (configured in bamboo repositories advanced settings)
+    git commit -am "[${bamboo_ci_app_version}] version update"
+
+    git pull --rebase $remote release
+
     git tag "${bamboo_ci_app_version}"
     git push --tags $remote HEAD:refs/heads/release
   fi
