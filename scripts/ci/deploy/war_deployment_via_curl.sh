5b24d4ad6b2e1cd44dd9af2c65b6fe444973a014
Nils Kuhn <nils.kuhn@iteratec.de>
2016-07-13 11:18:57 +0200

    fix in deployment script

--- scripts/ci/deploy/war_deployment_via_curl.sh
+++ scripts/ci/deploy/war_deployment_via_curl.sh
@@ -6,7 +6,7 @@ ping -c 3 $bamboo_tomcat_HOST_TO_DEPLOY_TO
 echo "deploy osm war to: http://$bamboo_tomcat_HOST_TO_DEPLOY_TO:$bamboo_tomcat_PORT_TO_DEPLOY/manager/text/deploy\?update\=true\&path\=/OpenSpeedMonitor"
 curl -T ./osm_war_to_deploy.war -u $bamboo_tomcat_TOMCAT_ADMIN_USERNAME:$bamboo_tomcat_TOMCAT_ADMIN_PASSWORD \
 	http://$bamboo_tomcat_HOST_TO_DEPLOY_TO:$bamboo_tomcat_PORT_TO_DEPLOY/manager/text/deploy\?update\=true\&path\=/OpenSpeedMonitor \
-	./deploy_osm_war_output.txt
+	> ./deploy_osm_war_output.txt
 
 curl_output=$(cat ./deploy_osm_war_output.txt)
 if [[ $curl_output == *"Failed to deploy application"* ]]
