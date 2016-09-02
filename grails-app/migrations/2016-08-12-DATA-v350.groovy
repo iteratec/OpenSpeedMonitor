5f9a6d1582b02ab9077845bab1a46f61b7411682
Nils Kuhn <nils.kuhn@iteratec.de>
2016-09-02 17:55:20 +0200

    fixed liquibase changeset

--- grails-app/migrations/2016-08-12-DATA-v350.groovy
+++ grails-app/migrations/2016-08-12-DATA-v350.groovy
@@ -12,7 +12,12 @@ databaseChangeLog = {
     }
 
     // There was a bug with nightly calculation of csiAggregations, so they have to be calculated again
+    // This changeset only runs in production if some csi_aggregations already exist. In test environment with h2 database
+    // it wouldn't run successfully, because h2 doesn't have a DATE function.
     changeSet(author: "mmi", id: "1472807664000-1") {
+        preConditions(onFail: 'MARK_RAN') {
+            sqlCheck(expectedResult: 1, 'select count(id)>0 from csi_aggregation;')
+        }
         grailsChange {
             change {
                 sql.eachRow("select id from csi_aggregation where aggregator_id in (select id from aggregator_type where name in ('page', 'shop', 'csiSystem')) and interval_id in (select id from csi_aggregation_interval where name in ('daily', 'weekly')) and cs_by_wpt_doc_complete_in_percent is null and started > DATE('2016-07-14') and closed_and_calculated=1;") { csiAggregationId ->
