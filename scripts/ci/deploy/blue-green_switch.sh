141633d4e3500afb43e9631f77a51c0915d183c1
Nils Kuhn <nils.kuhn@iteratec.de>
2016-09-07 17:34:41 +0200

    improved logging in blue green switch ci script

--- scripts/ci/deploy/blue-green_switch.sh
+++ scripts/ci/deploy/blue-green_switch.sh
@@ -13,13 +13,17 @@ echo "PORT_TO_SWITCH_TO=$PORT_TO_SWITCH_TO"
 # echo variables of previous (deploy) job
 echo "PORT_TO_DEPLOY=$bamboo_tomcat_PORT_TO_DEPLOY"
 echo "PORT_TO_UNDEPLOY=$bamboo_tomcat_PORT_TO_UNDEPLOY"
-
+stdout_file="osm_$bamboo_tomcat_HOST_TO_REVERSE_PROXY_$PORT_TO_SWITCH_TO_blue-green-switch-stdout.txt"
 
 # run blue/green switch on target machine
 ssh -p $bamboo_tomcat_SSH_PORT_TO_REVERSE_PROXY -o "StrictHostKeyChecking no" $bamboo_tomcat_JENKINS_USER_NAME@$bamboo_tomcat_HOST_TO_REVERSE_PROXY \
 	"sudo /opt/osm_blue_green_switch.sh $PORT_TO_SWITCH_FROM $PORT_TO_SWITCH_TO $bamboo_tomcat_OSM_REVERSE_PROXY_URL $bamboo_tomcat_OSM_REVERSE_PROXY_IP OpenSpeedMonitor 2>&1" \
-    > .\combined_output_on_local_host.txt
-cat .\combined_output_on_local_host.txt
+    > "/tmp/$stdout_file"
+
+scp -P $bamboo_tomcat_SSH_PORT_TO_REVERSE_PROXY -o "StrictHostKeyChecking no" $bamboo_tomcat_JENKINS_USER_NAME@$bamboo_tomcat_HOST_TO_REVERSE_PROXY:"/tmp/$stdout_file" .
+echo "stdout of blue-green-switch remote execution:"
+cat ./$stdout_file
+echo "END of stdout of blue-green-switch remote execution"
 
 # undeploy old version
 if [ $PORT_TO_SWITCH_FROM -ne -1 ];
