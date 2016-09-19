d70a28c1f4dac49db2d5fb6381489a8b8cad22dd
Nils Kuhn <nils.kuhn@iteratec.de>
2016-09-19 15:06:20 +0200

    WIP: bamboo deployment scripts

--- scripts/ci/deploy/blue-green_switch.sh
+++ scripts/ci/deploy/blue-green_switch.sh
@@ -14,9 +14,10 @@ echo "PORT_TO_SWITCH_TO=$PORT_TO_SWITCH_TO"
 echo "PORT_TO_DEPLOY=$bamboo_tomcat_PORT_TO_DEPLOY"
 echo "PORT_TO_UNDEPLOY=$bamboo_tomcat_PORT_TO_UNDEPLOY"
 stderr_file="osm_${bamboo_tomcat_HOST_TO_REVERSE_PROXY}_${PORT_TO_SWITCH_TO}_blue-green-switch-stderr.txt"
+echo "stderr_file=${stderr_file}"
 
-# run blue/green switch on target machine
-
+echo "run blue/green switch on target machine"
+echo "#######################################"
 ssh -p $bamboo_tomcat_SSH_PORT_TO_REVERSE_PROXY -o "StrictHostKeyChecking no" $bamboo_tomcat_JENKINS_USER_NAME@$bamboo_tomcat_HOST_TO_REVERSE_PROXY \
 	"sudo /opt/osm_blue_green_switch.sh $PORT_TO_SWITCH_FROM $PORT_TO_SWITCH_TO $bamboo_tomcat_OSM_REVERSE_PROXY_URL $bamboo_tomcat_OSM_REVERSE_PROXY_IP OpenSpeedMonitor 2> /tmp/${stderr_file}" \
    && scp -P $bamboo_tomcat_SSH_PORT_TO_REVERSE_PROXY -o "StrictHostKeyChecking no" $bamboo_tomcat_JENKINS_USER_NAME@$bamboo_tomcat_HOST_TO_REVERSE_PROXY:"/tmp/$stderr_file" .
@@ -24,15 +25,20 @@ ssh -p $bamboo_tomcat_SSH_PORT_TO_REVERSE_PROXY -o "StrictHostKeyChecking no" $b
 echo "stderr of blue-green-switch remote execution:"
 cat ./$stderr_file
 echo "END of stderr of blue-green-switch remote execution"
-
-# let this script fail if osm_blue_green_switch.sh failed to run
 cat ./$stderr_file | egrep ".*"
 if [ $? -eq 0 ]; then
-  exit 1
+    exit 1
 fi
 
-# undeploy old version
-if [ $PORT_TO_SWITCH_FROM -ne -1 ];
-	then
-		curl -u $bamboo_tomcat_TOMCAT_ADMIN_USERNAME:$bamboo_tomcat_TOMCAT_ADMIN_PASSWORD http://$bamboo_tomcat_HOST_TO_DEPLOY_TO:$PORT_TO_SWITCH_FROM/manager/text/undeploy\?path\=/OpenSpeedMonitor
+echo "undeploy old version"
+echo "#######################################"
+if [ $PORT_TO_SWITCH_FROM -ne -1 ]; then
+    curl -u $bamboo_tomcat_TOMCAT_ADMIN_USERNAME:$bamboo_tomcat_TOMCAT_ADMIN_PASSWORD http://$bamboo_tomcat_HOST_TO_DEPLOY_TO:$PORT_TO_SWITCH_FROM/manager/text/undeploy\?path\=/OpenSpeedMonitor
 fi
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "success if previous steps didn't fail"
+echo "#######################################"
+exit 0
