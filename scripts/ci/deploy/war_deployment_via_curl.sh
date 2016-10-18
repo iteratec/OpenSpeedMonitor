356148ed8477c6da855a3310185b4355546e4562
Marko Schnecke <marko.schnecke@iteratec.de>
2016-10-18 12:09:00 +0200

    changed deploying to /

--- scripts/ci/deploy/war_deployment_via_curl.sh
+++ scripts/ci/deploy/war_deployment_via_curl.sh
@@ -4,11 +4,11 @@ echo "ping $bamboo_tomcat_HOST_TO_DEPLOY_TO"
 ping -c 3 $bamboo_tomcat_HOST_TO_DEPLOY_TO
 mv *.war ROOT.war
 echo "deploy osm war to:"
-echo "  http://$bamboo_tomcat_HOST_TO_DEPLOY_TO:$bamboo_tomcat_PORT_TO_DEPLOY/manager/text/deploy\?update\=true\&path\=/ROOT"
+echo "  http://$bamboo_tomcat_HOST_TO_DEPLOY_TO:$bamboo_tomcat_PORT_TO_DEPLOY/manager/text/deploy\?update\=true\&path\=/"
 echo "  war file:"
 ls -la ./*.war
 curl -T ./*.war -u $bamboo_tomcat_TOMCAT_ADMIN_USERNAME:$bamboo_tomcat_TOMCAT_ADMIN_PASSWORD \
-	http://$bamboo_tomcat_HOST_TO_DEPLOY_TO:$bamboo_tomcat_PORT_TO_DEPLOY/manager/text/deploy\?update\=true\&path\=/ROOT \
+	http://$bamboo_tomcat_HOST_TO_DEPLOY_TO:$bamboo_tomcat_PORT_TO_DEPLOY/manager/text/deploy\?update\=true\&path\=/ \
 	> ./deploy_osm_war_output.txt
 
 curl_output=$(cat ./deploy_osm_war_output.txt)
