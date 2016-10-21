ec9ac010099b4b97a94623fd8313bd657035c999
Nils Kuhn <nils.kuhn@iteratec.de>
2016-10-21 16:48:20 +0200

    added logging to deployment script

--- scripts/ci/deploy/run-deployment-script-on-target-host.sh
+++ scripts/ci/deploy/run-deployment-script-on-target-host.sh
@@ -4,6 +4,8 @@ set -e
 echo "running deployment script on target host"
 echo "####################################################"
 stderr_file="osm_${bamboo_tomcat_SSH_PORT_TO_HOST_TO_DEPLOY_TO}_run_deployment_script_on_target_host_stderr.txt"
+echo "run on target host:"
+echo "sudo /opt/osm_deployment.sh $bamboo_tomcat_PORT_TO_DEPLOY /home/$bamboo_tomcat_JENKINS_USER_NAME $bamboo_tomcat_EXTERNAL_OSM_CONFIG_FOLDER $bamboo_tomcat_EXTERNAL_OSM_CONFIG_FILE $bamboo_tomcat_TOMCAT_SERVER_PORT_TO_WAIT_FOR $bamboo_tomcat_TOMCAT_AJP_PORT_TO_WAIT_FOR $bamboo_tomcat_TOMCAT_TLS_PORT_TO_WAIT_FOR"
 ssh -p $bamboo_tomcat_SSH_PORT_TO_HOST_TO_DEPLOY_TO -o "StrictHostKeyChecking no" $bamboo_tomcat_JENKINS_USER_NAME@$bamboo_tomcat_HOST_TO_DEPLOY_TO \
     "sudo /opt/osm_deployment.sh $bamboo_tomcat_PORT_TO_DEPLOY /home/$bamboo_tomcat_JENKINS_USER_NAME $bamboo_tomcat_EXTERNAL_OSM_CONFIG_FOLDER $bamboo_tomcat_EXTERNAL_OSM_CONFIG_FILE $bamboo_tomcat_TOMCAT_SERVER_PORT_TO_WAIT_FOR $bamboo_tomcat_TOMCAT_AJP_PORT_TO_WAIT_FOR $bamboo_tomcat_TOMCAT_TLS_PORT_TO_WAIT_FOR 2> /tmp/${stderr_file}" \
     && scp -P $bamboo_tomcat_SSH_PORT_TO_HOST_TO_DEPLOY_TO -o "StrictHostKeyChecking no" $bamboo_tomcat_JENKINS_USER_NAME@$bamboo_tomcat_HOST_TO_DEPLOY_TO:"/tmp/$stderr_file" .
