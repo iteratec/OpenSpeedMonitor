5e6402fac23be5b34b49ccbd5098c139137ce079
Marko Schnecke <marko.schnecke@iteratec.de>
2016-11-01 15:30:02 +0100

    adjusted the browser-window width

--- src/integration-test/groovy/GebConfig.groovy
+++ src/integration-test/groovy/GebConfig.groovy
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Dimension
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.phantomjs.PhantomJSDriver
@@ -16,7 +17,7 @@ driver = {
     caps.setCapability('phantomjs.binary.path','./node_modules/phantomjs-prebuilt/bin/phantomjs')
 
     def driverInstance = new PhantomJSDriver(caps)
-    driverInstance.manage().window().maximize()
+    driverInstance.manage().window().setSize(new Dimension(1000,1000)) // width of bootstrap 3  viewport of col-md-* is 970px
     driverInstance
 }
 
@@ -27,7 +28,7 @@ environments {
     chrome {
         driver = {
             def driverInstance = new ChromeDriver()
-            driverInstance.manage().window().maximize()
+            driverInstance.manage().window().setSize(new Dimension(1000,1000))// width of bootstrap 3  viewport of col-md-* is 970px
             driverInstance
         }
     }
@@ -36,7 +37,7 @@ environments {
     firefox {
         driver = {
             def driverInstance = new FirefoxDriver()
-            driverInstance.manage().window().maximize()
+            driverInstance.manage().window().setSize(new Dimension(1000,1000))// width of bootstrap 3  viewport of col-md-* is 970px
             driverInstance
         }
     }
