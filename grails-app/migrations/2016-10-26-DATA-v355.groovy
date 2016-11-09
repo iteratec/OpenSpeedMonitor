5d9dbbd633ffcad69bf05d0a4ae72c6f6d79d801
Marcus Meier <marcus.meier@iteratec.de>
2016-11-09 09:43:08 +0100

    renamed changelogs for refactoring tag attribute.

--- grails-app/migrations/2016-10-26-DATA-v355.groovy
+++ grails-app/migrations/2016-10-26-DATA-v355.groovy
@@ -1,5 +1,26 @@
 databaseChangeLog = {
-    // section measuredEventTag
+    // split eventResultTag
+    changeSet(author: "mmi", id: "1472807664005-1") {
+        grailsChange {
+            change {
+                sql.eachRow("select id, tag from event_result;") { csiAggregation ->
+                    String currentTag = csiAggregation.tag
+
+                    def jobGroupId = currentTag.split(";")[0]
+                    def measuredEventId = currentTag.split(";")[1]
+                    def pageId = currentTag.split(";")[2]
+                    def browserId = currentTag.split(";")[3]
+                    def locationId = currentTag.split(";")[4]
+
+                    sql.executeUpdate("update event_result set job_group_id = :jobGroupId, measured_event_id = :measuredEventId, page_id = :pageId, browser_id = :browserId, location_id = :locationId where id = :currentId",
+                            [currentId: csiAggregation.id, jobGroupId: jobGroupId, measuredEventId: measuredEventId, pageId: pageId, browserId: browserId, locationId: locationId])
+
+                }
+            }
+        }
+    }
+
+    // split measuredEventTag
     changeSet(author: "mmi", id: "1472807664001-1") {
         grailsChange {
             change {
@@ -20,7 +41,7 @@ databaseChangeLog = {
         }
     }
 
-    // section pageTag
+    // split pageTag
     changeSet(author: "mmi", id: "1472807664001-2") {
         grailsChange {
             change {
@@ -38,7 +59,7 @@ databaseChangeLog = {
         }
     }
 
-    // section shopTag
+    // split shopTag
     changeSet(author: "mmi", id: "1472807664001-3") {
         grailsChange {
             change {
@@ -55,4 +76,35 @@ databaseChangeLog = {
         }
     }
 
+    
+    changeSet(author: "mmi", id: "1473397000359-9") {
+        addNotNullConstraint(columnDataType: "bigint", columnName: "browser_id", tableName: "event_result")
+    }
+
+    changeSet(author: "mmi", id: "1473397000359-10") {
+        addNotNullConstraint(columnDataType: "bigint", columnName: "job_group_id", tableName: "event_result")
+    }
+
+    changeSet(author: "mmi", id: "1473397000359-11") {
+        addNotNullConstraint(columnDataType: "bigint", columnName: "location_id", tableName: "event_result")
+    }
+
+    changeSet(author: "mmi", id: "1473397000359-12") {
+        addNotNullConstraint(columnDataType: "bigint", columnName: "page_id", tableName: "event_result")
+    }
+
+    changeSet(author: "mmi", id: "1473397000359-13") {
+        dropColumn(columnName: "tag", tableName: "event_result")
+    }
+    changeSet(author: "mmi", id: "1473397000359-14") {
+        dropColumn(columnName: "tag", tableName: "csi_aggregation")
+    }
+
+    // Delete csiAggregationUpdateEvents which belong to already deleted csiAggregations.
+    changeSet(author: "mmi", id: "1477505809000-1") {
+        sql('''
+            delete from csi_aggregation_update_event where csi_aggregation_id not in (
+                select id from csi_aggregation);
+        ''')
+    }
 }
--- grails-app/migrations/2016-10-26-SCHEME-v355.groovy
+++ grails-app/migrations/2016-10-26-SCHEME-v355.groovy
@@ -1,5 +1,46 @@
 databaseChangeLog = {
 
+    // ### BEGIN Refactoring of tag attribute ###
+    changeSet(author: "marcus (generated)", id: "1473397000359-1") {
+        addColumn(tableName: "event_result") {
+            column(name: "browser_id", type: "bigint")
+        }
+    }
+
+    changeSet(author: "marcus (generated)", id: "1473397000359-2") {
+        addColumn(tableName: "event_result") {
+            column(name: "job_group_id", type: "bigint")
+        }
+    }
+
+    changeSet(author: "marcus (generated)", id: "1473397000359-3") {
+        addColumn(tableName: "event_result") {
+            column(name: "location_id", type: "bigint")
+        }
+    }
+
+    changeSet(author: "marcus (generated)", id: "1473397000359-4") {
+        addColumn(tableName: "event_result") {
+            column(name: "page_id", type: "bigint")
+        }
+    }
+
+    changeSet(author: "marcus (generated)", id: "1473397000359-5") {
+        addForeignKeyConstraint(baseColumnNames: "location_id", baseTableName: "event_result", constraintName: "FK_1lqpe6ckkhtlm17wdcqpp0bf4", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "location")
+    }
+
+    changeSet(author: "marcus (generated)", id: "1473397000359-6") {
+        addForeignKeyConstraint(baseColumnNames: "browser_id", baseTableName: "event_result", constraintName: "FK_3a0bt55f0gmtigcetbyg2bovk", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "browser")
+    }
+
+    changeSet(author: "marcus (generated)", id: "1473397000359-7") {
+        addForeignKeyConstraint(baseColumnNames: "page_id", baseTableName: "event_result", constraintName: "FK_5sk7tn8qu8oyb6h4kxs555lvd", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "page")
+    }
+
+    changeSet(author: "marcus (generated)", id: "1473397000359-8") {
+        addForeignKeyConstraint(baseColumnNames: "job_group_id", baseTableName: "event_result", constraintName: "FK_nd8wwj4cql52rwjpo11njys5r", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "job_group")
+    }
+
     changeSet(author: "marcus (generated)", id: "1473079647061-1") {
         addColumn(tableName: "csi_aggregation") {
             column(name: "browser_id", type: "bigint")
@@ -49,4 +90,6 @@ databaseChangeLog = {
     changeSet(author: "marcus (generated)", id: "1473079647061-10") {
         addForeignKeyConstraint(baseColumnNames: "location_id", baseTableName: "csi_aggregation", constraintName: "FK_rtpndsvi65apepp2ej6lkdns5", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "location")
     }
+
+    // ### END Refactoring of tag attribute ###
 }
