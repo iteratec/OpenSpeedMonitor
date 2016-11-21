55c1badf16c3d88f48ee91dd8614be9acff4abac
Stefan Burnicki <stefan.burnicki@iteratec.de>
2016-11-21 14:22:23 +0100

    [IT-1299] WIP first logic to get pages for result selection

--- grails-app/controllers/de/iteratec/osm/result/ResultSelectionController.groovy
+++ grails-app/controllers/de/iteratec/osm/result/ResultSelectionController.groovy
@@ -1,8 +1,10 @@
 package de.iteratec.osm.result
 
 import de.iteratec.osm.api.dto.JobGroupDto
+import de.iteratec.osm.api.dto.PageDto
 import de.iteratec.osm.measurement.schedule.dao.JobGroupDaoService
 import de.iteratec.osm.util.ControllerUtils
+import org.hibernate.FetchMode
 import org.joda.time.DateTime
 import org.springframework.http.HttpStatus
 
@@ -24,9 +26,38 @@ class ResultSelectionController {
         def availableJobGroups = jobGroupDaoService.findByJobResultsInTimeFrame(command.from.toDate(), command.to.toDate())
         ControllerUtils.sendObjectAsJSON(response, JobGroupDto.create(availableJobGroups))
     }
+
+    def getPagesInTimeFrame(ResultSelectionGetPagesCommand command) {
+        // need to explicitly select id an name, since gorm/hibernate takes 10x as long for fetching the page
+        def pages = EventResult.createCriteria().list {
+            fetchMode('page', FetchMode.JOIN)
+            and {
+                between("jobResultDate", command.from.toDate(), command.to.toDate())
+                if (command.jobGroupIds) {
+                    jobGroup {
+                        'in'("id", command.jobGroupIds)
+                    }
+                }
+            }
+
+            projections {
+                page {
+                    distinct('id')
+                    property('name')
+                }
+            }
+        }
+        ControllerUtils.sendObjectAsJSON(response, pages.collect { [id: it[0], name: it[1]] as PageDto })
+    }
 }
 
 class ResultSelectionTimeFrameCommand {
     DateTime from;
     DateTime to;
+}
+
+class ResultSelectionGetPagesCommand {
+    DateTime from;
+    DateTime to;
+    List<Long> jobGroupIds;
 }
