2e758bd9abd4e5c919d8199e28c84bc1a34f0c17
Nils Kuhn <nils.kuhn@iteratec.de>
2016-12-08 00:29:43 +0100

    fixed geb tests

--- src/integration-test/groovy/geb/pages/de/iteratec/osm/result/EventResultDashboardPage.groovy
+++ src/integration-test/groovy/geb/pages/de/iteratec/osm/result/EventResultDashboardPage.groovy
@@ -6,9 +6,12 @@ import geb.Page
  * Created by marcus on 16.06.16.
  */
 class EventResultDashboardPage extends Page {
+
     static url = "/eventResultDashboard/showAll"
 
-    static at = { title == "Dashboard" }
+    static at = {
+        title == "Time Series"
+    }
 
     static content = {
         showButton(to: EventResultDashboardPage) { $("#graphButtonHtmlId") }
