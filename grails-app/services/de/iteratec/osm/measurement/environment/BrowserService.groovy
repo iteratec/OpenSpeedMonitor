3cb9f7a5c0e99d3642d5689dde45a29f37d9552b
Marcus Meier <marcus.meier@iteratec.de>
2016-12-14 17:17:15 +0100

    refactored LocationPersisterService

--- grails-app/services/de/iteratec/osm/measurement/environment/BrowserService.groovy
+++ grails-app/services/de/iteratec/osm/measurement/environment/BrowserService.groovy
@@ -28,54 +28,63 @@ import org.joda.time.Duration
 @Deprecated
 @Transactional
 class BrowserService {
-	
-	Map<Long, Browser> browsermap
-	private DateTime lastFetchOfBrowsermap = new DateTime(1980,1,1,0,0)
 
-	/**
-	 * Returns a map of Browsers in db with id's of Browsers as keys.
-	 * @param ageToleranceInHours
-	 * @return id to object-map
-	 * 			[Browser1.id: Browser1,
-	 * 			Browser2.id: Browser2,
-	 * 			...
-	 * 			BrowserN.id: BrowserN]
-	 */
-	Map<Long, Browser> getCachedBrowserMap(Integer ageToleranceInHours) {
-		Duration durationSinceLastFetch = new Duration(lastFetchOfBrowsermap.getMillis(), new DateTime().getMillis())
-		if(!browsermap || durationSinceLastFetch.getStandardHours()>ageToleranceInHours){
-			browsermap = getBrowserMap()
-			lastFetchOfBrowsermap = new DateTime()
-		}
-		return browsermap
-	}
-	Map<Long, Browser> getBrowserMap() {
-		Map<Long, Browser> browsers = [:]
-		Browser.list().each{
-			browsers[it.ident()] = it
-		}
-		return browsers
-	}
+    Map<Long, Browser> browsermap
+    private DateTime lastFetchOfBrowsermap = new DateTime(1980, 1, 1, 0, 0)
+
+    /**
+     * Returns a map of Browsers in db with id's of Browsers as keys.
+     * @param ageToleranceInHours
+     * @return id to object-map
+     * 			[Browser1.id: Browser1,
+     * 			Browser2.id: Browser2,
+     * 			...
+     * 			BrowserN.id: BrowserN]
+     */
+    Map<Long, Browser> getCachedBrowserMap(Integer ageToleranceInHours) {
+        Duration durationSinceLastFetch = new Duration(lastFetchOfBrowsermap.getMillis(), new DateTime().getMillis())
+        if (!browsermap || durationSinceLastFetch.getStandardHours() > ageToleranceInHours) {
+            browsermap = getBrowserMap()
+            lastFetchOfBrowsermap = new DateTime()
+        }
+        return browsermap
+    }
+
+    Map<Long, Browser> getBrowserMap() {
+        Map<Long, Browser> browsers = [:]
+        Browser.list().each {
+            browsers[it.ident()] = it
+        }
+        return browsers
+    }
+
+    List<Browser> findAllByNameOrAlias(List<String> browserNameOrAlias) {
+        List<Browser> result = []
+        browserNameOrAlias.each {
+            result << findByNameOrAlias(it)
+        }
+        return result
+    }
 
     Browser findByNameOrAlias(String browserNameOrAlias) {
-		Browser ret = Browser.findByName(browserNameOrAlias)
-		if (ret==null) {
-			return findByAlias(browserNameOrAlias)
-		}else{
-			return ret
-		}
+        Browser ret = Browser.findByName(browserNameOrAlias)
+        if (ret == null) {
+            return findByAlias(browserNameOrAlias)
+        } else {
+            return ret
+        }
     }
-	
-	private Browser findByAlias(browserNameOrAlias){
-		Browser ret = Browser.findByName('undefined')?:new Browser(name:'undefined').save(failOnError:true)
-		Browser.list().each{currBrowser ->
-			def query = BrowserAlias.where{
-				browser == currBrowser
-			}
-			query.findAll{it.alias.equals(browserNameOrAlias)}.each{
-				ret = currBrowser
-			}
-		}
-		return ret
-	}
+
+    private Browser findByAlias(browserNameOrAlias) {
+        Browser ret = Browser.findByName('undefined') ?: new Browser(name: 'undefined').save(failOnError: true)
+        Browser.list().each { currBrowser ->
+            def query = BrowserAlias.where {
+                browser == currBrowser
+            }
+            query.findAll { it.alias.equals(browserNameOrAlias) }.each {
+                ret = currBrowser
+            }
+        }
+        return ret
+    }
 }
