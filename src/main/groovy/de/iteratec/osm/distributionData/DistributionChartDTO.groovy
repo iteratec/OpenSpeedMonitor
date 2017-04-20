bc9a8fcf057ca2a4cfb6a5ec76701cb57c76259f
Johannes Weiss <johannes.weiss@iteratec.de>
2017-04-20 09:15:12 +0200

    [IT-1615][CR-7] WIP: Distribution chart gets its colors from the chartColorProvider now

--- src/main/groovy/de/iteratec/osm/distributionData/DistributionChartDTO.groovy
+++ src/main/groovy/de/iteratec/osm/distributionData/DistributionChartDTO.groovy
@@ -3,5 +3,6 @@ package de.iteratec.osm.distributionData
 class DistributionChartDTO {
     Map<String, DistributionTrace> series = new HashMap<String, DistributionTrace>()
     Map<String, List<String>> filterRules = [:].withDefault {[]}
+    String dimensionalUnit = ""
     Map<String, String> i18nMap = [:]
 }
