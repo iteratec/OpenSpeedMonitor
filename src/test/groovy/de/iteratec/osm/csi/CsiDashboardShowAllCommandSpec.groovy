225ad4ec79d931555f6f1109f337cbcbb5d85fa0
Stefan Burnicki <stefan.burnicki@iteratec.de>
2017-04-26 15:58:18 +0200

    Removed outdated unit test

--- src/test/groovy/de/iteratec/osm/csi/CsiDashboardShowAllCommandSpec.groovy
+++ src/test/groovy/de/iteratec/osm/csi/CsiDashboardShowAllCommandSpec.groovy
@@ -5,7 +5,6 @@ import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import org.joda.time.DateTime
 import org.joda.time.Interval
-import org.junit.Test
 import spock.lang.Specification
 
 @TestMixin(GrailsUnitTestMixin)
@@ -18,29 +17,6 @@ class CsiDashboardShowAllCommandSpec extends Specification {
         dataBinder = applicationContext.getBean('grailsWebDataBinder')
     }
 
-
-    def "Test databinding from different valid date params"() {
-        given:
-        // Date (year-1900, month[0-11], day[1-31])
-        Date expectedFrom = new Date(116, 00, 03)
-        Date expectedTo = new Date(116, 01, 05)
-        SimpleMapDataBindingSource source = [from: from, to: to]
-        CsiDashboardShowAllCommand command = new CsiDashboardShowAllCommand()
-
-        when: "binding dates to command"
-        dataBinder.bind(command, source)
-
-        then: "the command has a valid date"
-        command.from == expectedFrom
-        command.to == expectedTo
-
-        where: "the command gets different type of dates"
-        from         | to
-        "03.01.2016" | "05.02.2016"
-        new Date(116, 00, 03) | new Date(116, 01, 05)
-    }
-
-    @Test
     def "Should warn about long processing time - weekly csi aggs interval two weeks"() {
         given: "Two weeks for 2 JobGroups, 5 Pages and 2 browsers of selected data and weekly csi aggs"
         int countOfSelectedBrowser = 2;
@@ -62,7 +38,6 @@ class CsiDashboardShowAllCommandSpec extends Specification {
         ) == false
     }
 
-    @Test
     def "Should not warn about long processing time - hourly csi aggs interval one year"() {
         given: "One year for 2 JobGroups, 5 Pages and 2 browsers of selected data and hourly csi aggs"
         int countOfSelectedBrowser = 2;
