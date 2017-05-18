28ae555b309a52410cbbc25f7aba196318802662
Johannes Weiss <johannes.weiss@iteratec.de>
2017-05-18 12:29:13 +0200

    [IT-1701] Refactored PaginationServiceTest

--- grails-app/services/de/iteratec/osm/result/PaginationService.groovy
+++ grails-app/services/de/iteratec/osm/result/PaginationService.groovy
@@ -22,6 +22,7 @@ import de.iteratec.osm.report.ui.PaginationListingRow
 import de.iteratec.osm.util.ParameterBindingUtility
 import grails.transaction.Transactional
 import grails.web.mapping.LinkGenerator
+
 /**
  * Provides methods to get {@link PaginationListing} for pagination.
  * @author rhc
@@ -30,115 +31,114 @@ import grails.web.mapping.LinkGenerator
 @Transactional
 class PaginationService {
 
-	LinkGenerator grailsLinkGenerator
-	
-	/**
-	 * <p>
-	 * Build a {@link PaginationListing} that contains all pagebuttons for pagination at showListResultsForJob.
-	 * </p>
-	 * 
-	 * @param cmd
-	 * 			The ListResultsForSpecificJobCommand contained already data of {@link TabularResultEventResultsCommandBase}.
-	 * @param eventResultsTotalRecords
-	 * 			The total records count.
-	 * @return
-	 */
-	public PaginationListing buildListResultsForJobPagination(TabularResultListResultsForSpecificJobCommand cmd, int eventResultsTotalRecords){
-		PaginationListing paginationListing = new PaginationListing()
-		for(int i=0; i < eventResultsTotalRecords/cmd.max; i++){
-			String paginationLink = createListResultsForJobPaginationLink(cmd, i*cmd.max)
-			String nextPaginationLink = createListResultsForJobPaginationLink(cmd, cmd.offset+cmd.max);
-			String prevPaginationLink = createListResultsForJobPaginationLink(cmd, cmd.offset-cmd.max);
-			paginationListing.addRow(new PaginationListingRow(i+1, paginationLink), cmd.offset, cmd.max, eventResultsTotalRecords, prevPaginationLink, nextPaginationLink)
-		}
-		
-		return paginationListing
-	}
-	
-	/**
-	 * <p>
-	 * Returns the PaginationLink as {@link String} for ListResultsForSpecificJobCommand.
-	 * </p>
-	 * 
-	 * @param cmd
-	 * 			The ListResultsForSpecificJobCommand contained already data of {@link TabularResultEventResultsCommandBase}.
-	 * @param offset
-	 * 			The offset of the current shown Record.
-	 * @return
-	 */
-	private String createListResultsForJobPaginationLink(TabularResultListResultsForSpecificJobCommand cmd, Integer offset){
-		String paginationLink = grailsLinkGenerator.link([
-				'controller': 'tabularResultPresentation',
-				'action': 'showListResultsForJob',
-				'params': [
-						'selectedTimeFrameInterval': 0,
-						'job.id': cmd.job.getId(),
-						'from': ParameterBindingUtility.formatDateTimeParameter(cmd.from),
-						'to': ParameterBindingUtility.formatDateTimeParameter(cmd.to),
-						'max': cmd.getMax(),
-						'offset': offset
-					]
-			]);
-	
-		return paginationLink
-	}
-	
-	
-	/**
-	 * <p>
-	 * Build a {@link PaginationListing} that contains all pagebuttons for pagination at listResults.
-	 * </p>
-	 * 
-	 * @param cmd
-	 *			The listResultsCommand contained already data of {@link TabularResultEventResultsCommandBase}.
-	 * @param eventResultsTotalRecords
-	 * 			The total records count.
-	 * @return
-	 */
-    public PaginationListing buildListResultsPagination(TabularResultListResultsCommand cmd, int eventResultsTotalRecords){
-		PaginationListing paginationListing = new PaginationListing()
-		
-		for(int i=0; i < eventResultsTotalRecords/cmd.max; i++){
-			String paginationLink = createListResultsPaginationLink(cmd, i*cmd.max)
-			String nextPaginationLink = createListResultsPaginationLink(cmd, cmd.offset+cmd.max);
-			String prevPaginationLink = createListResultsPaginationLink(cmd, cmd.offset-cmd.max);
-			
-			paginationListing.addRow(new PaginationListingRow(i+1, paginationLink), cmd.offset, cmd.max, eventResultsTotalRecords, prevPaginationLink, nextPaginationLink)
-		}
-		
-		return paginationListing
-	}
-	
-	/**
-	 * <p>
-	 * Returns the PaginationLink as {@link String} for ListResultsCommand.
-	 * </p>
-	 * 
-	 * @param cmd
-	 * 			The ListResultsCommand contained already data of {@link TabularResultEventResultsCommandBase}.
-	 * @param offset
-	 * 			The offset of the current shown Record.
-	 * @return
-	 */
-	private String createListResultsPaginationLink(TabularResultListResultsCommand cmd, int offset){
-		String paginationLink = grailsLinkGenerator.link([
-			'controller': 'tabularResultPresentation',
-			'action': 'listResults',
-			'params': [
-						'selectedTimeFrameInterval': 0,
-						'from': ParameterBindingUtility.formatDateTimeParameter(cmd.from),
-						'to': ParameterBindingUtility.formatDateTimeParameter(cmd.to),
-						'selectedFolder': cmd.getSelectedFolder(),
-						'selectedPages': cmd.getSelectedPages(),
-						'selectedBrowsers': cmd.getSelectedBrowsers(),
-                        'selectedConnectivities': cmd.getSelectedConnectivityProfiles(),
+    LinkGenerator grailsLinkGenerator
+
+    /**
+     * <p>
+     * Build a {@link PaginationListing} that contains all pagebuttons for pagination at showListResultsForJob.
+     * </p>
+     *
+     * @param cmd
+     * 			The ListResultsForSpecificJobCommand contained already data of {@link TabularResultEventResultsCommandBase}.
+     * @param eventResultsTotalRecords
+     * 			The total records count.
+     * @return
+     */
+    PaginationListing buildListResultsForJobPagination(TabularResultListResultsForSpecificJobCommand cmd, int eventResultsTotalRecords) {
+        PaginationListing paginationListing = new PaginationListing()
+        for (int i = 0; i < eventResultsTotalRecords / cmd.max; i++) {
+            String paginationLink = createListResultsForJobPaginationLink(cmd, i * cmd.max)
+            String nextPaginationLink = createListResultsForJobPaginationLink(cmd, cmd.offset + cmd.max)
+            String prevPaginationLink = createListResultsForJobPaginationLink(cmd, cmd.offset - cmd.max)
+            paginationListing.addRow(new PaginationListingRow(i + 1, paginationLink), cmd.offset, cmd.max, eventResultsTotalRecords, prevPaginationLink, nextPaginationLink)
+        }
+
+        return paginationListing
+    }
+
+    /**
+     * <p>
+     * Returns the PaginationLink as {@link String} for ListResultsForSpecificJobCommand.
+     * </p>
+     *
+     * @param cmd
+     * 			The ListResultsForSpecificJobCommand contained already data of {@link TabularResultEventResultsCommandBase}.
+     * @param offset
+     * 			The offset of the current shown Record.
+     * @return
+     */
+    private String createListResultsForJobPaginationLink(TabularResultListResultsForSpecificJobCommand cmd, Integer offset) {
+        String paginationLink = grailsLinkGenerator.link([
+                'controller': 'tabularResultPresentation',
+                'action'    : 'showListResultsForJob',
+                'params'    : [
+                        'selectedTimeFrameInterval': 0,
+                        'job.id'                   : cmd.job.getId(),
+                        'from'                     : ParameterBindingUtility.formatDateTimeParameter(cmd.from),
+                        'to'                       : ParameterBindingUtility.formatDateTimeParameter(cmd.to),
+                        'max'                      : cmd.getMax(),
+                        'offset'                   : offset
+                ]
+        ]);
+
+        return paginationLink
+    }
+
+    /**
+     * <p>
+     * Build a {@link PaginationListing} that contains all pagebuttons for pagination at listResults.
+     * </p>
+     *
+     * @param cmd
+     * 			The listResultsCommand contained already data of {@link TabularResultEventResultsCommandBase}.
+     * @param eventResultsTotalRecords
+     * 			The total records count.
+     * @return
+     */
+    PaginationListing buildListResultsPagination(TabularResultListResultsCommand cmd, int eventResultsTotalRecords) {
+        PaginationListing paginationListing = new PaginationListing()
+
+        for (int i = 0; i < eventResultsTotalRecords / cmd.max; i++) {
+            String paginationLink = createListResultsPaginationLink(cmd, i * cmd.max)
+            String nextPaginationLink = createListResultsPaginationLink(cmd, cmd.offset + cmd.max)
+            String prevPaginationLink = createListResultsPaginationLink(cmd, cmd.offset - cmd.max)
+
+            paginationListing.addRow(new PaginationListingRow(i + 1, paginationLink), cmd.offset, cmd.max, eventResultsTotalRecords, prevPaginationLink, nextPaginationLink)
+        }
+
+        return paginationListing
+    }
+
+    /**
+     * <p>
+     * Returns the PaginationLink as {@link String} for ListResultsCommand.
+     * </p>
+     *
+     * @param cmd
+     * 			The ListResultsCommand contained already data of {@link TabularResultEventResultsCommandBase}.
+     * @param offset
+     * 			The offset of the current shown Record.
+     * @return
+     */
+    private String createListResultsPaginationLink(TabularResultListResultsCommand cmd, int offset) {
+        String paginationLink = grailsLinkGenerator.link([
+                'controller': 'tabularResultPresentation',
+                'action'    : 'listResults',
+                'params'    : [
+                        'selectedTimeFrameInterval': 0,
+                        'from'                     : ParameterBindingUtility.formatDateTimeParameter(cmd.from),
+                        'to'                       : ParameterBindingUtility.formatDateTimeParameter(cmd.to),
+                        'selectedFolder'           : cmd.getSelectedFolder(),
+                        'selectedPages'            : cmd.getSelectedPages(),
+                        'selectedBrowsers'         : cmd.getSelectedBrowsers(),
+                        'selectedConnectivities'   : cmd.getSelectedConnectivityProfiles(),
                         'includeNativeConnectivity': cmd.getIncludeNativeConnectivity(),
-                        'customConnectivityName': cmd.getSelectedCustomConnectivityNames(),
-						'max': cmd.getMax(),
-						'offset': offset
-					]
-			]);
-		
-		return paginationLink
-	} 
+                        'customConnectivityName'   : cmd.getSelectedCustomConnectivityNames(),
+                        'max'                      : cmd.getMax(),
+                        'offset'                   : offset
+                ]
+        ]);
+
+        return paginationLink
+    }
 }
--- src/test/groovy/de/iteratec/osm/result/PaginationServiceTest.groovy
+++ src/test/groovy/de/iteratec/osm/result/PaginationServiceTest.groovy
@@ -18,21 +18,14 @@
 
 package de.iteratec.osm.result
 
-import de.iteratec.osm.measurement.environment.Browser
-import de.iteratec.osm.measurement.environment.BrowserAlias
-import de.iteratec.osm.measurement.environment.Location
-import de.iteratec.osm.measurement.environment.WebPageTestServer
-import de.iteratec.osm.measurement.schedule.Job
-import de.iteratec.osm.measurement.schedule.JobGroup
-import de.iteratec.osm.measurement.script.Script
-import de.iteratec.osm.report.ui.PaginationListing
+import org.joda.time.DateTime
+import grails.web.mapping.LinkGenerator
+import grails.buildtestdata.mixin.Build
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
-import grails.web.mapping.LinkGenerator
-import org.joda.time.DateTime
 import spock.lang.Specification
-
-import static org.junit.Assert.assertEquals
+import de.iteratec.osm.measurement.schedule.Job
+import de.iteratec.osm.report.ui.PaginationListing
 
 /**
  * Test-suite of {@link de.iteratec.osm.result.PaginationService}.
@@ -40,78 +33,19 @@ import static org.junit.Assert.assertEquals
  * @author rhc
  */
 @TestFor(PaginationService)
-@Mock([Job, Location, WebPageTestServer, Browser, BrowserAlias, JobGroup, Script])
+@Mock([Job])
+@Build([Job])
 class PaginationServiceTest extends Specification {
 
     PaginationService serviceUnderTest
-    WebPageTestServer server
-    Location ffAgent
-    Job job
-    String jobGroupName
 
-    void setup() {
+    def setup() {
         serviceUnderTest = service
-
-        serviceUnderTest.grailsLinkGenerator = Stub(LinkGenerator);
-
-        server = new WebPageTestServer(
-                baseUrl: 'http://server1.wpt.server.de',
-                active: true,
-                label: 'server 1 - wpt server',
-                proxyIdentifier: 'server 1 - wpt server',
-                dateCreated: new Date(),
-                lastUpdated: new Date()
-        ).save(failOnError: true)
-
-        String browserName = "FF"
-        Browser browserFF = Browser.findByName(browserName) ?: new Browser(name: browserName)
-                .addToBrowserAliases(alias: "FF")
-                .addToBrowserAliases(alias: "FF7")
-                .addToBrowserAliases(alias: "Firefox")
-                .addToBrowserAliases(alias: "Firefox7")
-                .save(failOnError: true)
-
-        ffAgent = new Location(
-                active: true,
-                location: 'physNetLabAgent01-FF',
-                label: 'physNetLabAgent01 - FF up to date',
-                browser: browserFF,
-                wptServer: server,
-                dateCreated: new Date(),
-                lastUpdated: new Date()
-        ).save(failOnError: true)
-
-        jobGroupName = 'CSI'
-        JobGroup group = new JobGroup(
-                name: jobGroupName).save(failOnError: true)
-
-        JobGroup nonCSIgroup = new JobGroup(
-                name: 'nonCSIgroup').save(failOnError: true)
-
-        Script script = Script.createDefaultScript('Unnamed').save(failOnError: true)
-        //lastRun: 07.08.2014 - 16:00
-        job = new Job(
-                active: false,
-                label: 'BV1 - Step 01',
-                description: 'This is job 01...',
-                lastRun: new Date(1407420000000L),
-                location: ffAgent,
-                frequencyInMin: 5,
-                runs: 1,
-                jobGroup: group,
-                script: script,
-                maxDownloadTimeInMinutes: 60,
-                customConnectivityProfile: true,
-                customConnectivityName: 'Custom (6.000/512 Kbps, 50ms)',
-                bandwidthDown: 6000,
-                bandwidthUp: 512,
-                latency: 50,
-                packetLoss: 0
-        ).save(failOnError: true)
+        serviceUnderTest.grailsLinkGenerator = Stub(LinkGenerator)
     }
 
-    void testBuildListResultsPagination() {
-        given:
+    def "test build list results pagination"() {
+        given: "a command for the tabular result list"
         TabularResultListResultsCommand cmd = new TabularResultListResultsCommand()
         cmd.setFrom(new DateTime(2014, 8, 8, 4, 0))
         cmd.setTo(new DateTime(2014, 8, 8, 6, 0))
@@ -120,59 +54,54 @@ class PaginationServiceTest extends Specification {
         cmd.setSelectedBrowsers([3L])
         cmd.setSelectedMeasuredEventIds([])
 
-        // Simulate GrailsLinkGenerator
-        // Inject relevant services
-        serviceUnderTest.grailsLinkGenerator = Stub(LinkGenerator) {
-            link(_) >> "http://example.com/eventResult/listResults?selectedTimeFrameInterval=0&from=2014-08-08T04%3A00%3A00.000Z&to=2014-08-08T06%3A00%3A00.000Z&selectedFolder=2&selectedPages=1&selectedBrowsers=3&_action_listResults=Show"
-        }
+        stubbedLinkFromService("http://example.com/eventResult/listResults?selectedTimeFrameInterval=0&from=2014-08-08T04%3A00%3A00.000Z&to=2014-08-08T06%3A00%3A00.000Z&selectedFolder=2&selectedPages=1&selectedBrowsers=3&_action_listResults=Show")
 
+        when: "buildListResultsPagination is called"
         PaginationListing paginationListing = serviceUnderTest.buildListResultsPagination(cmd, 100)
 
-        when:
+
+        then: "one gets the correct parameters to build the pagination for result list table"
         Map<String, List<String>> queryParams = getQueryParams(paginationListing.getRows().get(0).pageLink)
-
-        then:
-        assertEquals(2, paginationListing.getRows().size())
-        assertEquals(1, paginationListing.getRows().get(0).pageNumber)
-
-        assertEquals("2014-08-08T04:00:00.000Z", queryParams.get("from").get(0))
-        assertEquals("2014-08-08T06:00:00.000Z", queryParams.get("to").get(0))
-        assertEquals(cmd.getSelectedFolder().toString(), queryParams.get("selectedFolder").toString())
-        assertEquals(cmd.getSelectedPages().toString(), queryParams.get("selectedPages").toString())
-        assertEquals(cmd.getSelectedBrowsers().toString(), queryParams.get("selectedBrowsers").toString())
+        paginationListing.getRows().size() == 2
+        paginationListing.getRows().get(0).pageNumber == 1
+        queryParams.get("from").get(0) == "2014-08-08T04:00:00.000Z"
+        queryParams.get("to").get(0) == "2014-08-08T06:00:00.000Z"
+        queryParams.get("selectedFolder").toString() == cmd.getSelectedFolder().toString()
+        queryParams.get("selectedPages").toString() == cmd.getSelectedPages().toString()
+        queryParams.get("selectedBrowsers").toString() == cmd.getSelectedBrowsers().toString()
     }
 
-    void testBuildListResultsForJobPagination() {
-        given:
+    def "test build list results for job pagination"() {
+        given: "a command for the job list"
         TabularResultListResultsForSpecificJobCommand cmd = new TabularResultListResultsForSpecificJobCommand()
 
+        Job job = Job.build()
         cmd.setJob(job)
-
         cmd.setFrom(new DateTime(2014, 8, 7, 4, 0))
         cmd.setTo(new DateTime(2014, 8, 8, 4, 0))
         cmd.setMax(50)
         cmd.setOffset(0)
 
-        // Simulate GrailsLinkGenerator
-        // Inject relevant services
-        serviceUnderTest.grailsLinkGenerator = Stub(LinkGenerator) {
-            link(_) >> "http://example.com/eventResult/showListResultsForJob?selectedTimeFrameInterval=0&job.id=1&from=2014-08-07T04%3A00%3A00.000Z&to=2014-08-08T04%3A00%3A00.000Z"
-        };
+        stubbedLinkFromService("http://example.com/eventResult/showListResultsForJob?selectedTimeFrameInterval=0&job.id=1&from=2014-08-07T04%3A00%3A00.000Z&to=2014-08-08T04%3A00%3A00.000Z")
 
+        when:"when buildListResultsForJobPagination is called"
         PaginationListing paginationListing = serviceUnderTest.buildListResultsForJobPagination(cmd, 100)
 
-        when:
-        // put url-params into a map
+
+        then: "one gets the correct parameters to build the pagination for job list table"
         Map<String, List<String>> queryParams = getQueryParams(paginationListing.getRows().get(0).pageLink)
 
-        then:
-        // validate params
-        assertEquals(2, paginationListing.getRows().size())
-        assertEquals(1, paginationListing.getRows().get(0).pageNumber)
+        paginationListing.getRows().size() == 2
+        paginationListing.getRows().get(0).pageNumber == 1
+        queryParams.get("from").get(0) == "2014-08-07T04:00:00.000Z"
+        queryParams.get("to").get(0) == "2014-08-08T04:00:00.000Z"
+        queryParams.get("job.id").get(0).toString() == cmd.getJob().getId().toString()
+    }
 
-        assertEquals("2014-08-07T04:00:00.000Z", queryParams.get("from").get(0))
-        assertEquals("2014-08-08T04:00:00.000Z", queryParams.get("to").get(0))
-        assertEquals(cmd.getJob().getId().toString(), queryParams.get("job.id").get(0).toString())
+    private void stubbedLinkFromService(String linkAsString) {
+        serviceUnderTest.grailsLinkGenerator = Stub(LinkGenerator) {
+            link(_) >> linkAsString
+        }
     }
 
     /**
@@ -183,30 +112,30 @@ class PaginationServiceTest extends Specification {
      */
     private static Map<String, List<String>> getQueryParams(String url) {
         try {
-            Map<String, List<String>> params = new HashMap<String, List<String>>();
-            String[] urlParts = url.split("\\?");
+            Map<String, List<String>> params = new HashMap<String, List<String>>()
+            String[] urlParts = url.split("\\?")
             if (urlParts.length > 1) {
-                String query = urlParts[1];
+                String query = urlParts[1]
                 for (String param : query.split("&")) {
-                    String[] pair = param.split("=");
-                    String key = URLDecoder.decode(pair[0], "UTF-8");
-                    String value = "";
+                    String[] pair = param.split("=")
+                    String key = URLDecoder.decode(pair[0], "UTF-8")
+                    String value = ""
                     if (pair.length > 1) {
-                        value = URLDecoder.decode(pair[1], "UTF-8");
+                        value = URLDecoder.decode(pair[1], "UTF-8")
                     }
 
-                    List<String> values = params.get(key);
+                    List<String> values = params.get(key)
                     if (values == null) {
-                        values = new ArrayList<String>();
-                        params.put(key, values);
+                        values = new ArrayList<String>()
+                        params.put(key, values)
                     }
-                    values.add(value);
+                    values.add(value)
                 }
             }
 
-            return params;
+            return params
         } catch (UnsupportedEncodingException ex) {
-            throw new AssertionError(ex);
+            throw new AssertionError(ex)
         }
     }
 }
