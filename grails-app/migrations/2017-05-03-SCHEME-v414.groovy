38bb4d35f36141a8ecacc96ff99516d4fe4043b1
Stefan Burnicki <stefan.burnicki@iteratec.de>
2017-05-19 10:51:48 +0200

    Adjusted changelog to uppercase enum value

--- grails-app/migrations/2017-05-03-SCHEME-v414.groovy
+++ grails-app/migrations/2017-05-03-SCHEME-v414.groovy
@@ -41,7 +41,7 @@ databaseChangeLog = {
 
     changeSet(author: "dkl", id: "1493799755-1") {
         addColumn(tableName: "osm_configuration") {
-            column(defaultValue: "NotStarted", name: "infrastructure_setup_ran", type: "varchar(255)") {
+            column(defaultValue: "NOT_STARTED", name: "infrastructure_setup_ran", type: "varchar(255)") {
                 constraints(nullable: "false")
             }
         }
