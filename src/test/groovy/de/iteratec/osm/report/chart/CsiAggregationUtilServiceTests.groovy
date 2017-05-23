d540a4cebccf27c11460ecc3623e50aa81f0bfd2
Benjamin Wollmer <2wollmer@informatik.uni-hamburg.de>
2017-05-23 15:24:48 +0200

    [CR-36] resolved concerns

--- src/test/groovy/de/iteratec/osm/report/chart/CsiAggregationUtilServiceTests.groovy
+++ src/test/groovy/de/iteratec/osm/report/chart/CsiAggregationUtilServiceTests.groovy
@@ -17,267 +17,201 @@
 
 package de.iteratec.osm.report.chart
 
-import spock.lang.Specification
-
 import grails.test.mixin.TestFor
-
 import org.joda.time.DateTime
 import org.joda.time.DateTimeConstants
 import org.joda.time.Interval
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static de.iteratec.osm.report.chart.CsiAggregationInterval.*
 
 /**
  * Test-suite of {@link CsiAggregationUtilService}.
  */
 @TestFor(CsiAggregationUtilService)
-class CsiAggregationUtilServiceTests extends Specification{
+class CsiAggregationUtilServiceTests extends Specification {
 
-	CsiAggregationUtilService serviceUnderTest
-	
-	def setup() {
-		serviceUnderTest=service
-	}
-	
+    @Shared
+    DateTime midnightLastDayOfSummertime = new DateTime(2013, 10, 27, 0, 0, 0)
+    @Shared
+    DateTime twoHoursBeforeEndOfSummertime = midnightLastDayOfSummertime.plusHours(1)
+    @Shared
+    DateTime oneHourBeforeEndOfSummertime = midnightLastDayOfSummertime.plusHours(2)
+    @Shared
+    DateTime noonLastDayOfSummertime = new DateTime(2013, 10, 27, 12, 0, 0)
+    @Shared
+    DateTime oneHourAfterNoonLastDayOfSummertime = noonLastDayOfSummertime.plusHours(1)
+    @Shared
+    DateTime threeDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(3)
+    @Shared
+    DateTime tenDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(10)
+    @Shared
+    DateTime fourDaysAfterEndOfSummertime = midnightLastDayOfSummertime.plusDays(4)
+    @Shared
+    DateTime elevenDaysAfterEndOfSummertime = midnightLastDayOfSummertime.plusDays(11)
+    @Shared
+    DateTime twoDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(2)
+    @Shared
+    DateTime midnightFirstDayAfterSummertime = midnightLastDayOfSummertime.plusDays(1)
+    @Shared
+    DateTime midnightSecondDayAfterSummertime = midnightLastDayOfSummertime.plusDays(2)
+    @Shared
+    DateTime exactlyEndOfSummertime = midnightLastDayOfSummertime.plusHours(3)
+    @Shared
+    DateTime oneHourAfterEndOfSummertime = midnightLastDayOfSummertime.plusHours(4)
+    CsiAggregationUtilService serviceUnderTest
 
-    void "test resetToStartOfActualInterval with HourlyReset"() {
-		given:
-		DateTime dateToReset = new DateTime(2013, 1, 10, 13, 43, 12, 234)
-		DateTime expectedResult = new DateTime(2013, 1, 10, 13, 0, 0, 0)
-
-		when:
-		DateTime currentResult = serviceUnderTest.resetToStartOfActualInterval(
-				dateToReset, CsiAggregationInterval.HOURLY)
-
-		then:
-		expectedResult == currentResult
-	}
-	
-
-	void "test resetToStartOfActualInterval with DailyReset"() {
-		given:
-		DateTime dateToReset = new DateTime(2013, 1, 10, 13, 43, 12, 234)
-		DateTime expectedResult = new DateTime(2013, 1, 10, 0, 0, 0, 0)
-
-		when:
-		DateTime currentResult = serviceUnderTest.resetToStartOfActualInterval(
-				dateToReset, CsiAggregationInterval.DAILY)
-
-		then:
-		expectedResult == currentResult
-	}
-
-
-	void "test resetToStartOfActualInterval with WeeklyReset"() {
-		given:
-		DateTime dateToReset = new DateTime(2013, 1, 10, 13, 43, 12, 234)
-		DateTime expectedResult = new DateTime(2013, 1, 4, 0, 0, 0, 0)
-
-		when:
-		DateTime currentResult = serviceUnderTest.resetToStartOfActualInterval(
-				dateToReset, CsiAggregationInterval.WEEKLY)
-		then:
-		expectedResult == currentResult
+    def setup() {
+        serviceUnderTest = service
     }
 
 
-	void testResetToEndOfActualInterval_HourlyReset() {
-		given:
-		DateTime dateToReset = new DateTime(2013, 1, 10, 13, 43, 12)
-		DateTime expectedResult = new DateTime(2013, 1, 10, 14, 0, 0, 0)
+    @Unroll
+    void "test reset to start of actual interval with Interval: #interval"() {
+        when:
+        DateTime dateToReset = new DateTime(2013, 1, 10, 13, 43, 12, 234)
 
-		when:
-		DateTime currentResult = serviceUnderTest.resetToEndOfActualInterval(
-			dateToReset, CsiAggregationInterval.HOURLY)
+        then:
+        serviceUnderTest.resetToStartOfActualInterval(dateToReset, interval) == expect
 
-		then:
-		expectedResult == currentResult
-	}
+        where:
+        interval || expect
+        HOURLY   || new DateTime(2013, 1, 10, 13, 0, 0, 0)
+        DAILY    || new DateTime(2013, 1, 10, 0, 0, 0, 0)
+        WEEKLY   || new DateTime(2013, 1, 4, 0, 0, 0, 0)
+    }
 
+    @Unroll
+    void "test reset to end of actual interval withInterval: #interval"() {
+        when:
+        DateTime dateToReset = new DateTime(2013, 1, 10, 13, 43, 12)
 
-	void testResetToEndOfActualInterval_DailyReset() {
-		given:
-		DateTime dateToReset = new DateTime(2013, 1, 10, 13, 43, 12)
-		DateTime expectedResult = new DateTime(2013, 1, 11, 0, 0, 0, 0)
+        then:
+        serviceUnderTest.resetToEndOfActualInterval(dateToReset, interval) == expect
 
-		when:
-		DateTime currentResult = serviceUnderTest.resetToEndOfActualInterval(
-			dateToReset, CsiAggregationInterval.DAILY)
-		then:
-		expectedResult == currentResult
-	}
+        where:
+        interval || expect
+        HOURLY   || new DateTime(2013, 1, 10, 14, 0, 0, 0)
+        DAILY    || new DateTime(2013, 1, 11, 0, 0, 0)
+        WEEKLY   || new DateTime(2013, 1, 11, 0, 0, 0, 0)
+    }
 
-
-	void testResetToEndOfActualInterval_WeeklyReset() {
-		given:
-		DateTime dateToReset = new DateTime(2013, 1, 10, 13, 43, 12)
-		DateTime expectedResult = new DateTime(2013, 1, 11, 0, 0, 0, 0)
-
-		when:
-		DateTime currentResult = serviceUnderTest.resetToEndOfActualInterval(
-			dateToReset, CsiAggregationInterval.WEEKLY)
-
-		then:
-		expectedResult == currentResult
-	}
-
-
-
-	void testResetToStartOfActualInterval_WeeklyReset_Thursday() {
+    void "reset to start of actual interval with WeeklyReset on Thursday"() {
         given:
-		DateTime dateToReset = new DateTime(2013, 9, 10, 13, 43, 12)
-		DateTime expectedResult = new DateTime(2013, 9, 5, 0, 0, 0, 0)
+        DateTime dateToReset = new DateTime(2013, 9, 10, 13, 43, 12)
+        DateTime expectedResult = new DateTime(2013, 9, 5, 0, 0, 0, 0)
 
-		when:
-		DateTime currentResult = serviceUnderTest.resetToStartOfActualInterval(
-			dateToReset, CsiAggregationInterval.WEEKLY,
-			DateTimeConstants.THURSDAY)
+        when:
+        DateTime currentResult = serviceUnderTest.resetToStartOfActualInterval(dateToReset, WEEKLY, DateTimeConstants.THURSDAY)
 
-		then:
-		expectedResult == currentResult
-	}
+        then:
+        expectedResult == currentResult
+    }
 
 
-	void testResetToEndOfActualInterval_WeeklyReset_Thursday() {
-		given:
-		DateTime dateToReset = new DateTime(2013, 9, 10, 13, 43, 12)
-		DateTime expectedResult = new DateTime(2013, 9, 13, 0, 0, 0, 0)
+    void "reset to end of actual interval with WeeklyReset on Thursday"() {
+        given:
+        DateTime dateToReset = new DateTime(2013, 9, 10, 13, 43, 12)
+        DateTime expectedResult = new DateTime(2013, 9, 13, 0, 0, 0, 0)
 
-		when:
-		DateTime currentResult = serviceUnderTest.resetToEndOfActualInterval(
-			dateToReset, CsiAggregationInterval.WEEKLY,
-			DateTimeConstants.THURSDAY)
+        when:
+        DateTime currentResult = serviceUnderTest.resetToEndOfActualInterval(dateToReset, WEEKLY, DateTimeConstants.THURSDAY)
 
-		then:
-		expectedResult == currentResult
-	}
+        then:
+        expectedResult == currentResult
+    }
 
 
-	void testFixTimeFrameToMatchIntervalRange_WeeklyInterval() {
-		given:
-		DateTime startDate = new DateTime(2013, 6, 19, 15, 55, 00)
-		DateTime endDate = new DateTime(2013, 9, 19, 15, 55, 00)
+    void "fixTimeFrameToMatchIntervalRange with WeeklyInterval"() {
+        given:
+        DateTime startDate = new DateTime(2013, 6, 19, 15, 55, 00)
+        DateTime endDate = new DateTime(2013, 9, 19, 15, 55, 00)
 
-		DateTime expectedFixedStartDate = new DateTime(2013, 6, 14, 0, 0, 0, 0)
-		DateTime expectedFixedEndDate = new DateTime(2013, 9, 13, 0, 0, 0, 0)
+        DateTime expectedFixedStartDate = new DateTime(2013, 6, 14, 0, 0, 0, 0)
+        DateTime expectedFixedEndDate = new DateTime(2013, 9, 13, 0, 0, 0, 0)
 
-		Interval timeFrame = new Interval(startDate, endDate)
+        Interval timeFrame = new Interval(startDate, endDate)
 
-		when:
-		Interval fixedTimeFrame = serviceUnderTest.fixTimeFrameToMatchIntervalRange(
-			timeFrame, CsiAggregationInterval.WEEKLY)
+        when:
+        Interval fixedTimeFrame = serviceUnderTest.fixTimeFrameToMatchIntervalRange(timeFrame, WEEKLY)
 
-		then:
-		fixedTimeFrame
-		expectedFixedStartDate == fixedTimeFrame.getStart()
-		expectedFixedEndDate == fixedTimeFrame.getEnd()
-	}
+        then:
+        fixedTimeFrame
+        expectedFixedStartDate == fixedTimeFrame.getStart()
+        expectedFixedEndDate == fixedTimeFrame.getEnd()
+    }
 
+    @Unroll
+    void "test adding intervals of Interval:#interval"() {
+        expect:
+        serviceUnderTest.addOneInterval(date, interval) == expectedResult
 
-	void "test adding hour intervals"(){
-		given: "a couple dates around end of summertime"
-		DateTime midnightLastDayOfSummertime = new DateTime(2013, 10, 27, 0, 0, 0)
-		DateTime twoHoursBeforeEndOfSummertime = midnightLastDayOfSummertime.plusHours(1)
-		DateTime oneHourBeforeEndOfSummertime = midnightLastDayOfSummertime.plusHours(2)
-		DateTime noonLastDayOfSummertime = new DateTime(2013, 10, 27, 12, 0, 0)
-		DateTime oneHourAfterNoonLastDayOfSummertime = noonLastDayOfSummertime.plusHours(1)
-		expect:
-		oneHourBeforeEndOfSummertime == serviceUnderTest.addOneInterval(twoHoursBeforeEndOfSummertime, CsiAggregationInterval.HOURLY)
-		oneHourBeforeEndOfSummertime.getHourOfDay() == serviceUnderTest.addOneInterval(oneHourBeforeEndOfSummertime, CsiAggregationInterval.HOURLY).getHourOfDay()
-		oneHourAfterNoonLastDayOfSummertime == serviceUnderTest.addOneInterval(noonLastDayOfSummertime, CsiAggregationInterval.HOURLY)
-	}
+        where:
+        date                            | interval || expectedResult
+        twoHoursBeforeEndOfSummertime   | HOURLY   || oneHourBeforeEndOfSummertime
+        tenDaysBeforeEndOfSummertime    | WEEKLY   || threeDaysBeforeEndOfSummertime
+        fourDaysAfterEndOfSummertime    | WEEKLY   || elevenDaysAfterEndOfSummertime
+        threeDaysBeforeEndOfSummertime  | DAILY    || twoDaysBeforeEndOfSummertime
+        midnightFirstDayAfterSummertime | DAILY    || midnightSecondDayAfterSummertime
+    }
 
-	void "test adding week intervals"(){
-		given: "a couple dates around summertime"
-		DateTime midnightLastDayOfSummertime = new DateTime(2013, 10, 27, 0, 0, 0)
-		DateTime threeDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(3)
-		DateTime tenDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(10)
-		DateTime fourDaysAfterEndOfSummertime = midnightLastDayOfSummertime.plusDays(4)
-		DateTime elevenDaysAfterEndOfSummertime = midnightLastDayOfSummertime.plusDays(11)
+    @Unroll
+    void "test adding hour interval and enter summertime"() {
+        expect:
+        serviceUnderTest.addOneInterval(date, interval) == expectedResult
 
-		expect:
-		threeDaysBeforeEndOfSummertime == serviceUnderTest.addOneInterval(tenDaysBeforeEndOfSummertime, CsiAggregationInterval.WEEKLY)
-		fourDaysAfterEndOfSummertime == serviceUnderTest.addOneInterval(threeDaysBeforeEndOfSummertime, CsiAggregationInterval.WEEKLY)
-		elevenDaysAfterEndOfSummertime == serviceUnderTest.addOneInterval(fourDaysAfterEndOfSummertime, CsiAggregationInterval.WEEKLY)
-	}
+        where:
+        date                           | interval || expectedResult
+        noonLastDayOfSummertime        | HOURLY   || oneHourAfterNoonLastDayOfSummertime
+        threeDaysBeforeEndOfSummertime | WEEKLY   || fourDaysAfterEndOfSummertime
+        midnightLastDayOfSummertime    | DAILY    || midnightFirstDayAfterSummertime
+    }
 
+    @Unroll
+    void "test subtracting intervals of Interval:#interval"() {
+        expect:
+        serviceUnderTest.subtractOneInterval(date, interval) == expectedResult
 
-	void "test adding day intervals"(){
-		given: "a couple dates around summertime"
-		DateTime midnightLastDayOfSummertime = new DateTime(2013, 10, 27, 0, 0, 0)
-		DateTime threeDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(3)
-		DateTime twoDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(2)
-		DateTime midnightFirstDayAfterSummertime = midnightLastDayOfSummertime.plusDays(1)
-		DateTime midnightSecondDayAfterSummertime = midnightLastDayOfSummertime.plusDays(2)
+        where:
+        date                             | interval || expectedResult
+        threeDaysBeforeEndOfSummertime   | WEEKLY   || tenDaysBeforeEndOfSummertime
+        elevenDaysAfterEndOfSummertime   | WEEKLY   || fourDaysAfterEndOfSummertime
+        twoDaysBeforeEndOfSummertime     | DAILY    || threeDaysBeforeEndOfSummertime
+        midnightSecondDayAfterSummertime | DAILY    || midnightFirstDayAfterSummertime
+        oneHourBeforeEndOfSummertime     | HOURLY   || twoHoursBeforeEndOfSummertime
+        oneHourAfterEndOfSummertime      | HOURLY   || exactlyEndOfSummertime
+    }
 
-		expect:
-		twoDaysBeforeEndOfSummertime == serviceUnderTest.addOneInterval(threeDaysBeforeEndOfSummertime, CsiAggregationInterval.DAILY)
-		midnightFirstDayAfterSummertime == serviceUnderTest.addOneInterval(midnightLastDayOfSummertime, CsiAggregationInterval.DAILY)
-		midnightSecondDayAfterSummertime == serviceUnderTest.addOneInterval(midnightFirstDayAfterSummertime, CsiAggregationInterval.DAILY)
-	}
+    @Unroll
+    void "test subtracting hour interval of Interval:#interval and leave summertime"() {
+        expect:
+        serviceUnderTest.subtractOneInterval(date, interval) == expectedResult
 
-	void "test subtracting week intervals"(){
-		given: "a couple days around end of summertime"
-		DateTime midnightLastDayOfSummertime = new DateTime(2013, 10, 27, 0, 0, 0)
-		DateTime threeDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(3)
-		DateTime tenDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(10)
-		DateTime fourDaysAfterEndOfSummertime = midnightLastDayOfSummertime.plusDays(4)
-		DateTime elevenDaysAfterEndOfSummertime = midnightLastDayOfSummertime.plusDays(11)
-
-		expect:
-		tenDaysBeforeEndOfSummertime == serviceUnderTest.subtractOneInterval(threeDaysBeforeEndOfSummertime, CsiAggregationInterval.WEEKLY)
-		threeDaysBeforeEndOfSummertime == serviceUnderTest.subtractOneInterval(fourDaysAfterEndOfSummertime, CsiAggregationInterval.WEEKLY)
-		fourDaysAfterEndOfSummertime == serviceUnderTest.subtractOneInterval(elevenDaysAfterEndOfSummertime, CsiAggregationInterval.WEEKLY)
-	}
-
-	void "test subtracting day intervals"(){
-		given: "a couple dates around end of summertime"
-		DateTime midnightLastDayOfSummertime = new DateTime(2013, 10, 27, 0, 0, 0)
-		DateTime threeDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(3)
-		DateTime twoDaysBeforeEndOfSummertime = midnightLastDayOfSummertime.minusDays(2)
-		DateTime midnightFirstDayAfterSummertime = midnightLastDayOfSummertime.plusDays(1)
-		DateTime midnightSecondDayAfterSummertime = midnightLastDayOfSummertime.plusDays(2)
-
-		expect:
-		threeDaysBeforeEndOfSummertime == serviceUnderTest.subtractOneInterval(twoDaysBeforeEndOfSummertime, CsiAggregationInterval.DAILY)
-		midnightLastDayOfSummertime == serviceUnderTest.subtractOneInterval(midnightFirstDayAfterSummertime, CsiAggregationInterval.DAILY)
-		midnightFirstDayAfterSummertime == serviceUnderTest.subtractOneInterval(midnightSecondDayAfterSummertime, CsiAggregationInterval.DAILY)
-	}
-
-	void "test subtracting hours"(){
-		given: "a couple dates around end of summertime"
-		DateTime midnightLastDayOfSummertime = new DateTime(2013, 10, 27, 0, 0, 0)
-		DateTime twoHoursBeforeEndOfSummertime = midnightLastDayOfSummertime.plusHours(1)
-		DateTime oneHourBeforeEndOfSummertime = midnightLastDayOfSummertime.plusHours(2)
-		DateTime exactlyEndOfSummertime = midnightLastDayOfSummertime.plusHours(3)
-		DateTime oneHourAfterEndOfSummertime = midnightLastDayOfSummertime.plusHours(4)
-
-		expect:
-//		subtracting an hour
-		twoHoursBeforeEndOfSummertime == serviceUnderTest.subtractOneInterval(oneHourBeforeEndOfSummertime, CsiAggregationInterval.HOURLY)
-		oneHourBeforeEndOfSummertime.getHourOfDay() == serviceUnderTest.subtractOneInterval(exactlyEndOfSummertime, CsiAggregationInterval.HOURLY).getHourOfDay()
-		exactlyEndOfSummertime.getHourOfDay() == serviceUnderTest.subtractOneInterval(oneHourAfterEndOfSummertime, CsiAggregationInterval.HOURLY).getHourOfDay()
-	}
-
-
+        where:
+        date                            | interval || expectedResult
+        fourDaysAfterEndOfSummertime    | WEEKLY   || threeDaysBeforeEndOfSummertime
+        midnightFirstDayAfterSummertime | DAILY    || midnightLastDayOfSummertime
+        exactlyEndOfSummertime          | HOURLY   || oneHourBeforeEndOfSummertime
+    }
 
     void testIsInActualInterval() {
-		given:
-        Integer hourly = CsiAggregationInterval.HOURLY
-        Integer daily = CsiAggregationInterval.DAILY
-        Integer weekly = CsiAggregationInterval.WEEKLY
+        given:
         DateTime now = new DateTime()
 
         expect:
-        serviceUnderTest.isInActualInterval(now, hourly)
-        !serviceUnderTest.isInActualInterval(now.minusMinutes(hourly + 1), hourly)
-        !serviceUnderTest.isInActualInterval(now.minusMinutes(daily), hourly)
-        !serviceUnderTest.isInActualInterval(now.minusMinutes(weekly), hourly)
+        serviceUnderTest.isInActualInterval(now, HOURLY)
+        !serviceUnderTest.isInActualInterval(now.minusMinutes(HOURLY + 1), HOURLY)
+        !serviceUnderTest.isInActualInterval(now.minusMinutes(DAILY), HOURLY)
+        !serviceUnderTest.isInActualInterval(now.minusMinutes(WEEKLY), HOURLY)
 
-        serviceUnderTest.isInActualInterval(now, daily)
-        !serviceUnderTest.isInActualInterval(now.minusMinutes(daily + 1), daily)
-        !serviceUnderTest.isInActualInterval(now.minusMinutes(weekly), daily)
+        serviceUnderTest.isInActualInterval(now, DAILY)
+        !serviceUnderTest.isInActualInterval(now.minusMinutes(DAILY + 1), DAILY)
+        !serviceUnderTest.isInActualInterval(now.minusMinutes(WEEKLY), DAILY)
 
-        serviceUnderTest.isInActualInterval(now, weekly)
-        !serviceUnderTest.isInActualInterval(now.minusMinutes(weekly + 1), weekly)
+        serviceUnderTest.isInActualInterval(now, WEEKLY)
+        !serviceUnderTest.isInActualInterval(now.minusMinutes(WEEKLY + 1), WEEKLY)
 
     }
 }
