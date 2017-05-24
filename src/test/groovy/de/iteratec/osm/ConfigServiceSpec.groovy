ed88666a72ca600bd7e4cd8b6138392dbd725d97
Daniel Klisch <daniel.klisch@iteratec.de>
2017-05-24 11:18:43 +0200

    Added test in ConfigServiceSpec

--- src/test/groovy/de/iteratec/osm/ConfigServiceSpec.groovy
+++ src/test/groovy/de/iteratec/osm/ConfigServiceSpec.groovy
@@ -29,48 +29,59 @@ import spock.lang.Specification
 @Build([OsmConfiguration])
 @Mock([OsmConfiguration])
 class ConfigServiceSpec extends Specification {
-	static final Integer DEFAULT_MAX_DOWNLOAD_TIME_IN_MINUTES = 60
-	static final Integer DEFAULT_MIN_DOCCOMPLETE_TIME_IN_MILLISECS = 250
-	static final Integer DEFAULT_MAX_DOCCOMPLETE_TIME_IN_MILLISECS = 180000
-	static final Integer DEFAULT_INITIAL_CHART_HEIGHT_IN_PIXELS = 400
+    static final Integer DEFAULT_MAX_DOWNLOAD_TIME_IN_MINUTES = 60
+    static final Integer DEFAULT_MIN_DOCCOMPLETE_TIME_IN_MILLISECS = 250
+    static final Integer DEFAULT_MAX_DOCCOMPLETE_TIME_IN_MILLISECS = 180000
+    static final Integer DEFAULT_INITIAL_CHART_HEIGHT_IN_PIXELS = 400
 
-	void "one config is fine"(){
-		when: "only one config has been saved with a value"
-		OsmConfiguration.build(detailDataStorageTimeInWeeks: 4)
+    void "one config is fine"() {
+        when: "only one config has been saved with a value"
+        OsmConfiguration.build(detailDataStorageTimeInWeeks: 4)
 
-		then: "config service hat its values including the defaults"
-		service.getDetailDataStorageTimeInWeeks() == 4
-		service.getDefaultMaxDownloadTimeInMinutes() == DEFAULT_MAX_DOWNLOAD_TIME_IN_MINUTES
-		service.getMinDocCompleteTimeInMillisecs() == DEFAULT_MIN_DOCCOMPLETE_TIME_IN_MILLISECS
-		service.getMaxDocCompleteTimeInMillisecs() == DEFAULT_MAX_DOCCOMPLETE_TIME_IN_MILLISECS
-		service.getInitialChartHeightInPixels() == DEFAULT_INITIAL_CHART_HEIGHT_IN_PIXELS
-	}
+        then: "config service hat its values including the defaults"
+        service.getDetailDataStorageTimeInWeeks() == 4
+        service.getDefaultMaxDownloadTimeInMinutes() == DEFAULT_MAX_DOWNLOAD_TIME_IN_MINUTES
+        service.getMinDocCompleteTimeInMillisecs() == DEFAULT_MIN_DOCCOMPLETE_TIME_IN_MILLISECS
+        service.getMaxDocCompleteTimeInMillisecs() == DEFAULT_MAX_DOCCOMPLETE_TIME_IN_MILLISECS
+        service.getInitialChartHeightInPixels() == DEFAULT_INITIAL_CHART_HEIGHT_IN_PIXELS
+    }
 
-	void "more than one config is bad"(){
-		given: "two configs have been saved"
-		2.times {OsmConfiguration.build()}
+    void "more than one config is bad"() {
+        given: "two configs have been saved"
+        2.times { OsmConfiguration.build() }
 
-		when: "trying to access any value from config service"
-		service.getDetailDataStorageTimeInWeeks()
+        when: "trying to access any value from config service"
+        service.getDetailDataStorageTimeInWeeks()
 
-		then: "an illegalStateExpection should be thrown"
-		thrown(IllegalStateException)
-	}
+        then: "an illegalStateExpection should be thrown"
+        thrown(IllegalStateException)
+    }
 
-	void "values in config can be changed"(int from, int to){
-		given: "value in config has been set"
-		OsmConfiguration conf = OsmConfiguration.build(detailDataStorageTimeInWeeks: from)
+    void "values in config can be changed"(int from, int to) {
+        given: "value in config has been set"
+        OsmConfiguration conf = OsmConfiguration.build(detailDataStorageTimeInWeeks: from)
 
-		when: "value is changed in config"
-		conf.detailDataStorageTimeInWeeks = to
+        when: "value is changed in config"
+        conf.detailDataStorageTimeInWeeks = to
 
-		then: "config service has the new value"
-		service.getDetailDataStorageTimeInWeeks() == to
+        then: "config service has the new value"
+        service.getDetailDataStorageTimeInWeeks() == to
 
-		where:
-		from | to
-		1    | 2
-		3    | 4
-		5    | 1
-	}
+        where:
+        from | to
+        1    | 2
+        3    | 4
+        5    | 1
+    }
+
+    void "Failure with no config"() {
+        given: "no config"
+
+        when: "getting DetailDataStorageTimeInWeeks"
+        service.getDetailDataStorageTimeInWeeks()
+
+        then: "should fail with IllegalStateException"
+        thrown(IllegalStateException)
+    }
+
 }
