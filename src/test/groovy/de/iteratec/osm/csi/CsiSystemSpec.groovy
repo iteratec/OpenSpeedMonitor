33e9d7ffbc48382798ccb3e8fc5e31802a492218
Stefan Burnicki <stefan.burnicki@iteratec.de>
2017-06-12 11:10:40 +0200

    [IT-1701] Simplified CsiSystemSpec

--- src/test/groovy/de/iteratec/osm/csi/CsiSystemSpec.groovy
+++ src/test/groovy/de/iteratec/osm/csi/CsiSystemSpec.groovy
@@ -1,25 +1,15 @@
 package de.iteratec.osm.csi
 
 import de.iteratec.osm.measurement.schedule.JobGroup
+import grails.buildtestdata.mixin.Build
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 @TestFor(CsiSystem)
+@Build([JobGroup, CsiConfiguration])
 @Mock([JobGroup, CsiConfiguration, CsiDay, JobGroupWeight])
 class CsiSystemSpec extends Specification {
-
-    JobGroupWeight weight1
-    JobGroupWeight weight2
-
-
-    void "setup" () {
-        CsiConfiguration csiConfiguration = new CsiConfiguration(label: "a csi configuration", csiDay: new CsiDay())
-        JobGroup csiJobGroup1 = new JobGroup(name: "csiGroup 1", csiConfiguration: csiConfiguration)
-        JobGroup csiJobGroup2 = new JobGroup(name: "csiGroup 2", csiConfiguration: csiConfiguration)
-        weight1 = new JobGroupWeight(jobGroup: csiJobGroup1, weight: 5.5)
-        weight2 = new JobGroupWeight(jobGroup: csiJobGroup2, weight: 10.7)
-    }
 
     void "test empty CsiSystem is invalid"() {
         when: "an empty CsiSystem is created"
@@ -31,9 +21,10 @@ class CsiSystemSpec extends Specification {
 
     void "test a valid csiSystem"() {
         when: "a valid CsiSystem is created"
+            CsiConfiguration csiConfiguration = CsiConfiguration.build()
             CsiSystem system = new CsiSystem(label: "csiSystem")
-            system.addToJobGroupWeights(weight1)
-            system.addToJobGroupWeights(weight2)
+            system.addToJobGroupWeights(jobGroup: JobGroup.build(csiConfiguration: csiConfiguration), weight: 5.5)
+            system.addToJobGroupWeights(jobGroup: JobGroup.build(csiConfiguration: csiConfiguration), weight: 10.7)
 
         then: "it validates"
             system.validate()
@@ -42,7 +33,7 @@ class CsiSystemSpec extends Specification {
     void "test csiSystem contains only one jobGroupWeight then system is invalid"() {
         when: "a CsiSystem  with only one jobGroupWeight"
             CsiSystem system = new CsiSystem(label: "csiSytesm")
-            system.addToJobGroupWeights(weight1)
+            system.addToJobGroupWeights(jobGroup: JobGroup.build(csiConfiguration: CsiConfiguration.build()), weight: 5.5)
 
         then: "it does not validate"
             !system.validate()
