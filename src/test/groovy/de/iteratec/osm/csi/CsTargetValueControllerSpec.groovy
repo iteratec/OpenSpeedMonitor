1dec4994ad6898f1ecd891eba0fdb4389cb2c845
Stefan Burnicki <stefan.burnicki@iteratec.de>
2017-06-12 11:42:13 +0200

    [IT-1701] Cleanup of CsTargetValueControllerSpec

--- src/test/groovy/de/iteratec/osm/csi/CsTargetValueControllerSpec.groovy
+++ src/test/groovy/de/iteratec/osm/csi/CsTargetValueControllerSpec.groovy
@@ -2,6 +2,7 @@ package de.iteratec.osm.csi
 
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
+import org.joda.time.DateTime
 import spock.lang.Specification
 
 @TestFor(CsTargetValueController)
@@ -10,28 +11,30 @@ class CsTargetValueControllerSpec extends Specification {
 
     void "test creating a csTargetValue without any params"() {
         when: "nothing is in params"
-            request.method = 'POST'
-            controller.save()
+        request.method = 'POST'
+        controller.save()
 
         then: "response with errors"
-            !model.csTargetValue.validate()
-            CsTargetValue.count() == 0
+        !model.csTargetValue.validate()
+        CsTargetValue.count() == 0
     }
 
-    void "test creating a csTargetValue with valid params"() {
+    void "test creating a csTargetValue with valid params"(Object dateParam, DateTime expectedDate, double csInPercent) {
         when: "there are valid params"
-            request.method = 'POST'
-            params.date = date
-            params.csInPercent = csInPercent
-            controller.save()
+        request.method = 'POST'
+        params.date = dateParam
+        params.csInPercent = csInPercent
+        controller.save()
 
         then: "csTargetValue is created"
-            model.csTargetValue.validate()
-            CsTargetValue.count() == 1
+        model.csTargetValue.validate()
+        List<CsTargetValue> targetValues = CsTargetValue.list()
+        targetValues*.csInPercent == [csInPercent]
+        targetValues*.date == [expectedDate.toDate()]
 
         where:
-            date         | csInPercent
-            "05-05-2015" | 42
-            new Date()   | 42
+        dateParam                                 | expectedDate                   | csInPercent
+        "2015-05-05"                              | new DateTime(2015, 5, 5, 0, 0) | 42
+        new DateTime(2015, 5, 5, 0, 0).toDate() | new DateTime(2015, 5, 5, 0, 0) | 42
     }
 }
