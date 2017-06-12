3dc097430661bf6f76f23dec74ed2d0f742e5092
Stefan Burnicki <stefan.burnicki@iteratec.de>
2017-06-12 11:52:36 +0200

    [IT-1701] Cleanup of JobGroupWeightSpec

--- src/test/groovy/de/iteratec/osm/csi/JobGroupWeightSpec.groovy
+++ src/test/groovy/de/iteratec/osm/csi/JobGroupWeightSpec.groovy
@@ -1,23 +1,19 @@
 package de.iteratec.osm.csi
 
 import de.iteratec.osm.measurement.schedule.JobGroup
+import grails.buildtestdata.mixin.Build
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 @TestFor(JobGroupWeight)
+@Build([JobGroup])
 @Mock([JobGroup])
 class JobGroupWeightSpec extends Specification {
 
-    JobGroup jobGroup
-
-    void "setup" () {
-        jobGroup = new JobGroup(name: "jobGroup")
-    }
-
     void "test jobGroupWeight has to have a jobGroup with csiConfiguration"() {
         when: "creating a jobGroupWeight with a jobGroup without a csiConfiguration"
-        JobGroupWeight jobGroupWeight = new JobGroupWeight(jobGroup: jobGroup, weight: 12.0)
+        JobGroupWeight jobGroupWeight = new JobGroupWeight(jobGroup: JobGroup.build(), weight: 12.0)
 
         then: "the jobGroupWeight does not validate"
         !jobGroupWeight.validate()
