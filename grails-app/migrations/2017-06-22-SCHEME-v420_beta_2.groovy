338c940c0a3eb59196a5da8fafdce95ed132af60
Johannes Weiss <johannes.weiss@iteratec.de>
2017-06-22 15:05:30 +0200

    [IT-36] WIP: Remove not null constraint on column 'aggregator_id' of table 'csi_aggregation'

--- grails-app/migrations/2017-06-22-SCHEME-v420_beta_2.groovy
+++ grails-app/migrations/2017-06-22-SCHEME-v420_beta_2.groovy
@@ -2,4 +2,7 @@ databaseChangeLog = {
     changeSet(author: "jweiss", id: "1498121471-1") {
         addNotNullConstraint(columnDataType: "varchar(255)", columnName: "aggregation_type", tableName: "csi_aggregation")
     }
+    changeSet(author: "jweiss", id: "1498121471-2") {
+        dropNotNullConstraint(columnDataType: "int", columnName: "aggregator_id", tableName: "csi_aggregation")
+    }
 }
