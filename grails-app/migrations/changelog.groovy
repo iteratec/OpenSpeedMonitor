aad81a8dd2b0b774391f6366ee85dfad91ed6736
Stefan Burnicki <stefan.burnicki@iteratec.de>
2017-07-19 18:05:52 +0200

    Added changeset to changelog; consolidated chengeset for speed

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -42,6 +42,7 @@ databaseChangeLog = {
     include file: '2017-06-13-DATA-v420_beta.groovy'
     include file: '2017-06-16-SCHEME-v420_beta_2.groovy'
     include file: '2017-06-21-SCHEME-v420_beta_2.groovy'
+    include file: '2017-06-21-DATA-v420_beta_2.groovy'
     include file: '2017-06-22-SCHEME-v420_beta_2.groovy'
     include file: '2017-07-04-SCHEME-v420_beta_2.groovy'
     include file: '2017-07-04-DATA-v420_beta_2.groovy'
