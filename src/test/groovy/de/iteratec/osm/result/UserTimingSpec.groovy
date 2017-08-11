031aae394a20f66bd673fab7fd7e38135005f11b
mwg <moritz.wegmann@iteratec.de>
2017-08-11 15:18:49 +0200

    [IT-1335] fixes according to review [CR-101]

--- src/test/groovy/de/iteratec/osm/result/UserTimingSpec.groovy
+++ src/test/groovy/de/iteratec/osm/result/UserTimingSpec.groovy
@@ -9,7 +9,7 @@ import spock.lang.Specification
 class UserTimingSpec extends Specification {
 
 
-    void "test contraint for duration"(duration, userTimingType, expectedResult) {
+    void "test contraints for duration"(duration, userTimingType, expectedResult) {
         when:"userTiming is created"
         UserTiming testee = UserTiming.buildWithoutSave(duration: duration, type: userTimingType)
 
