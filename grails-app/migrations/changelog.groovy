5d0373ca7e430d8b2d23252b9b43efe18f750c9c
Nils Kuhn <nils.kuhn@iteratec.de>
2017-09-01 16:03:28 +0200

    renamed changeset

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -50,5 +50,5 @@ databaseChangeLog = {
     include file: '2017-07-10-SCHEME-v420_beta_2.groovy'
     include file: '2017-07-12-SCHEME-v420_beta_2.groovy'
     include file: '2017-07-18-DATA-v420_beta_2.groovy'
-    include file: '2017-08-31-DATA-v431.groovy'
+    include file: '2017-08-31-DATA-v430.groovy'
 }
