fe2e8bc567b0f196cf5cbe7cbbf59a3b6667aa98
mwg <moritz.wegmann@iteratec.de>
2017-09-11 11:47:51 +0200

    [IT-1848] enable browser delete as well as improved expection handling

--- grails-app/controllers/de/iteratec/osm/measurement/environment/BrowserController.groovy
+++ grails-app/controllers/de/iteratec/osm/measurement/environment/BrowserController.groovy
@@ -1,6 +1,9 @@
 package de.iteratec.osm.measurement.environment
 
+import de.iteratec.osm.result.EventResult
 import de.iteratec.osm.util.ControllerUtils
+import de.iteratec.osm.util.DataIntegrityViolationExpectionUtil
+import de.iteratec.osm.util.DataIntegrityViolationUtil
 import grails.converters.JSON
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpStatus
@@ -8,6 +11,7 @@ import org.springframework.http.HttpStatus
 import javax.servlet.http.HttpServletResponse
 
 import static org.springframework.http.HttpStatus.*
+
 //TODO: This controller was generated due to a scaffolding bug (https://github.com/grails3-plugins/scaffolding/issues/24). The dynamically scaffolded controllers cannot handle database exceptions
 
 class BrowserController {
@@ -16,7 +20,7 @@ class BrowserController {
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
     def index() {
-        
+
     }
 
     def show(Browser browser) {
@@ -29,18 +33,18 @@ class BrowserController {
 
     def save(Browser browser) {
         if (browser == null) {
-            
+
             notFound()
             return
         }
 
         if (browser.hasErrors()) {
 
-            respond browser.errors, view:'create'
+            respond browser.errors, view: 'create'
             return
         }
 
-        browser.save flush:true
+        browser.save flush: true
 
         request.withFormat {
             form multipartForm {
@@ -64,30 +68,53 @@ class BrowserController {
 
         if (browser.hasErrors()) {
 
-            respond browser.errors, view:'edit'
+            respond browser.errors, view: 'edit'
             return
         }
 
-        browser.save flush:true
+        browser.save flush: true
 
         request.withFormat {
             form multipartForm {
                 flash.message = message(code: 'default.updated.message', args: [message(code: 'browser.label', default: 'Browser'), browser.id])
                 redirect browser
             }
-            '*'{ respond browser, [status: OK] }
+            '*' { respond browser, [status: OK] }
         }
     }
 
-    def updateTable(){
+    def delete(Browser browser) {
+
+        if (browser == null) {
+            notFound()
+            return
+        }
+
+        try {
+            browser.delete(flush: true)
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'browser.label', default: 'Browser'), params.id])
+            redirect(action: "index")
+        }
+        catch (DataIntegrityViolationException e) {
+            String dependency = DataIntegrityViolationExpectionUtil.getEntityNameForForeignKeyViolation(e)
+            if(dependency){
+                flash.message = message(code: 'default.not.deleted.foreignKeyConstraint.message', args: [message(code: 'browser.label', default: 'Browser'), params.id, dependency])
+            }else{
+                flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'browser.label', default: 'Browser'), params.id])
+            }
+            redirect(action: "index")
+        }
+    }
+
+    def updateTable() {
         params.order = params.order ? params.order : "asc"
         params.sort = params.sort ? params.sort : "name"
-        params.sort = params.sort == "browserAliases"? "name" : params.sort
+        params.sort = params.sort == "browserAliases" ? "name" : params.sort
         params.max = params.max as Integer
         params.offset = params.offset as Integer
 
         List<Browser> result = Browser.createCriteria().list(params) {
-            if(params.filter)ilike("name","%"+params.filter+"%")
+            if (params.filter) ilike("name", "%" + params.filter + "%")
         }
         String templateAsPlainText = g.render(
                 template: 'browserTable',
@@ -105,7 +132,12 @@ class BrowserController {
                 flash.message = message(code: 'default.not.found.message', args: [message(code: 'browser.label', default: 'Browser'), params.id])
                 redirect action: "index", method: "GET"
             }
-            '*'{ render status: NOT_FOUND }
+            '*' { render status: NOT_FOUND }
         }
+    }
+
+    private static String toCamelCase( String text ) {
+        text = text.replaceAll( "(_)([A-Za-z0-9])", { Object[] it -> it[2].toUpperCase() } )
+        return text.capitalize()
     }
 }
