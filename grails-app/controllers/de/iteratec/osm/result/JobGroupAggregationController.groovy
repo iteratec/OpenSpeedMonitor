0caf001c2e1984ff5f27f4b3987a0891836c6736
mwg <moritz.wegmann@iteratec.de>
2017-09-20 15:28:36 +0200

    [IT-1335] & [CR-178] remove unnecessary code

--- grails-app/controllers/de/iteratec/osm/result/JobGroupAggregationController.groovy
+++ grails-app/controllers/de/iteratec/osm/result/JobGroupAggregationController.groovy
@@ -80,8 +80,6 @@ class JobGroupAggregationController extends ExceptionHandlerController {
 
         JobGroupAggregationChartDTO jobGroupAggregationChartDTO = new JobGroupAggregationChartDTO()
 
-        EventResult
-
         //Label translations
         jobGroupAggregationChartDTO.i18nMap.put("measurand", i18nService.msg("de.iteratec.result.measurand.label", "Measurand"))
         jobGroupAggregationChartDTO.i18nMap.put("jobGroup", i18nService.msg("de.iteratec.isr.wptrd.labels.filterFolder", "JobGroup"))
