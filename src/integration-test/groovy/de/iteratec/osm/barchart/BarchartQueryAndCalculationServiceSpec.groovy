17a484915f1130fb53251aac943534c4cd691501
Philipp Albrecht <philipp.albrecht@iteratec.de>
2017-10-24 19:56:09 +0200

    [IT-1847] improve test coverage for median calculation

--- src/integration-test/groovy/de/iteratec/osm/barchart/BarchartQueryAndCalculationServiceSpec.groovy
+++ src/integration-test/groovy/de/iteratec/osm/barchart/BarchartQueryAndCalculationServiceSpec.groovy
@@ -18,20 +18,72 @@ import spock.lang.Unroll
 @Unroll
 class BarchartQueryAndCalculationServiceSpec extends NonTransactionalIntegrationSpec {
 
-    BarchartQueryAndCalculationService barchartMedianService
+    BarchartQueryAndCalculationService barchartQueryAndCalculationService
 
     private SelectedMeasurand measurand
     private SelectedMeasurand userTimingMeasurand
-    private Page page_one
-    private Page page_two
-    private JobGroup jobGroup_one
-    private JobGroup jobGroup_two
+    private Page page_one, page_two
+    private JobGroup jobGroup_one, jobGroup_two, jobGroup_three
 
     def setup() {
         OsmConfiguration.build()
         buildTestData()
         measurand = new SelectedMeasurand(name: "DOC_COMPLETE_TIME", cachedView: CachedView.UNCACHED, selectedType: SelectedMeasurandType.MEASURAND)
         userTimingMeasurand = new SelectedMeasurand(name: "mapsapi_apiboot2_firstmap", cachedView: CachedView.UNCACHED, selectedType: SelectedMeasurandType.USERTIMING_MARK)
+    }
+
+    void "Test median for measurand and usertimings"() {
+        when: "Median service is called with query parameters"
+        def medians = barchartQueryAndCalculationService.getMediansFor(
+                [jobGroup_one, jobGroup_two],
+                [page_one, page_two],
+                yesterday(),
+                tomorrow(),
+                [measurand, userTimingMeasurand]
+        )
+        then: "Get calculated medians in EventResultProjections"
+        medians[0].projectedProperties.get(measurand.getDatabaseRelevantName()) == 1500
+        medians[0].projectedProperties.get(userTimingMeasurand.getDatabaseRelevantName()) == 2000
+        medians[1].projectedProperties.get(measurand.getDatabaseRelevantName()) == 1750
+        medians[2].projectedProperties.get(measurand.getDatabaseRelevantName()) == 3000
+    }
+
+    void "Test median for jobGroups only"() {
+        when: "Median service is called with query parameters"
+        def medians = barchartQueryAndCalculationService.getMediansFor(
+                [jobGroup_one, jobGroup_two],
+                [],
+                yesterday(),
+                tomorrow(),
+                [measurand]
+        )
+        then: "Get medians for jobGroupAggregation"
+        medians[0].projectedProperties.get(measurand.getDatabaseRelevantName()) == 1500
+        medians[1].projectedProperties.get(measurand.getDatabaseRelevantName()) == 2000
+    }
+
+    void "Test median for jobGroups with no results"() {
+        when: "Median service is called with query parameters"
+        def medians = barchartQueryAndCalculationService.getMediansFor(
+                [jobGroup_three],
+                [],
+                yesterday(),
+                tomorrow(),
+                [measurand]
+        )
+        then: "Get no EventResultProjections since jobGroup is empty"
+        medians.size() == 0
+    }
+
+    private buildTestJobGroups() {
+        jobGroup_one = JobGroup.build(name: "test job one")
+        jobGroup_two = JobGroup.build(name: "test job two")
+        jobGroup_three = JobGroup.build(name: "test job three")
+    }
+
+    private buildTestPages() {
+        page_one = Page.build(name: "test page one")
+        page_two = Page.build(name: "test page two")
     }
 
     private Date yesterday() {
@@ -44,31 +96,6 @@ class BarchartQueryAndCalculationServiceSpec extends NonTransactionalIntegration
         final Calendar cal = Calendar.getInstance()
         cal.add(Calendar.DATE, +1)
         return cal.getTime()
-    }
-
-    void "Test median for measurand"() {
-        when: "Median service is called with query parameters"
-        def medians = barchartMedianService.getMediansFor(
-                [jobGroup_one, jobGroup_two],
-                [page_one, page_two],
-                yesterday(),
-                tomorrow(),
-                [measurand, userTimingMeasurand]
-        )
-        then: "Get calculated medians in EventResultProjections"
-        medians[0].projectedProperties.get(measurand.getDatabaseRelevantName()) == 1500
-        medians[0].projectedProperties.get(userTimingMeasurand.getDatabaseRelevantName()) == 2000
-        medians[1].projectedProperties.get(measurand.getDatabaseRelevantName()) == 1750
-    }
-
-    private buildTestJobGroups() {
-        jobGroup_one = JobGroup.build(name: "test job one")
-        jobGroup_two = JobGroup.build(name: "test job two")
-    }
-
-    private buildTestPages() {
-        page_one = Page.build(name: "test page one")
-        page_two = Page.build(name: "test page two")
     }
 
     private buildEventResultsWithUserTimings() {
@@ -104,6 +131,13 @@ class BarchartQueryAndCalculationServiceSpec extends NonTransactionalIntegration
                 ])
     }
 
+    private buildTestData() {
+        buildTestJobGroups()
+        buildTestPages()
+        buildEventResultsWithUserTimings()
+        buildEventResultsWithoutUserTimings()
+    }
+
     private buildEventResultsWithoutUserTimings() {
         EventResult.build(docCompleteTimeInMillisecs: 1000,
                 fullyLoadedTimeInMillisecs: 5000,
@@ -135,12 +169,21 @@ class BarchartQueryAndCalculationServiceSpec extends NonTransactionalIntegration
                 page: page_two,
                 jobGroup: jobGroup_two,
                 cachedView: CachedView.UNCACHED)
-    }
 
-    private buildTestData() {
-        buildTestJobGroups()
-        buildTestPages()
-        buildEventResultsWithUserTimings()
-        buildEventResultsWithoutUserTimings()
+        EventResult.build(docCompleteTimeInMillisecs: 3000,
+                fullyLoadedTimeInMillisecs: 5000,
+                page: page_one,
+                jobGroup: jobGroup_two,
+                cachedView: CachedView.UNCACHED)
+        EventResult.build(docCompleteTimeInMillisecs: null,
+                fullyLoadedTimeInMillisecs: 5000,
+                page: page_one,
+                jobGroup: jobGroup_two,
+                cachedView: CachedView.UNCACHED)
+        EventResult.build(docCompleteTimeInMillisecs: null,
+                fullyLoadedTimeInMillisecs: 5000,
+                page: page_one,
+                jobGroup: jobGroup_two,
+                cachedView: CachedView.UNCACHED)
     }
 }
