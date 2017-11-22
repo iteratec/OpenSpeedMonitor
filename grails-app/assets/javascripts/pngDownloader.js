9044aed3dacf415aa0534001f940c9af4629dd1f
Fabian Sobanski <fabian.sobanski@iteratec.de>
2017-11-22 18:37:51 +0100

    [IT-1616] Add resize functionality for download dialog
    I still need to add a nice visual indication for the resize handle.

---
+++ grails-app/assets/javascripts/pngDownloader.js
@@ -0,0 +1,83 @@
+function initPngDownloadModal() {
+    var downloadContainer = document.getElementById("download-chart-container");
+    var originalContainer = document.getElementById("svg-container");
+    var resizeHandle = document.getElementById("resize-east");
+    var modalDialog = document.getElementById("downloadAsPngDialog");
+    var widthField = document.getElementById("pngWidth");
+    var svgElement = originalContainer.getElementsByTagName("svg")[0];
+    var placeholder = svgElement.cloneNode(true);
+    var width = originalContainer.offsetWidth;
+    var sideMargin = OpenSpeedMonitor.ChartComponents ? OpenSpeedMonitor.ChartComponents.common.ComponentMargin : 3;
+    var minWidthEstimate = 200 + 2 * sideMargin;
+    var startX;
+    var origCursor = document.body.style.cursor;
+    var origSvgCursor = svgElement.style.cursor;
+
+    placeholder.id = "svg-placeholder";
+    downloadContainer.appendChild(svgElement);
+    originalContainer.appendChild(placeholder);
+
+    function getMaxWidth() {
+        //We need to encapsulated this inside a function and can't store it in a variable because the modalDialog
+        //is hidden at the beginning when initPngDownloadModal() executes and it therefore has width 0.
+        return modalDialog.offsetWidth - sideMargin -
+            parseInt(window.getComputedStyle(resizeHandle).marginLeft.slice(0, -2));
+    }
+
+    function getValidatedWith(width) {
+        return Math.min(Math.max(width, minWidthEstimate), getMaxWidth())
+    }
+
+    function setContainerWidth(containerWidth) {
+        downloadContainer.style.width = getValidatedWith(containerWidth) + "px";
+        widthField.value = downloadContainer.offsetWidth;
+        window.dispatchEvent(new Event("resize"));
+    }
+
+    function reactToModalDialogResize() {
+        if (downloadContainer.offsetWidth > getMaxWidth()) {
+            setContainerWidth(0);
+            widthField.max = getMaxWidth();
+        }
+    }
+
+    function doDrag(e) {
+        setContainerWidth(width + e.clientX - startX);
+    }
+
+    function stopDrag() {
+        document.documentElement.removeEventListener("mousemove", doDrag);
+        document.documentElement.removeEventListener("mouseup", stopDrag);
+        document.body.style.cursor = origCursor;
+        svgElement.style.cursor = origSvgCursor;
+    }
+
+    function initDrag(e) {
+        document.body.style.cursor = svgElement.style.cursor = "e-resize";
+        startX = e.clientX;
+        width = downloadContainer.offsetWidth;
+        document.documentElement.addEventListener("mousemove", doDrag);
+        document.documentElement.addEventListener("mouseup", stopDrag);
+    }
+
+    function onWidthFieldInput() {
+        setContainerWidth(widthField.value);
+    }
+
+    downloadContainer.style.width = width + "px";
+    widthField.value = width;
+    widthField.min = minWidthEstimate;
+    widthField.max = getMaxWidth();
+    window.addEventListener("resize", reactToModalDialogResize);
+    resizeHandle.addEventListener("mousedown", initDrag);
+    widthField.addEventListener("change", onWidthFieldInput);
+
+    $("#downloadAsPngModal").on("hide.bs.modal", function restoreSvg() {
+        window.removeEventListener("resize", reactToModalDialogResize);
+        widthField.removeEventListener("onchange", setContainerWidth);
+        originalContainer.removeChild(placeholder);
+        originalContainer.appendChild(svgElement);
+        window.dispatchEvent(new Event("resize"));
+        $("#downloadAsPngModal").off("hide.bs.modal", restoreSvg);
+    });
+}
