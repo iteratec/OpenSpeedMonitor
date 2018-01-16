c08d06e530c704ef4c6c06bc3920506098140a6e
owe <ole.wellnitz@iteratec.de>
2018-01-16 16:58:45 +0100

    [IT-1853] removed unnecessary code from CiPipeService

--- grails-app/services/de/iteratec/osm/integrations/CiPipeService.groovy
+++ grails-app/services/de/iteratec/osm/integrations/CiPipeService.groovy
@@ -6,30 +6,10 @@ import grails.transaction.Transactional
 @Transactional
 class CiPipeService {
 
-    /**
-     *
-     * @param job
-     * @return
-     */
     String getCiIntegrationScriptFor(Job job) {
 
         return this.class.classLoader.getResourceAsStream('OsmCiPipeCheck.groovy.template').text
             .replace('{{jobId}}', String.valueOf(job.ident()))
             .replace('{{wptServerBaseUrl}}', job?.location?.wptServer?.baseUrl)
-
-//        new BufferedReader(new InputStreamReader(templateStream))
-//
-//        return
-//
-//        Template template = new StreamingTemplateEngine().createTemplate(
-//            new BufferedReader(new InputStreamReader(templateStream))
-//        )
-//        return template.make(
-//            [
-//                jobId: job.ident(),
-//                wptServerBaseUrl: job?.location?.wptServer?.baseUrl
-//            ].withDefault{ key -> return "\${${key}}" }
-//        )
     }
-
 }
