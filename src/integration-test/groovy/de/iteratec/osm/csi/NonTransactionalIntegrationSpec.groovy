691147391e3e6ef95e4ee69836184b5afa6c0994
Fabian Sobanski <fabian.sobanski@iteratec.de>
2018-05-07 11:44:59 +0200

    [IT-1681] WIP

--- src/integration-test/groovy/de/iteratec/osm/csi/NonTransactionalIntegrationSpec.groovy
+++ src/integration-test/groovy/de/iteratec/osm/csi/NonTransactionalIntegrationSpec.groovy
@@ -1,50 +1,48 @@
 package de.iteratec.osm.csi
 
 import grails.buildtestdata.TestDataBuilder
-import org.grails.orm.hibernate.cfg.DefaultGrailsDomainConfiguration
-import org.hibernate.cfg.Configuration
+import org.hibernate.boot.MetadataSources
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder
+import org.hibernate.boot.spi.MetadataImplementor
 import org.hibernate.tool.hbm2ddl.SchemaExport
 import spock.lang.Shared
 import spock.lang.Specification
 
+import java.sql.Connection
+
+/*
+See https://stackoverflow.com/a/32227118
+ */
 class NonTransactionalIntegrationSpec extends Specification implements TestDataBuilder {
 
     @Shared
-    private static Configuration _configuration
+    private static MetadataSources metadata
+    Connection connection//Wie krieg ich die? Datasource.getConnection
+
 
     @Shared
     def grailsApplication
 
     static transactional = false
 
-    def setup() {
-        if (!_configuration) {
-            // 1-time creation of the configuration
+    def cleanup() {
+        if (!metadata) {
+            metadata = new MetadataSources(new StandardServiceRegistryBuilder().applySetting(
+                    "hibernate.dialect", "org.hibernate.dialect.MySQLDialect").build())
+            // [...] adding annotated classes to metadata here...
+            configuration.addAnnotatedClass()
+
             Properties properties = new Properties()
             properties.setProperty 'hibernate.connection.driver_class', grailsApplication.config.dataSource.driverClassName
             properties.setProperty 'hibernate.connection.username', grailsApplication.config.dataSource.username
             properties.setProperty 'hibernate.connection.password', grailsApplication.config.dataSource.password ?:""
             properties.setProperty 'hibernate.connection.url', grailsApplication.config.dataSource.url
             properties.setProperty 'hibernate.dialect', 'org.hibernate.dialect.H2Dialect'
-
-            _configuration = new DefaultGrailsDomainConfiguration(grailsApplication: grailsApplication, properties: properties)
         }
-        new SchemaExport(_configuration).create(false, true)
-    }
-
-    def cleanup() {
-      if (!_configuration) {
-          // 1-time creation of the configuration
-          Properties properties = new Properties()
-          properties.setProperty 'hibernate.connection.driver_class', grailsApplication.config.dataSource.driverClassName
-          properties.setProperty 'hibernate.connection.username', grailsApplication.config.dataSource.username
-          properties.setProperty 'hibernate.connection.password', grailsApplication.config.dataSource.password ?:""
-          properties.setProperty 'hibernate.connection.url', grailsApplication.config.dataSource.url
-          properties.setProperty 'hibernate.dialect', 'org.hibernate.dialect.H2Dialect'
-
-          _configuration = new DefaultGrailsDomainConfiguration(grailsApplication: grailsApplication, properties: properties)
-      }
-      new SchemaExport(_configuration).create(false, true)
+        new SchemaExport(
+                (MetadataImplementor) metadata.buildMetadata(),
+                connection // pre-configured Connection here
+        ).create(false, true)
     }
 
 }
