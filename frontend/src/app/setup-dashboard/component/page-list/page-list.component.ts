595cfbc555d16340f0100fb3bfd4742e36ed76b2
ole.wellnitz@iteratec.de <ole.wellnitz@iteratec.de>
2018-06-12 13:30:47 +0200

    [IT-2096] show page list grouped by jobGroup

--- frontend/src/app/setup-dashboard/component/page-list/page-list.component.ts
+++ frontend/src/app/setup-dashboard/component/page-list/page-list.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {JobGroup} from "../../model/job-group.model";
-import {IPage} from "../../../common/model/page.model";
+import {IPageId} from "../../../common/model/page.model";
 import {PageService} from "../../service/rest/page.service";
-import {filter, map} from "rxjs/internal/operators";
+import {map} from "rxjs/internal/operators";
 import {Observable} from "rxjs/index";
+import {IJobGroupToPagesMapping} from "../../../common/model/job-group-to-page-mapping.model";
 
 @Component({
   selector: 'app-page-list',
@@ -11,20 +12,17 @@ import {Observable} from "rxjs/index";
   styleUrls: ['./page-list.component.css']
 })
 export class PageListComponent {
-  pageList$: Observable<IPage[]>;
+  pageList$: Observable<IPageId[]>;
 
   @Input() jobGroup: JobGroup;
 
   constructor(private pageService: PageService) {
     this.pageList$ = this.pageService.pages$.pipe(
-      filter(() => !!this.jobGroup),
-      map((pages: IPage[]) => this.filterPagesByJobGroup(this.jobGroup, pages))
+      map((jobGroupsWithPages: IJobGroupToPagesMapping[]) => this.filterPagesByJobGroup(this.jobGroup, jobGroupsWithPages))
     );
   }
 
-
-  private filterPagesByJobGroup(jobGroup: JobGroup, pages: IPage[]): IPage[] {
-    return pages
-      .filter( a => a.jobGroupId == jobGroup.getId())
+  private filterPagesByJobGroup(jobGroup: JobGroup, jobGroupsWithPages: IJobGroupToPagesMapping[]): IPageId[] {
+    return jobGroupsWithPages.find( a => a.id == jobGroup.getId()).pages;
   }
 }
--- frontend/src/app/setup-dashboard/service/rest/page.service.ts
+++ frontend/src/app/setup-dashboard/service/rest/page.service.ts
@@ -3,13 +3,14 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {JobGroup} from "../../model/job-group.model";
 import {Observable, ReplaySubject} from "rxjs/index";
 import {IPage} from "../../../common/model/page.model";
+import {IJobGroupToPagesMapping} from "../../../common/model/job-group-to-page-mapping.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PageService {
 
-  public pages$ = new ReplaySubject<IPage[]>(1);
+  public pages$ = new ReplaySubject<IJobGroupToPagesMapping[]>(1);
 
 
   constructor(private http: HttpClient) {
@@ -18,7 +19,7 @@ export class PageService {
 
 
   updatePages() {
-    this.http.get<IPage[]>("page/getPagesForActiveJobGroups")
+    this.http.get<IJobGroupToPagesMapping[]>("page/getPagesForActiveJobGroups")
       .subscribe(next => this.pages$.next(next), error => this.handleError(error));
   }
 
