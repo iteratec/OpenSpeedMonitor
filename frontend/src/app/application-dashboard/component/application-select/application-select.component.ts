72e3dbafc3ab964ac64009a0c880b32802664d2a
MoWeg <moritz.wegmann@gmail.com>
2018-06-22 17:13:38 +0200

    [IT-2244] improve style, set default value and order alphabetically

--- frontend/src/app/application-dashboard/component/application-select/application-select.component.ts
+++ frontend/src/app/application-dashboard/component/application-select/application-select.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {JobGroupService} from "../../../setup-dashboard/service/rest/job-group.service";
 import {Observable} from "rxjs/index";
+import {map, take} from "rxjs/internal/operators";
 import {JobGroupDTO} from "../../../common/model/job-group.model";
 
 @Component({
@@ -9,16 +10,25 @@ import {JobGroupDTO} from "../../../common/model/job-group.model";
   styleUrls: ['./application-select.component.css']
 })
 export class ApplicationSelectComponent {
-  @Output() onSelectedApplicationChanged: EventEmitter<JobGroupDTO> = new EventEmitter();
+  @Output() selectApplication: EventEmitter<JobGroupDTO> = new EventEmitter();
   jobGroups$: Observable<JobGroupDTO[]>;
-  application: JobGroupDTO;
+  selectedApplication: JobGroupDTO;
 
   constructor(private jobGroupService: JobGroupService) {
-    this.jobGroups$ = jobGroupService.activeOrRecentlyMeasured$
+    this.jobGroups$ = jobGroupService.activeOrRecentlyMeasured$.pipe(
+      map((jobGroups: JobGroupDTO[]) =>
+        jobGroups.sort((a, b) => a.name.localeCompare(b.name, [], {sensitivity: "base"})
+        ))
+    );
+    this.jobGroups$
+      .pipe(take(1))
+      .subscribe((jobGroups: JobGroupDTO[]) => {
+        this.selectedApplication = jobGroups[0];
+      });
   }
 
   setApplication(jobGroup: JobGroupDTO) {
-    this.application = jobGroup
-    this.onSelectedApplicationChanged.emit(this.application)
+    this.selectedApplication = jobGroup;
+    this.selectApplication.emit(jobGroup)
   }
 }
