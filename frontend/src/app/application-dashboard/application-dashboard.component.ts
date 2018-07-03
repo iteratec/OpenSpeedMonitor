c7cf7eb5344e0a57bec7958b888c575aef034dc7
Philipp Albrecht <philipp.albrecht@iteratec.de>
2018-07-03 17:37:59 +0200

    [IT-2245] refactor some code and improve observable usage

--- frontend/src/app/application-dashboard/application-dashboard.component.ts
+++ frontend/src/app/application-dashboard/application-dashboard.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {JobGroupDTO} from "../shared/model/job-group.model";
 import {ActivatedRoute, Router} from '@angular/router';
+import {JobGroupService} from "../shared/service/rest/job-group.service";
+import {combineLatest, Observable} from 'rxjs';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'osm-application-dashboard',
@@ -8,17 +11,42 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./application-dashboard.component.css']
 })
 export class ApplicationDashboardComponent {
-  application: JobGroupDTO;
-  jobGroupId: number;
+  jobGroups$: Observable<JobGroupDTO[]>;
+  selectedApplication$: Observable<JobGroupDTO>;
 
-  constructor(private route: ActivatedRoute, private router: Router) {
-    this.route.paramMap.subscribe(params => {
-      this.jobGroupId = +params.get('jobGroupId');
+  constructor(private jobGroupService: JobGroupService, private route: ActivatedRoute, private router: Router) {
+    this.jobGroups$ = jobGroupService.activeOrRecentlyMeasured$.pipe(
+      map((jobGroups: JobGroupDTO[]) => this.sortJobGroupsByName(jobGroups))
+    );
+
+    this.selectedApplication$ = combineLatest(this.jobGroups$, this.route.paramMap).pipe(
+      map(([jobGroups, params]) => this.lookForJobGroupWithId(jobGroups, params.get('jobGroupId'))),
+      filter(jobGroup => !!jobGroup)
+    );
+
+    this.handleInvalidNavigation();
+  }
+
+  private handleInvalidNavigation() {
+    combineLatest(this.jobGroups$, this.route.paramMap).subscribe(([jobGroups, params]) => {
+      if (this.lookForJobGroupWithId(jobGroups, params.get('jobGroupId'))) {
+        return;
+      }
+      if (!params.get('jobGroupId')) {
+        this.updateApplication(jobGroups[0])
+      }
     });
   }
 
   updateApplication(jobGroup: JobGroupDTO) {
     this.router.navigate(['/application-dashboard', jobGroup.id]);
-    this.application = jobGroup
+  }
+
+  private lookForJobGroupWithId(jobGroups: JobGroupDTO[], jobGroupId: string) {
+    return jobGroups.find(jobGroup => jobGroup.id == Number(jobGroupId));
+  }
+
+  private sortJobGroupsByName(jobGroups: JobGroupDTO[]) {
+    return jobGroups.sort((a, b) => a.name.localeCompare(b.name, [], {sensitivity: "base"}))
   }
 }
--- frontend/src/app/application-dashboard/component/application-select/application-select.component.ts
+++ frontend/src/app/application-dashboard/component/application-select/application-select.component.ts
@@ -1,7 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {JobGroupService} from "../../../shared/service/rest/job-group.service";
-import {Observable} from "rxjs/index";
-import {map, take} from "rxjs/internal/operators";
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {JobGroupDTO} from "../../../shared/model/job-group.model";
 
 @Component({
@@ -9,35 +6,15 @@ import {JobGroupDTO} from "../../../shared/model/job-group.model";
   templateUrl: './application-select.component.html',
   styleUrls: ['./application-select.component.css']
 })
-export class ApplicationSelectComponent implements OnInit {
-  @Output() selectApplication: EventEmitter<JobGroupDTO> = new EventEmitter();
-  @Input() jobGroupId: number;
-  jobGroups$: Observable<JobGroupDTO[]>;
-  selectedApplication: JobGroupDTO;
+export class ApplicationSelectComponent {
+  @Output() selectedApplicationChange: EventEmitter<JobGroupDTO> = new EventEmitter();
+  @Input() applications: JobGroupDTO[];
+  @Input() selectedApplication: JobGroupDTO;
 
-  constructor(private jobGroupService: JobGroupService) {
-    this.jobGroups$ = jobGroupService.activeOrRecentlyMeasured$.pipe(
-      map((jobGroups: JobGroupDTO[]) =>
-        jobGroups.sort((a, b) => a.name.localeCompare(b.name, [], {sensitivity: "base"})
-        ))
-    );
-  }
-
-  ngOnInit() {
-    this.jobGroups$
-      .pipe(take(1))
-      .subscribe((jobGroups: JobGroupDTO[]) => {
-        this.setApplication(this.lookForJobGroupWithId(jobGroups, this.jobGroupId));
-      });
+  constructor() {
   }
 
   setApplication(jobGroup: JobGroupDTO) {
-    this.selectedApplication = jobGroup;
-    this.selectApplication.emit(jobGroup)
-  }
-
-  lookForJobGroupWithId(jobGroups: JobGroupDTO[], jobGroupId: number) {
-    return (jobGroups.filter(jobGroup => jobGroup.id === jobGroupId).length === 1) ?
-      jobGroups.filter(jobGroup => jobGroup.id === jobGroupId)[0] : jobGroups[0];
+    this.selectedApplicationChange.emit(jobGroup)
   }
 }
