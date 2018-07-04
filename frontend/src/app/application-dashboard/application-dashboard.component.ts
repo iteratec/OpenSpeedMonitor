13bbc59f20d4e59214bb68f4d02aba365346161d
ole.wellnitz@iteratec.de <ole.wellnitz@iteratec.de>
2018-07-04 13:45:12 +0200

    [IT-2246] remove unnecessary property in csiDto and remove another console log

--- frontend/src/app/application-dashboard/application-dashboard.component.ts
+++ frontend/src/app/application-dashboard/application-dashboard.component.ts
@@ -12,7 +12,6 @@ export class ApplicationDashboardComponent {
   constructor() { }
 
   updateApplication(jobGroup: JobGroupDTO) {
-    console.log(jobGroup);
     this.application = jobGroup
   }
 }
--- frontend/src/app/application-dashboard/model/csi.model.ts
+++ frontend/src/app/application-dashboard/model/csi.model.ts
@@ -1,6 +1,5 @@
 export interface CsiDTO {
   date: string,
   csiDocComplete: number,
-  csiVisComplete: number,
-  isCsiCalculated: boolean
+  csiVisComplete: number
 }
