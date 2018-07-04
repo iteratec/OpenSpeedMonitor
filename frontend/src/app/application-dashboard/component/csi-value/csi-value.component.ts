8667ea7a4d36d36c0c6240b7e4c7c0672d9a6506
MoWeg <moritz.wegmann@iteratec.de>
2018-07-04 15:44:22 +0200

    [IT-2279] fixes according to review CR-294

--- frontend/src/app/application-dashboard/component/csi-value/csi-value.component.ts
+++ frontend/src/app/application-dashboard/component/csi-value/csi-value.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, Input, OnInit, ViewChild, ɵisDefaultChangeDetectionStrategy} from '@angular/core';
-import {arc} from "d3-shape";
+import {Arc, arc, DefaultArcObject} from "d3-shape";
 import {select} from "d3-selection";
 import {transition} from "d3-transition";
 import {interpolate} from "d3-interpolate";
@@ -16,19 +16,19 @@ export class CsiValueComponent implements OnInit {
 
   csiValueClass: string;
   size: number;
+  outerRadius: number;
   valueFontSize: string;
   descriptionFontSize: string;
 
   arcGenerator: any;
   @ViewChild("svg") svgElement: ElementRef;
 
+  constructor() {
+    transition() // needed for the import statement
+  }
+
   ngOnInit(): void {
-    this.csiValue = this.roundCsiValue(this.csiValue);
-    this.csiValueClass = this.determineClass(this.csiValue);
-    this.initByInput();
-    let outerRadius = this.size / 2;
-    let innerRadius = outerRadius - outerRadius * 0.15;
-    this.arcGenerator = this.getArcGenerator(innerRadius, outerRadius);
+    this.initByInputs();
 
     let selection = select(this.svgElement.nativeElement).selectAll("g.csi-circle").data([this.csiValue])
     this.enter(selection.enter());
@@ -36,67 +36,7 @@ export class CsiValueComponent implements OnInit {
     this.exit(selection.exit());
   }
 
-  getArcGenerator(innerRadius: number, outerRadius: number) {
-    return arc()
-      .innerRadius(innerRadius)
-      .outerRadius(outerRadius)
-      .startAngle(Math.PI)
-      .endAngle((input: any) => {
-        return input as number;
-      })
-  }
-
-  constructor() {
-    transition() // needed for the import statement
-  }
-
-  enter(selection: any) {
-    const baseCircle = this.arcGenerator(3 * Math.PI);
-
-    const circleGroup = selection
-      .append("g")
-      .attr("class", "csi-circle")
-      .attr("transform", "translate(" + this.size / 2 + "," + this.size / 2 + ")");
-
-    circleGroup
-      .append("path")
-      .attr("class", "csi-circle-background")
-      .attr("d", baseCircle)
-      .attr("fill", "currentColor")
-      .style("opacity", 0.1);
-    circleGroup
-      .append("path")
-      .attr("class", "csi-circle-value")
-      .attr("fill", "currentColor")
-  }
-
-  update(selection: any) {
-    selection
-      .select("path.csi-circle-value")
-      .transition()
-      .duration(1000)
-      .attrTween("d", this.tweenArc(this.calculateCsiArcTarget(this.csiValue)))
-  }
-
-  private tweenArc(target: any) {
-    return (d: any) => {
-      const interpolator = interpolate(Math.PI, target);
-      return (t) => {
-        return this.arcGenerator(interpolator(t));
-      }
-    };
-  }
-
-
-  private calculateCsiArcTarget(csiValue: number) {
-    return 2 * csiValue / 100 * Math.PI + Math.PI;
-  }
-
-  exit(selection: any) {
-    selection.remove()
-  }
-
-  private initByInput() {
+  private initByInputs() {
     if (!this.description) {
       this.description = "CSI";
     }
@@ -110,6 +50,68 @@ export class CsiValueComponent implements OnInit {
       this.valueFontSize = '18';
       this.descriptionFontSize = '12';
     }
+
+    this.outerRadius = this.size / 2;
+    let innerRadius = this.outerRadius - this.outerRadius * 0.15;
+    this.arcGenerator = this.getArcGenerator(innerRadius, this.outerRadius);
+
+    this.csiValue = this.roundCsiValue(this.csiValue);
+    this.csiValueClass = this.determineClass(this.csiValue);
+  }
+
+  private getArcGenerator(innerRadius: number, outerRadius: number){
+    return arc()
+      .innerRadius(innerRadius)
+      .outerRadius(outerRadius)
+      .startAngle(Math.PI)
+      .endAngle((input: any) => {
+        return input as number;
+      })
+  }
+
+  private enter(selection: any) {
+    const baseCircle = this.arcGenerator(3 * Math.PI);
+
+    const circleGroup = selection
+      .append("g")
+      .attr("class", "csi-circle")
+      .attr("transform", `translate(${this.outerRadius},${this.outerRadius})`);
+
+    circleGroup
+      .append("path")
+      .attr("class", "csi-circle-background")
+      .attr("d", baseCircle)
+      .attr("fill", "currentColor")
+      .style("opacity", 0.1);
+    circleGroup
+      .append("path")
+      .attr("class", "csi-circle-value")
+      .attr("fill", "currentColor")
+  }
+
+  private update(selection: any) {
+    selection
+      .select("path.csi-circle-value")
+      .transition()
+      .duration(1000)
+      .attrTween("d", this.tweenArc(this.calculateCsiArcTarget(this.csiValue)))
+  }
+
+  private exit(selection: any) {
+    selection.remove()
+  }
+
+  private tweenArc(target: any) {
+    return (d: any) => {
+      const interpolator = interpolate(Math.PI, target);
+      return (t) => {
+        return this.arcGenerator(interpolator(t));
+      }
+    };
+  }
+
+  private calculateCsiArcTarget(csiValue: number) {
+    return 2 * csiValue / 100 * Math.PI + Math.PI;
   }
 
   private determineClass(csiValue: number):string{
