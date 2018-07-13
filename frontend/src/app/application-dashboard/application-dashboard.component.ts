a625571bfb5d06a8ae0033a6dede32814203cc9d
MoWeg <moritz.wegmann@iteratec.de>
2018-07-13 14:06:14 +0200

    [IT-2305] decouple page list from application update

--- frontend/src/app/application-dashboard/application-dashboard.component.ts
+++ frontend/src/app/application-dashboard/application-dashboard.component.ts
@@ -39,6 +39,7 @@ export class ApplicationDashboardComponent implements OnDestroy {
     this.selectedApplication = this.findApplicationById(applications, applicationId);
     if (this.selectedApplication) {
       this.dashboardService.updateMetricsForApplication(this.selectedApplication.id);
+      this.dashboardService.updatePagesForApplication(this.selectedApplication.id);
     }
   }
 
--- frontend/src/app/application-dashboard/services/application-dashboard.service.ts
+++ frontend/src/app/application-dashboard/services/application-dashboard.service.ts
@@ -1,32 +1,38 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, ReplaySubject} from "rxjs/index";
+import {ReplaySubject} from "rxjs/index";
 import {PageDto} from "../models/page.model";
 import {MetricsDto} from "../models/metrics.model";
 
 @Injectable()
 export class ApplicationDashboardService {
   metrics$: ReplaySubject<MetricsDto[]> = new ReplaySubject<MetricsDto[]>(1);
+  pages$: ReplaySubject<PageDto[]> = new ReplaySubject<PageDto[]>(1);
 
   constructor(private http: HttpClient) {
   }
 
-  getPagesForJobGroup(applicationId: number): Observable<PageDto[]> {
-    return this.http.get<PageDto[]>('/applicationDashboard/rest/getPagesForApplication', {
-      params: {
-        applicationId: applicationId ? applicationId.toString() : ""
-      }
-    });
+  updateMetricsForApplication(applicationId: number) {
+    this.http.get<MetricsDto[]>('/applicationDashboard/rest/getMetricsForApplication', this.createParams(applicationId))
+      .subscribe((response: MetricsDto[]) => this.metrics$.next(response), error => this.handleError(error));
   }
 
-  updateMetricsForApplication(applicationId: number) {
-    this.http.get<MetricsDto[]>('/applicationDashboard/rest/getMetricsForApplication', {
+  updatePagesForApplication(applicationId: number) {
+    this.http.get<PageDto[]>('/applicationDashboard/rest/getPagesForApplication', this.createParams(applicationId))
+      .subscribe((response: PageDto[]) => this.pages$.next(response), error => this.handleError(error))
+  }
+
+  private handleError(error: any) {
+    console.log(error);
+  }
+
+  private createParams(applicationId: number) {
+    return {
       params: {
         applicationId: applicationId ? applicationId.toString() : ""
       }
-    }).subscribe((response: MetricsDto[]) => {
-      this.metrics$.next(response)
-    });
+    }
   }
 
 }
+
