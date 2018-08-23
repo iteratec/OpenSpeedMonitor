35ca342a01660d63e5610cabeedba5c82b13265d
Finn Martens <Finn.Martens@iteratec.de>
2018-08-23 14:12:22 +0200

    [IT-2254] Added buttons and styling for the different error cases

--- frontend/src/app/application-dashboard/components/csi-info/csi-info.component.ts
+++ frontend/src/app/application-dashboard/components/csi-info/csi-info.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {ApplicationCsiListDTO} from "../../models/csi-list.model";
+import {ApplicationDTO} from "../../models/application.model";
 
 @Component({
   selector: 'osm-csi-info',
@@ -9,27 +10,38 @@ import {ApplicationCsiListDTO} from "../../models/csi-list.model";
 export class CsiInfoComponent implements OnChanges {
 
   @Input() csiData: ApplicationCsiListDTO;
+  @Input() selectedApplication: ApplicationDTO;
 
+  errorCase: number;
   infoText: string;
+  iconClass: string;
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.infoText = this.setText()
+    console.log(this.selectedApplication);
+    this.infoText = this.setInformation();
   }
 
-  private setText () {
+  private setInformation () {
     if (!this.csiData.hasCsiConfiguration) {
       // Case 2: No CSI configuration for application
+      this.errorCase = 2;
+      this.iconClass = 'icon-info fas fa-info-circle';
       return 'The calculation of the customer satisfaction index (CSI) is not configured for this application.'
     }
     if (this.csiData.hasJobResults) {
       if (this.csiData.hasInvalidJobResults) {
         // Case 4: Measurements have errors
+        this.errorCase = 4;
+        this.iconClass = 'icon-warning fas fa-exclamation-triangle';
         return 'The measurements are not working properly.'
       }
       // Case 3: CSI config and job results but no csi value
+      this.errorCase = 3;
+      this.iconClass = 'icon-warning fas fa-exclamation-triangle';
       return 'The configuration of the customer satisfaction index (CSI) does not produce a CSI value for this application or there are no new measurements since the CSI configuration has been updated.'
     }
     // Case 1: Application has not been measured
+    this.iconClass = 'icon-info fas fa-info-circle';
     return 'This application has not been measured yet.'
   }
 
