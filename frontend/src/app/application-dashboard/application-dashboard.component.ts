6353fdf1a6bb306607368d6afec952bfb92f2bf2
Johannes Weiß <johannes.weiss@iteratec.de>
2018-08-29 18:04:39 +0200

    Use grid layout for whole page and regain first row as one element

--- frontend/src/app/application-dashboard/application-dashboard.component.ts
+++ frontend/src/app/application-dashboard/application-dashboard.component.ts
@@ -4,6 +4,10 @@ import {combineLatest, Observable, Subject} from 'rxjs';
 import {map, takeUntil} from 'rxjs/operators';
 import {ApplicationDashboardService} from './services/application-dashboard.service';
 import {ApplicationDTO} from './models/application.model';
+import {PageMetricsDto} from "./models/page-metrics.model";
+import {ResponseWithLoadingState} from "./models/response-with-loading-state.model";
+import {ApplicationCsiListDTO} from "./models/csi-list.model";
+import {CsiDTO} from "./models/csi.model";
 
 @Component({
   selector: 'osm-application-dashboard',
@@ -14,15 +18,43 @@ export class ApplicationDashboardComponent implements OnDestroy {
   applications$: Observable<ApplicationDTO[]>;
   selectedApplication: ApplicationDTO;
   destroyed$ = new Subject<void>();
+  pages$: Observable<PageMetricsDto[]>;
+  csiValues$: Observable<ApplicationCsiListDTO>;
+  recentCsiDate$: Observable<string>;
+  recentCsiValue$: Observable<CsiDTO>;
+  hasConfiguration$: Observable<boolean>;
+  isLoading: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private dashboardService: ApplicationDashboardService
   ) {
+    this.pages$ = this.dashboardService.metrics$;
     this.applications$ = dashboardService.activeOrRecentlyMeasured$.pipe(
       map((applications: ApplicationDTO[]) => this.sortApplicationsByName(applications))
     );
+
+    this.csiValues$ = this.dashboardService.csiValues$.pipe(
+      map((res: ResponseWithLoadingState<ApplicationCsiListDTO>) => {
+        return res.data;
+      }));
+
+    this.recentCsiValue$ = this.dashboardService.csiValues$.pipe(
+      map((res: ResponseWithLoadingState<ApplicationCsiListDTO>) => {
+        this.isLoading = res.isLoading;
+        const csiDto: CsiDTO = res.data.csiDtoList.slice(-1)[0];
+        return csiDto ? csiDto : null;
+      }));
+
+    this.recentCsiDate$ = this.dashboardService.csiValues$.pipe(
+      map((res: ResponseWithLoadingState<ApplicationCsiListDTO>) => {
+        const csiDto: CsiDTO = res.data.csiDtoList.slice(-1)[0];
+        return csiDto ? csiDto.date : null;
+      }));
+
+    this.hasConfiguration$ = this.dashboardService.csiValues$.pipe(
+      map((res: ResponseWithLoadingState<ApplicationCsiListDTO>) => res.data.hasCsiConfiguration));
 
     combineLatest(this.route.paramMap, this.applications$)
       .pipe(takeUntil(this.destroyed$))
