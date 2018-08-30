8fc8c865a1faba2d8d78319010b429d6ed545c9d
Johannes Weiß <johannes.weiss@iteratec.de>
2018-08-30 10:49:25 +0200

    Clean up of unused components

--- frontend/src/app/application-dashboard/application-dashboard.module.ts
+++ frontend/src/app/application-dashboard/application-dashboard.module.ts
@@ -2,14 +2,12 @@ import {NgModule} from '@angular/core';
 import {ApplicationDashboardComponent} from './application-dashboard.component';
 import {ApplicationSelectComponent} from './components/application-select/application-select.component';
 import {SharedModule} from "../shared/shared.module";
-import {PageListComponent} from './components/page-list/page-list.component';
 import {ApplicationDashboardService} from "./services/application-dashboard.service";
 import {PageComponent} from './components/page/page.component';
-import {ApplicationCsiComponent} from './components/application-csi/application-csi.component';
 import {ApplicationDashboardEntryComponent} from './application-dashboard.entry-component'
 import {RouterModule, Routes} from "@angular/router";
 import {CsiValueComponent} from "./components/csi-value/csi-value.component";
-import { CsiGraphComponent } from './components/csi-graph/csi-graph.component';
+import {CsiGraphComponent} from './components/csi-graph/csi-graph.component';
 
 const DashboardRoutes: Routes = [
   {path: '', component: ApplicationDashboardComponent},
@@ -25,8 +23,6 @@ const DashboardRoutes: Routes = [
     ApplicationDashboardEntryComponent,
     ApplicationDashboardComponent,
     ApplicationSelectComponent,
-    ApplicationCsiComponent,
-    PageListComponent,
     PageComponent,
     CsiValueComponent,
     CsiGraphComponent
