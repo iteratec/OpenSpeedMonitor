22a68131956f002c9d509d40a096225581514fba
Philipp Albrecht <philipp.albrecht@iteratec.de>
2018-09-06 12:47:44 +0000

    [IT-2319] add deployment

--- scripts/ci/deploy/travis_curltrigger.sh
+++ scripts/ci/deploy/travis_curltrigger.sh
@@ -6,4 +6,4 @@ export PROJECT_NAME=OSM
 export PLAN_NAME=DOD
 export STAGE_NAME=JOB1
 
-curl --user $BAMBOO_USER:$BAMBOO_PASS -X POST -d "$STAGE_NAME&ExecuteAllStages" https://bamboo.iteratec.io/rest/api/latest/queue/$PROJECT_NAME-$PLAN_NAME 
+curl --user $BAMBOO_USER:$BAMBOO_PASS -X POST -d "$STAGE_NAME&ExecuteAllStages" https://bamboo.iteratec.io/rest/api/latest/queue/$PROJECT_NAME-$PLAN_NAME
