7960e2fda2b86e78c67c16075203cb1ddac89cbb
Philipp Albrecht <philipp.albrecht@iteratec.de>
2018-09-07 09:49:01 +0000

    [IT-2319] inline script

--- scripts/ci/deploy/travis_curltrigger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export BAMBOO_USER=$BAMBOO_TU_USERNAME
-export BAMBOO_PASS=$BAMBOO_TU_PASSWORD
-export PROJECT_NAME=OSM
-export PLAN_NAME=DOD
-export STAGE_NAME=JOB1
-
-curl --user $BAMBOO_USER:$BAMBOO_PASS -X POST -d "$STAGE_NAME&ExecuteAllStages" https://bamboo.iteratec.io/rest/api/latest/queue/$PROJECT_NAME-$PLAN_NAME
