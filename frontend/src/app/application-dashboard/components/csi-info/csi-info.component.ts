62f087d087a2a1dbbb1908fccff703204cbab616
Finn Martens <Finn.Martens@iteratec.de>
2018-09-12 09:50:10 +0200

    [IT-2459] Moved translation to html

--- frontend/src/app/application-dashboard/components/csi-info/csi-info.component.ts
+++ frontend/src/app/application-dashboard/components/csi-info/csi-info.component.ts
@@ -2,7 +2,6 @@ import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {ApplicationCsiListDTO} from "../../models/csi-list.model";
 import {ApplicationDTO} from "../../models/application.model";
 import {ApplicationDashboardService} from "../../services/application-dashboard.service";
-import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'osm-csi-info',
@@ -19,7 +18,7 @@ export class CsiInfoComponent implements OnChanges {
   buttonText: string;
   iconClass: string;
 
-  constructor(private applicationDashboardService: ApplicationDashboardService, private translationService: TranslateService) {
+  constructor(private applicationDashboardService: ApplicationDashboardService) {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -31,27 +30,27 @@ export class CsiInfoComponent implements OnChanges {
       // Case 2: No CSI configuration for application
       this.errorCase = 2;
       this.iconClass = 'icon-info fas fa-info-circle';
-      this.buttonText = this.translationService.instant("frontend.de.iteratec.osm.applicationDashboard.csiInfo.button.noCsiConfig");
-      return this.translationService.instant("frontend.de.iteratec.osm.applicationDashboard.csiInfo.noCsiConfig");
+      this.buttonText = 'frontend.de.iteratec.osm.applicationDashboard.csiInfo.button.noCsiConfig';
+      return 'frontend.de.iteratec.osm.applicationDashboard.csiInfo.noCsiConfig';
     }
     if (this.csiData.hasJobResults) {
       if (this.csiData.hasInvalidJobResults) {
         // Case 4: Measurements have errors
         this.errorCase = 4;
         this.iconClass = 'icon-warning fas fa-exclamation-triangle';
-        this.buttonText = this.translationService.instant("frontend.de.iteratec.osm.applicationDashboard.csiInfo.button.invalidMeasurement");
-        return this.translationService.instant("frontend.de.iteratec.osm.applicationDashboard.csiInfo.invalidMeasurement");
+        this.buttonText = 'frontend.de.iteratec.osm.applicationDashboard.csiInfo.button.invalidMeasurement';
+        return 'frontend.de.iteratec.osm.applicationDashboard.csiInfo.invalidMeasurement';
       }
       // Case 3: CSI config and job results but no csi value
       this.errorCase = 3;
       this.iconClass = 'icon-warning fas fa-exclamation-triangle';
-      this.buttonText = this.translationService.instant("frontend.de.iteratec.osm.applicationDashboard.csiInfo.button.noCsiValue");
-      return this.translationService.instant("frontend.de.iteratec.osm.applicationDashboard.csiInfo.noCsiValue");
+      this.buttonText = 'frontend.de.iteratec.osm.applicationDashboard.csiInfo.button.noCsiValue';
+      return 'frontend.de.iteratec.osm.applicationDashboard.csiInfo.noCsiValue';
     }
     // Case 1: Application has not been measured
     this.errorCase = 1;
     this.iconClass = 'icon-info fas fa-info-circle';
-    return this.translationService.instant("frontend.de.iteratec.osm.applicationDashboard.csiInfo.notMeasured");
+    return 'frontend.de.iteratec.osm.applicationDashboard.csiInfo.notMeasured';
   }
 
   createCsiConfiguration () {
