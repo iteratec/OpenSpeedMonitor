a82260b37f8fd0b38c974f904388b064c4559642
Philipp Albrecht <philipp.albrecht@iteratec.de>
2018-09-14 12:03:53 +0000

    [IT-2439] git add merge script

--- scripts/ci/building/merge_release_into_dev_and_master.sh
+++ scripts/ci/building/merge_release_into_dev_and_master.sh
@@ -2,9 +2,9 @@
 
 git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch --all
-git checkout test/file
-#git merge release
-touch test
-git add .
-git commit -m "test push of travis"
+git checkout develop
+git merge release
+git push https://iteraspeed:$GITHUB_USER_TOKEN@github.com/iteratec/OpenSpeedMonitor.git
+git checkout master
+git merge release
 git push https://iteraspeed:$GITHUB_USER_TOKEN@github.com/iteratec/OpenSpeedMonitor.git
