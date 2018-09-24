a7b08c2b392b982b0b01907d0d10038dad782e40
bamboo iteratec <osm@iteratec.de>
2018-09-24 12:42:27 +0000

    [IT-2439] fix tag and push

--- scripts/ci/building/push_new_versionnumber.sh
+++ scripts/ci/building/push_new_versionnumber.sh
@@ -28,31 +28,14 @@ else
   fi
   echo "jira_version=$jira_version"
 
-  echo "push commit and tag for new set version"
-  echo "########################################'"
-  remote=origin
-
-  echo "git remote remove $remote"
-  git remote remove $remote
-
-  remote_url=https://$bamboo_git_USER_NAME:$bamboo_git_PASSWORD@github.com/iteratec/OpenSpeedMonitor.git
-  echo "set remote $remote to '$remote_url'"
-  git remote add -f $remote $remote_url
-
-  git config user.email 'osm@iteratec.de'
-  git config user.name 'bamboo iteratec'
-
   # the following commit message is referenced by regex in bamboo to exclude these commits
   # while picking up changes (configured in bamboo repositories advanced settings)
   echo "git commit -am '[${jira_version}] version update'"
   git status
   git commit -am "[${jira_version}] version update"
 
-  echo "pull --rebase $remote release"
-  git pull --rebase $remote release
-
   echo "git tag 'v${jira_version}'"
   git tag "v${jira_version}"
-  echo "git push --tags $remote HEAD:refs/heads/release"
-  git push --tags $remote HEAD:refs/heads/release
+  echo "git push --tags"
+  git push --tags
 fi
