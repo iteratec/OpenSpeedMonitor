f3ad455bd3dd3fc7d8cff110e0512360042f4478
Philipp Albrecht <philipp.albrecht@iteratec.de>
2018-09-24 11:56:52 +0000

    [IT-2439] fix dev into release merge script

--- scripts/ci/building/merge_develop_into_release.sh
+++ scripts/ci/building/merge_develop_into_release.sh
@@ -2,27 +2,25 @@
 
 echo "prepare some variables"
 echo "########################################'"
-echo "bamboo_planRepository_branchName=$bamboo_planRepository_branchName"
+echo "start bamboo job to merge develop into release"
+echo "########################################'"
+remote=origin
 
-if [ "${bamboo_planRepository_branchName}" = "release" ]; then
+echo "git remote remove $remote"
+git remote remove $remote
 
-  echo "start bamboo job to merge develop into release"
-  echo "########################################'"
-  remote=origin
+remote_url=https://$bamboo_git_USER_NAME:$bamboo_git_PASSWORD@github.com/iteratec/OpenSpeedMonitor.git
+echo "set remote $remote to '$remote_url'"
+git remote add -f $remote $remote_url
 
-  echo "git remote remove $remote"
-  git remote remove $remote
+git config user.email 'osm@iteratec.de'
+git config user.name 'bamboo iteratec'
 
-  remote_url=https://$bamboo_git_USER_NAME:$bamboo_git_PASSWORD@github.com/iteratec/OpenSpeedMonitor.git
-  echo "set remote $remote to '$remote_url'"
-  git remote add -f $remote $remote_url
-
-  git config user.email 'osm@iteratec.de'
-  git config user.name 'bamboo iteratec'
-
-  echo "########################################'"
-  echo "git merge develop"
-  git merge develop
-else
-  echo 'Wrong branch. Committing only into the release branch.'
-fi
+echo "########################################'"
+echo "git fetch release"
+git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch --all
+echo "git checkout release"
+git checkout release
+echo "git merge develop"
+git merge develop
