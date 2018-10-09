fc70a6745a800d81fb45b185b9c1ed2bd69d4282
fho <felix.hoffmann@iteratec.de>
2018-10-09 10:35:42 +0200

    Added documentation to explain odd liquibase change set.

--- grails-app/migrations/2018-10-01-v500-job-label-optional.groovy
+++ grails-app/migrations/2018-10-01-v500-job-label-optional.groovy
@@ -2,6 +2,11 @@ databaseChangeLog = {
     changeSet(author: "fho", id: "20181001-label-1") {
         dropNotNullConstraint(columnDataType: "varchar(255)", tableName: "job", columnName: "label")
     }
+    /*
+    Column 'label' was created with a unique constraint that was not named. Liquibase thus generates a name for it.
+    Since that name is not known beforehand, the index cannot be deleted using dropUniqueConstraint(). Hence the
+    necessity to create a new column and copy all rows.
+     */
     changeSet(author: "fho", id: "20181008-label-1") {
         addColumn(tableName: "job") {
             column(name: "label_temp", type: "varchar(255)")
