d2c45e408f3e1b8e2105a8ea4bb350eb5336b843
Fabian Wiecz <fabian.wieczorek@iteratec.de>
2018-11-26 13:48:52 +0100

    Fixes according to change requests

--- grails-app/services/de/iteratec/osm/system/MissingJobResultCheckService.groovy
+++ grails-app/services/de/iteratec/osm/system/MissingJobResultCheckService.groovy
@@ -11,53 +11,42 @@ import org.quartz.CronExpression
 @Transactional
 class MissingJobResultCheckService {
     static final int FIRST_CHECK_DAYS_AGO = 1
-    static final long SCHEDULE_THRESHOLD_MINUTES = 3
+    static final int SCHEDULE_THRESHOLD_MINUTES = 3
 
     JobResultPersisterService jobResultPersisterService
 
     def fillMissingJobResults() {
         Date latestCheckDate = getLatestCheck()?.date
-        int missingResults = 0
-        if(latestCheckDate) {
-            missingResults = fillMissingJobResultsSinceDate( new DateTime(latestCheckDate).minusMinutes(SCHEDULE_THRESHOLD_MINUTES.toInteger()))
-            new MissingJobResultCheck(date: new Date(), missingResults: missingResults).save()
-        }
-        else {
-            DateTime dateDaysAgo =  new DateTime().minusDays(FIRST_CHECK_DAYS_AGO)
-            missingResults = fillMissingJobResultsSinceDate( dateDaysAgo )
-            new MissingJobResultCheck(date: new Date(), missingResults: missingResults).save()
-        }
+        DateTime startDate = latestCheckDate ? new DateTime(latestCheckDate).minusMinutes(SCHEDULE_THRESHOLD_MINUTES) : new DateTime().minusDays(FIRST_CHECK_DAYS_AGO)
+        int missingResults = fillMissingJobResultsSinceDate( startDate )
+        persistMissingJobResultCheck(missingResults)
         log.info("Fixed Missing JobResults: $missingResults")
     }
 
     private int fillMissingJobResultsSinceDate(DateTime since) {
         int missingJobResults = 0
-        getPreparedJobList(since).forEach{ job ->
-            def existingResults = getJobResultsSince(job.job, job.since)
-            missingJobResults += checkForMissingJobResults(existingResults, job.since, job.job)
+        getPreparedJobList().each{ job ->
+            List<JobResult> existingResults = getJobResultsSince(job, since)
+            missingJobResults += checkForMissingJobResults(existingResults, since, job)
         }
         return missingJobResults
     }
 
-    private List<Map> getPreparedJobList(DateTime since) {
-        return Job.findAllByDeletedAndActiveAndExecutionScheduleIsNotNull(false, true).collect {
-            ['job': it, 'id': it.id, 'since': since]
-        }
+    private List<Map> getPreparedJobList() {
+        return Job.findAllByDeletedAndActiveAndExecutionScheduleIsNotNull(false, true)
     }
 
-    private def getJobResultsSince(Job job, DateTime since) {
-        return JobResult.findAllByJobAndDateGreaterThan(job, since.toDate())
+    private List<JobResult> getJobResultsSince(Job job, DateTime since) {
+        return JobResult.findAllByJobAndExecutionDateBetween(job, since.toDate(), new Date())
     }
 
     private int checkForMissingJobResults( List<JobResult> existingResults, DateTime since, Job job) {
         DateTime now = new DateTime()
         List<DateTime> scheduledDates = getScheduledDates(since.toDate(), now.toDate(), job)
         existingResults.forEach{ jobResult ->
-            if(jobResult.executionDate && (jobResult.executionDate <= now.toDate())) {
-                DateTime jobResultDate = findDateForJobResult(scheduledDates, jobResult )
-                if(jobResultDate) {
-                    scheduledDates.remove(jobResultDate)
-                }
+            DateTime jobResultDate = findDateForJobResult(scheduledDates, jobResult )
+            if(jobResultDate) {
+                scheduledDates.remove(jobResultDate)
             }
         }
         if(!scheduledDates.empty) {
@@ -69,15 +58,15 @@ class MissingJobResultCheckService {
 
     DateTime findDateForJobResult(List<DateTime> dates, JobResult jobResult) {
         DateTime execDate = new DateTime(jobResult.executionDate)
-        List<DateTime> closeDates = dates.findAll { Math.abs(dateDist(it, execDate)) < SCHEDULE_THRESHOLD_MINUTES * (1000L * 60L) }
+        List<DateTime> closeDates = dates.findAll { dateDist(it, execDate) < SCHEDULE_THRESHOLD_MINUTES * (1000L * 60L) }
         if(!closeDates.empty) { // Find closest date
-            return (closeDates.collect{ [Math.abs(dateDist(it, execDate)), it] }.sort().first())[1]
+            return closeDates.sort{ dateDist(it, execDate) }.first()
         }
         return null
     }
 
     long dateDist(DateTime d1, DateTime d2) {
-        return Math.abs((d1 - d2.millis).millis)
+        return Math.abs(d1.millis - d2.millis)
     }
 
     List<DateTime> getScheduledDates(Date since, Date until, Job job) {
@@ -94,6 +83,18 @@ class MissingJobResultCheckService {
     private void persistMissingJobResults(List<DateTime> missingResults, Job job) {
         for(jobResult in missingResults){
             jobResultPersisterService.persistMissingJobResult(job, jobResult.toDate())
+        }
+    }
+
+    private void persistMissingJobResultCheck(int missingResults) {
+        MissingJobResultCheck latest = getLatestCheck()
+        if(latest) {
+            latest.date = new Date()
+            latest.missingResults = missingResults
+            latest.save()
+        }
+        else {
+            new MissingJobResultCheck(date: new Date(), missingResults: missingResults).save()
         }
     }
 
