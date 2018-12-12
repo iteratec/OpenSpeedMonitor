0e987bbf2c4ba32577f7a1c59907c75b905203d6
Finn Martens <Finn.Martens@iteratec.de>
2018-12-12 13:21:56 +0100

    [IT-2480] Added JobHealthReportService Unit test

--- grails-app/services/de/iteratec/osm/report/external/JobHealthReportService.groovy
+++ grails-app/services/de/iteratec/osm/report/external/JobHealthReportService.groovy
@@ -8,25 +8,38 @@ class JobHealthReportService {
     GraphiteSocketProvider graphiteSocketProvider
 
     void reportJobHealthStatusToGraphite(Date date) {
-        if (jobsToReport) {
-            log.debug("Starting job health report for the jobs: ${jobsToReport}")
-            jobsToReport.each { job ->
-                Collection<GraphiteServer> graphiteServers = job.jobGroup.jobHealthGraphiteServers
+        List<Job> jobs = Job.createCriteria().list {
+            eq 'deleted', false
+            eq 'active', true
+            isNotNull 'jobStatistic'
+            jobGroup {
+                isNotNull 'jobHealthGraphiteServers'
+                isNotEmpty 'jobHealthGraphiteServers'
+            }
+        }
+        if (jobs) {
+            reportJobHealthStatusToGraphite(date, jobs)
+        }
+    }
 
-                graphiteServers.each { graphiteServer ->
-                    GraphiteSocket socket = graphiteSocketProvider.getSocket(graphiteServer)
+    void reportJobHealthStatusToGraphite(Date date, List<Job> jobsToReport) {
+        log.debug("Starting job health report for the jobs: ${jobsToReport}")
+        jobsToReport.each { job ->
+            Collection<GraphiteServer> graphiteServers = job.jobGroup.jobHealthGraphiteServers
 
-                    String basePath = determineBasePath(job, graphiteServer)
+            graphiteServers.each { graphiteServer ->
+                GraphiteSocket socket = graphiteSocketProvider.getSocket(graphiteServer)
 
-                    if (job.jobStatistic.percentageSuccessfulTestsOfLast5 != null) {
-                        socket.sendDate(GraphitePathName.valueOf(basePath + ".percentageSuccessfulTestsOfLast5"), job.jobStatistic.percentageSuccessfulTestsOfLast5, date)
-                    }
-                    if (job.jobStatistic.percentageSuccessfulTestsOfLast25 != null) {
-                        socket.sendDate(GraphitePathName.valueOf(basePath + ".percentageSuccessfulTestsOfLast25"), job.jobStatistic.percentageSuccessfulTestsOfLast25, date)
-                    }
-                    if (job.jobStatistic.percentageSuccessfulTestsOfLast150 != null) {
-                        socket.sendDate(GraphitePathName.valueOf(basePath + ".percentageSuccessfulTestsOfLast150"), job.jobStatistic.percentageSuccessfulTestsOfLast150, date)
-                    }
+                String basePath = determineBasePath(job, graphiteServer)
+
+                if (job.jobStatistic.percentageSuccessfulTestsOfLast5 != null) {
+                    socket.sendDate(GraphitePathName.valueOf(basePath + ".percentageSuccessfulTestsOfLast5"), job.jobStatistic.percentageSuccessfulTestsOfLast5, date)
+                }
+                if (job.jobStatistic.percentageSuccessfulTestsOfLast25 != null) {
+                    socket.sendDate(GraphitePathName.valueOf(basePath + ".percentageSuccessfulTestsOfLast25"), job.jobStatistic.percentageSuccessfulTestsOfLast25, date)
+                }
+                if (job.jobStatistic.percentageSuccessfulTestsOfLast150 != null) {
+                    socket.sendDate(GraphitePathName.valueOf(basePath + ".percentageSuccessfulTestsOfLast150"), job.jobStatistic.percentageSuccessfulTestsOfLast150, date)
                 }
             }
         }
@@ -43,17 +56,5 @@ class JobHealthReportService {
         pathElements.add(GraphitePathName.replaceInvalidGraphitePathCharacters(job.id.toString()))
         String basePath = pathElements.join('.')
         return basePath
-    }
-
-    private List<Job> getJobsToReport() {
-        return Job.createCriteria().list {
-            eq 'deleted', false
-            eq 'active', true
-            isNotNull 'jobStatistic'
-            jobGroup {
-                isNotNull 'jobHealthGraphiteServers'
-                isNotEmpty 'jobHealthGraphiteServers'
-            }
-        }
     }
 }
