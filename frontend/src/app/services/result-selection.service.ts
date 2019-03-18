1647f8503309d05ef1499b7effb8cb3da4678395
mwg <moritz.wegmann@iteratec.de>
2019-03-18 18:47:02 +0100

    translate selected measurand as well

--- frontend/src/app/services/result-selection.service.ts
+++ frontend/src/app/services/result-selection.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {EMPTY, Observable, OperatorFunction, ReplaySubject, combineLatest} from "rxjs";
 import {MeasurandGroup, SelectableMeasurand} from "../models/measurand.model";
@@ -16,8 +16,8 @@ export class ResultSelectionService {
   userTimings$: ReplaySubject<ResponseWithLoadingState<MeasurandGroup>> = new ReplaySubject(1);
   heroTimings$: ReplaySubject<ResponseWithLoadingState<MeasurandGroup>> = new ReplaySubject(1);
   requestCounts$: ReplaySubject<ResponseWithLoadingState<MeasurandGroup>> = new ReplaySubject(1);
-  requestSizes$:  ReplaySubject<ResponseWithLoadingState<MeasurandGroup>> = new ReplaySubject(1);
-  percentages$:  ReplaySubject<ResponseWithLoadingState<MeasurandGroup>> = new ReplaySubject(1);
+  requestSizes$: ReplaySubject<ResponseWithLoadingState<MeasurandGroup>> = new ReplaySubject(1);
+  percentages$: ReplaySubject<ResponseWithLoadingState<MeasurandGroup>> = new ReplaySubject(1);
 
   selectedApplications$: ReplaySubject<Application[]> = new ReplaySubject<Application[]>(1);
   selectedPages$: ReplaySubject<Page[]> = new ReplaySubject<Page[]>(1);
@@ -34,21 +34,23 @@ export class ResultSelectionService {
     ).subscribe(this.heroTimings$)
   }
 
-  updateApplications(applications: Application[]){
+  updateApplications(applications: Application[]) {
     this.selectedApplications$.next(applications);
     this.updatePages([]);
   }
-  updatePages(pages: Page[]){
+
+  updatePages(pages: Page[]) {
     this.selectedPages$.next(pages);
   }
 
-  private combinedParams():Observable<any>{
+  private combinedParams(): Observable<any> {
     return combineLatest(
       this.selectedApplications$,
       this.selectedPages$,
-      (applications: Application[], pages:Page[]) => this.generateParams(applications, pages));
+      (applications: Application[], pages: Page[]) => this.generateParams(applications, pages));
   }
-  private generateParams(applications: Application[], pages: Page[]){
+
+  private generateParams(applications: Application[], pages: Page[]) {
     let now: Date = new Date();
     let threeDaysAgo: Date = new Date();
     threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
@@ -60,39 +62,49 @@ export class ResultSelectionService {
     }
   }
 
-  private getUserTimings(params): Observable<ResponseWithLoadingState<MeasurandGroup>>{
+  private getUserTimings(params): Observable<ResponseWithLoadingState<MeasurandGroup>> {
     const userTimingsUrl: string = '/resultSelection/getUserTimings';
-    const groupName: string =  "USER_TIMINGS";
+    const groupName: string = "USER_TIMINGS";
     this.setToLoading(this.userTimings$, groupName);
     return this.getSelectableMeasurands(userTimingsUrl, params, groupName);
   }
-  private getHeroTimings(params): Observable<ResponseWithLoadingState<MeasurandGroup>>{
+
+  private getHeroTimings(params): Observable<ResponseWithLoadingState<MeasurandGroup>> {
     const heroTimingsUrl: string = '/resultSelection/getHeroTimings';
-    const groupName: string =  "HERO_TIMINGS";
+    const groupName: string = "HERO_TIMINGS";
     this.setToLoading(this.heroTimings$, groupName);
     return this.getSelectableMeasurands(heroTimingsUrl, params, groupName);
   }
 
-  private getMeasurands(){
-    this.setToLoading( this.loadTimes$, "LOAD_TIMES");
-    this.setToLoading(this.requestCounts$,"REQUEST_COUNTS");
-    this.setToLoading( this.requestSizes$, "REQUEST_SIZES");
-    this.setToLoading( this.percentages$, "PERCENTAGES");
+  private getMeasurands() {
+    this.setToLoading(this.loadTimes$, "LOAD_TIMES");
+    this.setToLoading(this.requestCounts$, "REQUEST_COUNTS");
+    this.setToLoading(this.requestSizes$, "REQUEST_SIZES");
+    this.setToLoading(this.percentages$, "PERCENTAGES");
     const url: string = '/resultSelection/getMeasurands';
     return this.http.get<MeasurandGroup[]>(url).pipe(
       handleError()
     ).subscribe((groups: MeasurandGroup[]) => {
-      groups.forEach((group: MeasurandGroup)=>{
-        let responseWithLoadingState: ResponseWithLoadingState<MeasurandGroup> = {isLoading: false, data: {name: "frontend.de.iteratec.isr.measurand.group."+group.name, values: group.values.map(measurand => ({name: "frontend.de.iteratec.isr.measurand."+measurand.name, id: measurand.id }))}};
+      groups.forEach((group: MeasurandGroup) => {
+        let responseWithLoadingState: ResponseWithLoadingState<MeasurandGroup> = {
+          isLoading: false,
+          data: {
+            name: "frontend.de.iteratec.isr.measurand.group." + group.name,
+            values: group.values.map(measurand => ({
+              name: "frontend.de.iteratec.isr.measurand." + measurand.name,
+              id: measurand.id
+            }))
+          }
+        };
         let concerningSubject$ = this.getDefaultSubjectByMeasurandGroup(group.name);
-        if(concerningSubject$){
+        if (concerningSubject$) {
           concerningSubject$.next(responseWithLoadingState);
         }
       });
     })
   }
 
-  private getDefaultSubjectByMeasurandGroup(name: string): ReplaySubject<ResponseWithLoadingState<MeasurandGroup>>|undefined{
+  private getDefaultSubjectByMeasurandGroup(name: string): ReplaySubject<ResponseWithLoadingState<MeasurandGroup>> | undefined {
     let subject$: ReplaySubject<ResponseWithLoadingState<MeasurandGroup>>;
     switch (name) {
       case "LOAD_TIMES":
@@ -111,14 +123,17 @@ export class ResultSelectionService {
     return subject$;
   }
 
-  private setToLoading(subject$: ReplaySubject<ResponseWithLoadingState<MeasurandGroup>>, groupName: string){
-    subject$.next({isLoading: true, data: {name: groupName, values:[]}});
+  private setToLoading(subject$: ReplaySubject<ResponseWithLoadingState<MeasurandGroup>>, groupName: string) {
+    subject$.next({isLoading: true, data: {name: groupName, values: []}});
   }
 
-  private getSelectableMeasurands(url: string, params: any,groupName: string): Observable<ResponseWithLoadingState<MeasurandGroup>>{
+  private getSelectableMeasurands(url: string, params: any, groupName: string): Observable<ResponseWithLoadingState<MeasurandGroup>> {
     return this.http.get<SelectableMeasurand[]>(url, {params}).pipe(
       handleError(),
-      map(dtos => ({isLoading: false, data: {name: "frontend.de.iteratec.isr.measurand.group."+groupName, values: dtos}})),
+      map(dtos => ({
+        isLoading: false,
+        data: {name: "frontend.de.iteratec.isr.measurand.group." + groupName, values: dtos}
+      })),
     )
   }
 }
