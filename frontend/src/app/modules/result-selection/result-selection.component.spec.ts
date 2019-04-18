e2cab7a48fcfa7e7f181678b4922a7d02b685313
Finn Martens <Finn.Martens@iteratec.de>
2019-04-18 15:55:40 +0200

    [IT-2651] Included the new component so the initial test can run successfully.

--- frontend/src/app/modules/result-selection/result-selection.component.spec.ts
+++ frontend/src/app/modules/result-selection/result-selection.component.spec.ts
@@ -6,6 +6,7 @@ import {SharedMocksModule} from "../../testing/shared-mocks.module";
 import {ResultSelectionService} from "./services/result-selection.service";
 import {OsmLangService} from "../../services/osm-lang.service";
 import {GrailsBridgeService} from "../../services/grails-bridge.service";
+import {ResultSelectionPageLocationConnectivityComponent} from "./components/result-selection-page-location-connectivity/result-selection-page-location-connectivity.component";
 
 describe('ResultSelectionComponent', () => {
   let component: ResultSelectionComponent;
@@ -15,7 +16,8 @@ describe('ResultSelectionComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         ResultSelectionComponent,
-        ResultSelectionTimeFrameComponent
+        ResultSelectionTimeFrameComponent,
+        ResultSelectionPageLocationConnectivityComponent
       ],
       imports: [
         SharedMocksModule
--- frontend/src/app/modules/result-selection/result-selection.module.ts
+++ frontend/src/app/modules/result-selection/result-selection.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { ResultSelectionComponent } from './result-selection.component';
+import {NgModule} from '@angular/core';
+import {ResultSelectionComponent} from './result-selection.component';
 import {ResultSelectionService} from "./services/result-selection.service";
 import {MeasurandSelectComponent} from "./components/measurand-select/measurand-select.component";
 import {SharedModule} from "../shared/shared.module";
-import { ResultSelectionTimeFrameComponent } from './components/result-selection-time-frame/result-selection-time-frame.component';
+import {ResultSelectionTimeFrameComponent} from './components/result-selection-time-frame/result-selection-time-frame.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {
   OWL_DATE_TIME_FORMATS,
   OwlDateTimeModule,
   OwlNativeDateTimeModule
 } from 'ng-pick-datetime';
+import {ResultSelectionPageLocationConnectivityComponent} from './components/result-selection-page-location-connectivity/result-selection-page-location-connectivity.component';
+import {NgSelectModule} from "@ng-select/ng-select";
 
 export const TIME_FORMAT = {
   fullPickerInput: {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'},
@@ -21,13 +23,15 @@ export const TIME_FORMAT = {
     SharedModule,
     ReactiveFormsModule,
     FormsModule,
+    NgSelectModule,
     OwlDateTimeModule,
     OwlNativeDateTimeModule
   ],
   declarations: [
     ResultSelectionComponent,
     ResultSelectionTimeFrameComponent,
-    MeasurandSelectComponent
+    MeasurandSelectComponent,
+    ResultSelectionPageLocationConnectivityComponent
   ],
   exports: [MeasurandSelectComponent],
   providers: [
@@ -46,4 +50,5 @@ export const TIME_FORMAT = {
     ResultSelectionComponent
   ]
 })
-export class ResultSelectionModule { }
+export class ResultSelectionModule {
+}
