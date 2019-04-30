8964bca7123a1bb2a89ea5eb61f3fb38857ef238
Finn Martens <Finn.Martens@iteratec.de>
2019-04-30 14:13:15 +0200

    [IT-2651] Refactored selection of values and added i18n.

--- frontend/src/app/modules/result-selection/components/result-selection-page-location-connectivity/result-selection-page-location-connectivity.component.ts
+++ frontend/src/app/modules/result-selection/components/result-selection-page-location-connectivity/result-selection-page-location-connectivity.component.ts
@@ -20,31 +20,25 @@ export class ResultSelectionPageLocationConnectivityComponent {
   connectivities$: Observable<Connectivity[]>;
 
   pageAndEventSelectionActive: boolean = true;
-  browserAndLocationSelectionActive: boolean;
-  connectivitySelectionActive: boolean;
+  browserAndLocationSelectionActive: boolean = false;
+  connectivitySelectionActive: boolean = false;
 
   uniquePages$: Observable<Page[]>;
   uniqueBrowsers$: Observable<Browser[]>;
   locations$: Observable<Location[]>;
   measuredEvents$: Observable<MeasuredEvent[]>;
 
-  selectedPages: number[];
-  selectedEvents: number[];
-  selectableEvents: number[];
-  allEventsSelected: boolean = true;
-
-  selectedBrowsers: number[];
-  selectedLocations: number[];
-  selectableBrowsers: number[];
-  selectableLocations: number[];
-  allLocationsSelected: boolean = true;
-  allBrowsersSelected: boolean = true;
-
-  selectedConnectivities: number[];
-  selectableConnectivities: number[];
-  allConnectivitiesSelected: boolean = true;
+  selectedPages: number[] = [];
+  selectedEvents: number[] = [];
+  selectedBrowsers: number[] = [];
+  selectedLocations: number[] = [];
+  selectedConnectivities: number[] = [];
 
   @Input() currentChart: string;
+  @Input() showMeasuredStepSelection: boolean = true;
+  @Input() showLocationSelection: boolean = true;
+  @Input() showOnlyPageSelection: boolean = false;
+  @Input() showOnlyBrowserSelection: boolean = false;
 
   constructor(private resultSelectionService: ResultSelectionService) {
     this.eventsAndPages$ = this.resultSelectionService.eventsAndPages$;
@@ -53,7 +47,7 @@ export class ResultSelectionPageLocationConnectivityComponent {
 
     let defaultFrom = new Date();
     let defaultTo = new Date();
-    defaultFrom.setDate(defaultTo.getDate() - 7);
+    defaultFrom.setDate(defaultTo.getDate() - 28);
     const testResultSelectionCommand = {
       from: defaultFrom,
       to: defaultTo,
@@ -70,7 +64,6 @@ export class ResultSelectionPageLocationConnectivityComponent {
     this.eventsAndPages$.subscribe(next => {
       if (next) {
         this.measuredEvents$ = of(this.sortAlphabetically(next));
-        this.selectableEvents = next.map(value => value.id);
         let pages: Page[] = next.map(value => value.parent);
         let uniquePages: Page[] = this.getUniqueElements(pages);
         this.uniquePages$ = of(uniquePages);
@@ -80,106 +73,11 @@ export class ResultSelectionPageLocationConnectivityComponent {
     this.locationsAndBrowsers$.subscribe(next => {
       if (next) {
         this.locations$ = of(this.sortAlphabetically(next));
-        this.selectableLocations = next.map(value => value.id);
         let browsers: Browser[] = next.map(value => value.parent);
         let uniqueBrowsers: Browser[] = this.getUniqueElements(browsers);
-        this.selectableBrowsers = uniqueBrowsers.map(value => value.id);
         this.uniqueBrowsers$ = of(uniqueBrowsers);
       }
     });
-
-    this.connectivities$.subscribe(next => {
-      if (next) {
-        this.selectableConnectivities = next.map(value => value.id);
-      }
-    })
-  }
-
-  switchTab(clickedTab: HTMLLIElement): void {
-    clickedTab.classList.add('active');
-    if (clickedTab.id === 'browserAndLocationTab') {
-      this.pageAndEventSelectionActive = false;
-      this.browserAndLocationSelectionActive = true;
-      this.connectivitySelectionActive = false;
-    } else if (clickedTab.id === 'connectivityTab') {
-      this.pageAndEventSelectionActive = false;
-      this.browserAndLocationSelectionActive = false;
-      this.connectivitySelectionActive = true;
-    } else {
-      this.pageAndEventSelectionActive = true;
-      this.browserAndLocationSelectionActive = false;
-      this.connectivitySelectionActive = false;
-    }
-  }
-
-  selectAllItems(selectAllCheckbox: HTMLInputElement): void {
-    if (selectAllCheckbox.id === 'selectAllEventsCheckbox') {
-      if (this.selectedEvents && this.selectedEvents.length > 0) {
-        this.selectedEvents = [];
-        this.allEventsSelected = true;
-        selectAllCheckbox.checked = true;
-      }
-    } else if (selectAllCheckbox.id === 'selectAllLocationsCheckbox') {
-      if (this.selectedLocations && this.selectedLocations.length > 0) {
-        this.selectedLocations = [];
-        this.allLocationsSelected = true;
-        selectAllCheckbox.checked = true;
-      }
-    } else if (selectAllCheckbox.id === 'selectAllBrowsersCheckbox') {
-      if (this.selectedBrowsers && this.selectedBrowsers.length > 0) {
-        this.selectedBrowsers = [];
-        this.allBrowsersSelected = true;
-        selectAllCheckbox.checked = true;
-        document.querySelector('#result-selection-browser-selection').attributes.getNamedItem('style').value = 'opacity: 0.5;';
-      }
-    } else {
-      if (this.selectedConnectivities && this.selectedConnectivities.length > 0) {
-        this.selectedConnectivities = [];
-        this.allConnectivitiesSelected = true;
-        selectAllCheckbox.checked = true;
-        document.querySelector('#result-selection-connectivity-selection').attributes.getNamedItem('style').value = 'opacity: 0.5;';
-      }
-    }
-  }
-
-  changedSelection(selectAllCheckbox: HTMLInputElement): void {
-    if (selectAllCheckbox.id === 'selectAllEventsCheckbox') {
-      if (this.selectedEvents && this.selectedEvents.length > 0) {
-        this.allEventsSelected = false;
-        selectAllCheckbox.checked = false;
-      } else {
-        this.allEventsSelected = true;
-        selectAllCheckbox.checked = true;
-      }
-    } else if (selectAllCheckbox.id === 'selectAllLocationsCheckbox') {
-      if (this.selectedLocations && this.selectedLocations.length > 0) {
-        this.allLocationsSelected = false;
-        selectAllCheckbox.checked = false;
-      } else {
-        this.allLocationsSelected = true;
-        selectAllCheckbox.checked = true;
-      }
-    } else if (selectAllCheckbox.id === 'selectAllBrowsersCheckbox') {
-      if (this.selectedBrowsers && this.selectedBrowsers.length > 0) {
-        this.allBrowsersSelected = false;
-        selectAllCheckbox.checked = false;
-        document.querySelector('#result-selection-browser-selection').attributes.getNamedItem('style').value = 'opacity: 1;';
-      } else {
-        this.allBrowsersSelected = true;
-        selectAllCheckbox.checked = true;
-        document.querySelector('#result-selection-browser-selection').attributes.getNamedItem('style').value = 'opacity: 0.5;';
-      }
-    } else {
-      if (this.selectedConnectivities && this.selectedConnectivities.length > 0) {
-        this.allConnectivitiesSelected = false;
-        selectAllCheckbox.checked = false;
-        document.querySelector('#result-selection-connectivity-selection').attributes.getNamedItem('style').value = 'opacity: 1;';
-      } else {
-        this.allConnectivitiesSelected = true;
-        selectAllCheckbox.checked = true;
-        document.querySelector('#result-selection-connectivity-selection').attributes.getNamedItem('style').value = 'opacity: 0.5;';
-      }
-    }
   }
 
   private getUniqueElements(items) {
