e4146e1a0bc2d7b48234c915922b66a7e4d6627f
Nils Kuhn <nils.kuhn@iteratec.de>
2019-05-23 16:05:03 +0200

    Installed karma-source-map-support for better stacktraces in tests

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -4,14 +4,15 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular', 'source-map-support'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('karma-mocha-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-source-map-support')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
