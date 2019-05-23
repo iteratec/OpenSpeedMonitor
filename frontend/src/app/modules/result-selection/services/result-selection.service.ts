98919b40ce037c436cf3733a67ddce11318092fd
Johannes Weiß <johannes.weiss@iteratec.de>
2019-05-23 08:50:13 +0200

    Deleted unused methods from result selection service which moved into store

--- frontend/src/app/modules/result-selection/services/result-selection.service.ts
+++ frontend/src/app/modules/result-selection/services/result-selection.service.ts
@@ -4,10 +4,9 @@ import {BehaviorSubject, combineLatest, EMPTY, Observable, OperatorFunction, Rep
 import {MeasurandGroup, SelectableMeasurand} from "../../../models/measurand.model";
 import {ResponseWithLoadingState} from "../../../models/response-with-loading-state.model";
 import {catchError, map, switchMap, startWith} from "rxjs/operators";
-import {Application, SelectableApplication, ApplicationWithPages} from "../../../models/application.model";
+import {Application} from "../../../models/application.model";
 import {Page} from "../../../models/page.model";
 import {Caller, ResultSelectionCommand} from "../models/result-selection-command.model";
-import {Chart} from "../models/chart.model";
 import {Connectivity} from 'src/app/models/connectivity.model';
 import {Location} from 'src/app/models/location.model';
 import {MeasuredEvent} from 'src/app/models/measured-event.model';
@@ -35,12 +34,9 @@ export class ResultSelectionService {
   selectedApplications$: ReplaySubject<Application[]> = new ReplaySubject<Application[]>(1);
   selectedPages$: ReplaySubject<Page[]> = new ReplaySubject<Page[]>(1);
 
-  applications$: ReplaySubject<SelectableApplication[]> = new ReplaySubject(1);
-  applicationsAndPages$: ReplaySubject<ApplicationWithPages[]> = new ReplaySubject(1);
   eventsAndPages$: BehaviorSubject<MeasuredEvent[]> = new BehaviorSubject([]);
   locationsAndBrowsers$: BehaviorSubject<Location[]> = new BehaviorSubject([]);
   connectivities$: ReplaySubject<Connectivity[]> = new ReplaySubject(1);
-  resultCount$: ReplaySubject<string> = new ReplaySubject<string>(1);
 
   constructor(private http: HttpClient) {
     this.getMeasurands();
@@ -124,9 +120,6 @@ export class ResultSelectionService {
     })
   }
 
-
-
-
   updateMeasurands(resultSelectionCommand: ResultSelectionCommand): Observable<MeasurandGroup[]> {
     const params = this.createParamsFromResultSelectionCommand(resultSelectionCommand);
     return this.http.get('/resultSelection/getMeasurands', {params: params}).pipe(
@@ -166,81 +159,6 @@ export class ResultSelectionService {
         data: {name: "frontend.de.iteratec.isr.measurand.group." + groupName, values: dtos}
       })),
     )
-  }
-
-  loadSelectableData(resultSelectionCommand: ResultSelectionCommand, chart: Chart): void {
-    this.loadResultCount(resultSelectionCommand);
-    this.loadUserTimings(resultSelectionCommand);
-    this.loadHeroTimings(resultSelectionCommand);
-
-    if (chart !== Chart.PageComparison) {
-      this.loadSelectableApplications(resultSelectionCommand);
-    } else {
-      this.loadSelectableApplicationsAndPages(resultSelectionCommand);
-    }
-
-    if (chart === Chart.TimeSeries) {
-      this.loadSelectableLocationsAndBrowsers(resultSelectionCommand);
-      this.loadSelectableConnectivities(resultSelectionCommand);
-    }
-
-    if (chart === Chart.TimeSeries || chart === Chart.PageAggregation || chart === Chart.Distribution) {
-      this.loadSelectableEventsAndPages(resultSelectionCommand);
-    }
-  }
-
-  loadSelectableApplications(resultSelectionCommand: ResultSelectionCommand): void {
-    this.fetchResultSelectionData<SelectableApplication[]>(resultSelectionCommand, URL.APPLICATIONS)
-      .subscribe(next => this.applications$.next(next));
-  }
-
-  loadSelectableApplicationsAndPages(resultSelectionCommand: ResultSelectionCommand): void {
-    this.fetchResultSelectionData<ApplicationWithPages[]>(resultSelectionCommand, URL.APPLICATIONS_AND_PAGES)
-      .subscribe(next => this.applicationsAndPages$.next(next));
-  }
-
-  loadSelectableEventsAndPages(resultSelectionCommand: ResultSelectionCommand): void {
-    this.fetchResultSelectionData<MeasuredEvent[]>(resultSelectionCommand, URL.EVENTS_AND_PAGES)
-      .subscribe(next => this.eventsAndPages$.next(next));
-  }
-
-  loadSelectableLocationsAndBrowsers(resultSelectionCommand: ResultSelectionCommand): void {
-    this.fetchResultSelectionData<Location[]>(resultSelectionCommand, URL.LOCATIONS_AND_BROWSERS)
-      .subscribe(next => this.locationsAndBrowsers$.next(next));
-  }
-
-  loadSelectableConnectivities(resultSelectionCommand: ResultSelectionCommand): void {
-    this.fetchResultSelectionData<Connectivity[]>(resultSelectionCommand, URL.CONNECTIVITIES)
-      .subscribe(next => this.connectivities$.next(next));
-  }
-
-  loadResultCount(resultSelectionCommand: ResultSelectionCommand): void {
-    this.fetchResultSelectionData<string>(resultSelectionCommand, URL.RESULT_COUNT)
-      .subscribe(next => this.resultCount$.next(next));
-  }
-
-  loadUserTimings(resultSelectionCommand: ResultSelectionCommand): void {
-    this.fetchResultSelectionData<SelectableMeasurand[]>(resultSelectionCommand, URL.USER_TIMINGS)
-      .subscribe(next => {
-        const groupName: string = "User Timings";
-        let responseWithLoadingState: ResponseWithLoadingState<MeasurandGroup> = {
-          isLoading: false,
-          data: {name: groupName, values: next}
-        };
-        this.userTimings$.next(responseWithLoadingState);
-      });
-  }
-
-  loadHeroTimings(resultSelectionCommand: ResultSelectionCommand): void {
-    this.fetchResultSelectionData<SelectableMeasurand[]>(resultSelectionCommand, URL.HERO_TIMINGS)
-      .subscribe(next => {
-        const groupName: string = "Hero Timings";
-        let responseWithLoadingState: ResponseWithLoadingState<MeasurandGroup> = {
-          isLoading: false,
-          data: {name: groupName, values: next}
-        };
-        this.heroTimings$.next(responseWithLoadingState);
-      });
   }
 
   fetchResultSelectionData<T>(resultSelectionCommand: ResultSelectionCommand, url: URL): Observable<T> {
