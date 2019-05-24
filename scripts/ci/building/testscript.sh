bcec50c3041a8fc7390e937866a2fe5d50f9f34d
Philipp Albrecht <philipp.albrecht@iteratec.de>
2019-05-24 16:38:07 +0200

    deployment key script

---
+++ scripts/ci/building/testscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# openssl aes-256-cbc -K $encrypted_0e6b9eee7eaa_key -iv $encrypted_0e6b9eee7eaa_iv -in travis_deploy_github.enc -out travis_deploy_github -d
+
+git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch --all
+git add build.gradle
+git commit -m "this is a test for the deployment key"
+git push
+git checkout develop
+git merge feature/travisDeployKey
+git push
