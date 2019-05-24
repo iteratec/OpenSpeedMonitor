162012b1abfcfc86509297519ae63a10240aaf4b
Philipp Albrecht <philipp.albrecht@iteratec.de>
2019-05-24 18:19:39 +0200

    deployment key script

--- scripts/ci/building/testscript.sh
+++ scripts/ci/building/testscript.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# openssl aes-256-cbc -K $encrypted_0e6b9eee7eaa_key -iv $encrypted_0e6b9eee7eaa_iv -in travis_deploy_github.enc -out travis_deploy_github -d
-
-git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch --all
 git checkout develop
-git remote -v
 git status
 git merge feature/travisDeployKey
+git remote set-url origin git@github.com:iteratec/OpenSpeedMonitor.git
+git remote -v
 git commit -m "merge feature branch"
 git push
