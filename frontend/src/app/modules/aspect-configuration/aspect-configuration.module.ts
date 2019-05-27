6a3a26d6d3cfce4d0f3fe6a9ae063e0fc6bb81ea
Nils Kuhn <nils.kuhn@iteratec.de>
2019-05-27 16:53:36 +0200

    Added route for new component

--- frontend/src/app/modules/aspect-configuration/aspect-configuration.module.ts
+++ frontend/src/app/modules/aspect-configuration/aspect-configuration.module.ts
@@ -4,18 +4,22 @@ import {AspectConfigurationComponent} from './aspect-configuration.component';
 import {RouterModule, Routes} from "@angular/router";
 import {SharedModule} from "../shared/shared.module";
 import {AspectMetricsComponent} from './components/aspect-metrics/aspect-metrics.component';
+import {MetricFinderModule} from "../metric-finder/metric-finder.module";
+import {EditAspectMetricsComponent} from './components/edit-aspect-metrics/edit-aspect-metrics.component';
 
 const routes: Routes = [
   {path: '', component: AspectConfigurationComponent},
-  {path: ':applicationId/:pageId', component: AspectConfigurationComponent}
+  {path: ':applicationId/:pageId', component: AspectConfigurationComponent},
+  {path: 'edit/:applicationId/:pageId/:browserId/:aspectType', component: EditAspectMetricsComponent}
 ];
 
 @NgModule({
-  declarations: [AspectConfigurationComponent, AspectMetricsComponent],
+  declarations: [AspectConfigurationComponent, AspectMetricsComponent, EditAspectMetricsComponent],
   imports: [
     RouterModule.forChild(routes),
     CommonModule,
-    SharedModule
+    SharedModule,
+    MetricFinderModule
   ],
   exports: [
     RouterModule
--- frontend/src/app/modules/aspect-configuration/components/aspect-metrics/aspect-metrics.component.ts
+++ frontend/src/app/modules/aspect-configuration/components/aspect-metrics/aspect-metrics.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ExtendedPerformanceAspect, PerformanceAspectType} from "../../../../models/perfomance-aspect.model";
 import {Observable, of} from "rxjs";
+import {ApplicationService} from "../../../../services/application.service";
+import {Application} from "../../../../models/application.model";
+import {Page} from "../../../../models/page.model";
 
 @Component({
   selector: 'osm-aspect-metrics',
@@ -11,8 +14,12 @@ export class AspectMetricsComponent implements OnInit {
   @Input() aspects: ExtendedPerformanceAspect[];
   @Input() aspectType: PerformanceAspectType;
   aspectsToShow$: Observable<ExtendedPerformanceAspect[]>;
+  application$: Observable<Application>;
+  page$: Observable<Page>;
 
-  constructor() {
+  constructor(private applicationService: ApplicationService) {
+    this.application$ = applicationService.selectedApplication$;
+    this.page$ = applicationService.selectedPage$;
   }
 
   ngOnInit() {
