2b8046d96d9e666e0fc3d77e1faed3baf5f5a6a3
jiayiw5 <jenny961105@gmail.com>
2019-07-02 09:44:03 +0200

    [IT-2742]added first layout for aggregationChartDataService

--- frontend/src/app/modules/aggregation/components/aggregation-chart/aggregation-chart.component.ts
+++ frontend/src/app/modules/aggregation/components/aggregation-chart/aggregation-chart.component.ts
@@ -48,18 +48,87 @@ export class AggregationChartComponent implements OnInit {
     ]
   };
 
+  data = {
+    aggregationValue: "avg",
+    fitlerRules:{
+      "Otto.de Lang": [
+        {
+          jobGroup: "Job Group",
+          page: "finn"
+
+        }]
+    },
+    hasComparativeData: false,
+    i18nMap: {
+      measurand: "Measurand",
+      jobGroup: "Job Group",
+      page: "Page",
+      comparativeImprovement: "Improvement",
+      comparativeDeterioration: "Deterioration"
+    },
+    selectedFilter: "desc",
+    series:[
+      {
+        aggregationValue: "avg",
+        browser: null,
+        deviceType: null,
+        jobGroup: "LokalTest_pal",
+        measurand: "DOC_COMPLETE_TIME",
+        measurandGroup: "LOAD_TIMES",
+        measurandLabel: "Document Complete",
+        operatingSystem: null,
+        page: "finn",
+        unit: "ms",
+        value: 1314.2,
+        valueComparative: null
+      },
+      {
+        aggregationValue: "avg",
+        browser: null,
+        deviceType: null,
+        jobGroup: "Otto.de",
+        measurand: "IMAGE_TOTAL_BYTES",
+        measurandGroup: "LOAD_TIMES",
+        measurandLabel: "Document Complete",
+        operatingSystem: null,
+        page: "ADS",
+        unit: "ms",
+        value: 1336.352,
+        valueComparative: null
+      },
+      {
+        aggregationValue: "avg",
+        browser: null,
+        deviceType: null,
+        jobGroup: "Otto.de",
+        measurand: "IMAGE_TOTAL_BYTES",
+        measurandGroup: "LOAD_TIMES",
+        measurandLabel: "Document Complete",
+        operatingSystem: null,
+        page: "ADS",
+        unit: "ms",
+        value: 1336.352,
+        valueComparative: null
+      }
+    ],
+    stackBars: true,
+    width: -1
+  };
+
   constructor(private grailsBridgeService: GrailsBridgeService, private aggregationChartDataService: AggregationChartDataService) {
     window.addEventListener("aggregationChartLoaded", () => {
-      this.grailsBridgeService.globalOsmNamespace.ChartModules.AggregationData.setData = (svg) => {
-        console.log(svg);
-        console.log("Angular Service")
-      };
-      this.grailsBridgeService.globalOsmNamespace.ChartModules.Aggregation('#aggregation-chart').setData("Real Data");
+      //this.grailsBridgeService.globalOsmNamespace.ChartModules.AggregationData.setData = (data) => {
+      //  this.aggregationChartDataService.setData(data);
+      //};
+      this.grailsBridgeService.globalOsmNamespace.ChartModules.AggregationData = this.aggregationChartDataService;
+      this.grailsBridgeService.globalOsmNamespace.ChartModules.Aggregation('#aggregation-chart').setData(this.data);
+      //this.grailsBridgeService.globalOsmNamespace.ChartModules.GuiHandling.aggregation().init();
     });
   }
 
   ngOnInit(): void {
     const assetPathAggregationChart: string = this.grailsBridgeService.globalOsmNamespace.assetPaths.aggregationChart;
+    this.grailsBridgeService.globalOsmNamespace.postLoader.loadJavascript(assetPathAggregationChart, 'aggregationGuiHandling');
     this.grailsBridgeService.globalOsmNamespace.postLoader.loadJavascript(assetPathAggregationChart, 'aggregationChart');
   }
 }
