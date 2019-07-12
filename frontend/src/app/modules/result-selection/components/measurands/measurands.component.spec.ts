5730cda6b70faf65ed3091d47d2e3a3c237eaa61
DanielSteger <daniel.steger@iteratec.de>
2019-07-12 10:00:09 +0200

    [IT-2743] Add frontend tests for measurand component.

--- frontend/src/app/modules/result-selection/components/measurands/measurands.component.spec.ts
+++ frontend/src/app/modules/result-selection/components/measurands/measurands.component.spec.ts
@@ -5,10 +5,13 @@ import {MeasurandSelectComponent} from "./measurand-select/measurand-select.comp
 import {SharedMocksModule} from "../../../../testing/shared-mocks.module";
 import {ResultSelectionStore} from "../../services/result-selection.store";
 import {ResultSelectionService} from "../../services/result-selection.service";
+import {By} from "@angular/platform-browser";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('MeasurandsComponent', () => {
   let component: MeasurandsComponent;
   let fixture: ComponentFixture<MeasurandsComponent>;
+  let resultSelectionStore: ResultSelectionStore;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,7 +19,7 @@ describe('MeasurandsComponent', () => {
       declarations: [ MeasurandsComponent, MeasurandSelectComponent ],
       providers: [
         ResultSelectionStore,
-      ResultSelectionService
+        ResultSelectionService
       ]
     })
     .compileComponents();
@@ -24,11 +27,63 @@ describe('MeasurandsComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MeasurandsComponent);
+    resultSelectionStore = TestBed.get(ResultSelectionStore);
     component = fixture.componentInstance;
+    component.multipleMeasurands = true;
+
+    resultSelectionStore.loadTimes$.next({
+      isLoading: false,
+      name: 'frontend.de.iteratec.isr.measurand.group.LOAD_TIMES',
+      values: [
+        {
+          id: 'DOC_COMPLETE_TIME',
+          name: 'frontend.de.iteratec.isr.measurand.DOC_COMPLETE_TIME'
+        },
+        {
+          id: 'DOM_TIME',
+          name: 'frontend.de.iteratec.isr.measurand.DOM_TIME'
+        },
+        {
+          id: 'FIRST_BYTE',
+          name: 'frontend.de.iteratec.isr.measurand.FIRST_BYTE'
+        }
+      ]
+    });
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should select multiple measurands if measurands are added', () => {
+    expect(component.selectedMeasurands.length).toBe(1);
+    expect(fixture.debugElement.queryAll(By.directive(MeasurandSelectComponent)).length).toBe(1);
+    const addMeasurand: HTMLButtonElement = fixture.nativeElement.querySelector('#add-measurand-button');
+    addMeasurand.click();
+    fixture.detectChanges();
+    expect(component.selectedMeasurands.length).toBe(2);
+    expect(fixture.debugElement.queryAll(By.directive(MeasurandSelectComponent)).length).toBe(2);
+    addMeasurand.click();
+    fixture.detectChanges();
+    expect(component.selectedMeasurands.length).toBe(3);
+    expect(fixture.debugElement.queryAll(By.directive(MeasurandSelectComponent)).length).toBe(3);
+  });
+
+  it('should deselect measurands if removed until only one measurand is available', () => {
+    expect(component.selectedMeasurands.length).toBe(1);
+    expect(fixture.debugElement.queryAll(By.directive(MeasurandSelectComponent)).length).toBe(1);
+    const addMeasurand: HTMLButtonElement = fixture.nativeElement.querySelector('#add-measurand-button');
+    addMeasurand.click();
+    fixture.detectChanges();
+    expect(component.selectedMeasurands.length).toBe(2);
+    expect(fixture.debugElement.queryAll(By.directive(MeasurandSelectComponent)).length).toBe(2);
+
+    const removeMeasurand: HTMLButtonElement = fixture.nativeElement.querySelector('#remove-measurand-button');
+    removeMeasurand.click();
+    fixture.detectChanges();
+    expect(component.selectedMeasurands.length).toBe(1);
+    expect(fixture.debugElement.queryAll(By.directive(MeasurandSelectComponent)).length).toBe(1);
+    expect(fixture.nativeElement.querySelector('#remove-measurand-button')).toBeFalsy();
   });
 });
