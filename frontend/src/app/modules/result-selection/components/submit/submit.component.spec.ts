a3ccd593784323e55d0c2a5fa99458e1d5ec8d70
DanielSteger <daniel.steger@iteratec.de>
2019-07-12 15:01:31 +0200

    [IT-2743] Add frontend tests for submit component.

--- frontend/src/app/modules/result-selection/components/submit/submit.component.spec.ts
+++ frontend/src/app/modules/result-selection/components/submit/submit.component.spec.ts
@@ -5,10 +5,12 @@ import {BarchartDataService} from "../../../chart/services/barchart-data.service
 import {ResultSelectionStore} from "../../services/result-selection.store";
 import {SharedMocksModule} from "../../../../testing/shared-mocks.module";
 import {ResultSelectionService} from "../../services/result-selection.service";
+import {By} from "@angular/platform-browser";
 
 describe('SubmitComponent', () => {
   let component: SubmitComponent;
   let fixture: ComponentFixture<SubmitComponent>;
+  let resultSelectionStore: ResultSelectionStore;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,11 +27,184 @@ describe('SubmitComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SubmitComponent);
+    resultSelectionStore = TestBed.get(ResultSelectionStore);
     component = fixture.componentInstance;
+    component.applicationsRequired = true;
+    component.pagesRequired = true;
+    component.measurandsRequired = true;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should enable submit button if application, page and measurand are selected', () => {
+    resultSelectionStore.resultCount$.next(100);
+    resultSelectionStore._resultSelectionCommand$.next({
+      from: new Date(),
+      to: new Date(),
+      jobGroupIds: [1, 2, 3],
+      pageIds: [4, 7, 9]
+    });
+    resultSelectionStore._remainingGetBarchartCommand$.next({
+      measurands: ["DOC_COMPLETE_TIME"]
+    });
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should enable submit button if nothing is required', () => {
+    component.applicationsRequired = false;
+    component.pagesRequired = false;
+    component.measurandsRequired = false;
+    resultSelectionStore.resultCount$.next(100);
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should warn of unavailable barchart data', () => {
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should warn of unavailable barchart data, even if application, page and measurand are selected', () => {
+    resultSelectionStore._resultSelectionCommand$.next({
+      from: new Date(),
+      to: new Date(),
+      jobGroupIds: [1, 2, 3],
+      pageIds: [4, 7, 9]
+    });
+    resultSelectionStore._remainingGetBarchartCommand$.next({
+      measurands: ["DOC_COMPLETE_TIME"]
+    });
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should warn that no application, page and measurand is selected', () => {
+    resultSelectionStore.resultCount$.next(100);
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should warn that no application is selected', () => {
+    component.pagesRequired = false;
+    component.measurandsRequired = false;
+    resultSelectionStore.resultCount$.next(100);
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should warn that no page is selected', () => {
+    component.applicationsRequired = false;
+    component.measurandsRequired = false;
+    resultSelectionStore.resultCount$.next(100);
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should warn that no measurand is selected.', () => {
+    component.applicationsRequired = false;
+    component.pagesRequired = false;
+    resultSelectionStore.resultCount$.next(100);
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should not warn about the selected application', () => {
+    resultSelectionStore.resultCount$.next(100);
+    component.applicationsSelected$.next(true);
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should not warn about the selected application', () => {
+    resultSelectionStore.resultCount$.next(100);
+    resultSelectionStore._resultSelectionCommand$.next({
+      from: new Date(),
+      to: new Date(),
+      jobGroupIds: [1, 2, 3]
+    });
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should not warn about the selected page', () => {
+    resultSelectionStore.resultCount$.next(100);
+    resultSelectionStore._resultSelectionCommand$.next({
+      from: new Date(),
+      to: new Date(),
+      pageIds: [3, 5, 6]
+    });
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
+  });
+
+  it('should not warn about the selected meausrand', () => {
+    resultSelectionStore.resultCount$.next(100);
+    resultSelectionStore._remainingGetBarchartCommand$.next({
+      measurands: ["DOC_COMPLETE_TIME"]
+    });
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#bet-barchart-data-submit')).nativeElement.disabled).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#unavailable-data'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#no-application-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-page-selected'))).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#no-measurand-selected'))).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('#long-processing-time'))).toBeFalsy();
   });
 });
