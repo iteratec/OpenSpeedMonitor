9f028ca842e6fc6a1e407f6fe2e5517c77b65880
DanielSteger <daniel.steger@iteratec.de>
2019-09-13 11:23:20 +0200

    [IT-2810] Add a loading state to performance aspect types

--- frontend/src/app/services/performance-aspect.service.ts
+++ frontend/src/app/services/performance-aspect.service.ts
@@ -3,19 +3,21 @@ import {BehaviorSubject, EMPTY} from "rxjs";
 import {PerformanceAspectType} from "../models/perfomance-aspect.model";
 import {HttpClient} from "@angular/common/http";
 import {catchError} from "rxjs/operators";
+import {ResponseWithLoadingState} from "../models/response-with-loading-state.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerformanceAspectService {
 
-  aspectTypes$ = new BehaviorSubject<PerformanceAspectType[]>([]);
+  aspectTypes$ = new BehaviorSubject<ResponseWithLoadingState<PerformanceAspectType[]>>({isLoading: false, data: []});
 
   constructor(private http: HttpClient) {
     this.loadAspectTypes();
   }
 
   loadAspectTypes() {
+    this.aspectTypes$.next({...this.aspectTypes$.getValue(), isLoading: true});
     this.http.get<PerformanceAspectType[]>("/aspectConfiguration/rest/getAspectTypes").pipe(
       catchError((error) => {
         console.error(error);
@@ -24,7 +26,7 @@ export class PerformanceAspectService {
         types.map((type) => {
           type.kind = "performance-aspect-type"
         });
-        this.aspectTypes$.next(types)
+        this.aspectTypes$.next({isLoading: false, data: types})
     });
   }
 }
