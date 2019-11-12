e537a136817013b47def40dcac96623c4eb2d1c3
DanielSteger <daniel.steger@iteratec.de>
2019-11-12 15:34:34 +0100

    Show spinner when waiting for time series chart.

--- frontend/src/app/modules/time-series/time-series.component.ts
+++ frontend/src/app/modules/time-series/time-series.component.ts
@@ -4,6 +4,7 @@ import {LinechartDataService} from "./services/linechart-data.service";
 import {ResultSelectionStore} from "../result-selection/services/result-selection.store";
 import {EventResultData, EventResultDataDTO} from './models/event-result-data.model';
 import {BehaviorSubject} from 'rxjs';
+import {SpinnerService} from "../shared/services/spinner.service";
 
 @Component({
   selector: 'osm-time-series',
@@ -13,17 +14,27 @@ import {BehaviorSubject} from 'rxjs';
 export class TimeSeriesComponent implements OnInit {
 
   public results$ = new BehaviorSubject<EventResultDataDTO>(new EventResultData());
+  showChart: boolean = false;
 
-  constructor(private linechartDataService: LinechartDataService, private resultSelectionStore: ResultSelectionStore) { }
+  constructor(private linechartDataService: LinechartDataService, private resultSelectionStore: ResultSelectionStore, private spinnerService: SpinnerService) { }
 
   ngOnInit() {
+    this.showChart = false;
+    if (this.resultSelectionStore.validQuery) {
+      this.getTimeSeriesChartData();
+    }
   }
 
   getTimeSeriesChartData() {
+    this.showChart = true;
+    this.spinnerService.showSpinner('time-series-chart-spinner');
     this.linechartDataService.fetchEventResultData<EventResultDataDTO>(
       this.resultSelectionStore.resultSelectionCommand,
       this.resultSelectionStore.remainingResultSelection,
       URL.EVENT_RESULT_DASHBOARD_LINECHART_DATA
-    ).subscribe(next => this.results$.next(next));
+    ).subscribe(next => {
+      this.spinnerService.hideSpinner('time-series-chart-spinner');
+      this.results$.next(next)
+    });
   }
 }
