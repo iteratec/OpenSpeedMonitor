8c86c2f3e285cc1edd1398d5562b2de2808138cf
DanielSteger <daniel.steger@iteratec.de>
2019-11-19 16:58:00 +0100

    Fix time series test

--- frontend/src/app/modules/time-series/time-series.component.spec.ts
+++ frontend/src/app/modules/time-series/time-series.component.spec.ts
@@ -6,6 +6,7 @@ import {ResultSelectionModule} from "../result-selection/result-selection.module
 import {OsmLangService} from "../../services/osm-lang.service";
 import {GrailsBridgeService} from "../../services/grails-bridge.service";
 import {TimeSeriesLineChartComponent} from "./components/time-series-line-chart/time-series-line-chart.component";
+import {SpinnerService} from "../shared/services/spinner.service";
 
 describe('TimeSeriesComponent', () => {
   let component: TimeSeriesComponent;
@@ -23,7 +24,8 @@ describe('TimeSeriesComponent', () => {
       ],
       providers: [
         OsmLangService,
-        GrailsBridgeService
+        GrailsBridgeService,
+        SpinnerService
       ]
     })
     .compileComponents();
