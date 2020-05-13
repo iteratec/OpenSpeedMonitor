b28ee8d2e04e1f37fb71c76a6e63c10d040d9dd6
Daniel Steger <daniel.steger@iteratec.com>
2020-05-13 14:43:16 +0200

    Refactored

--- frontend/src/app/modules/shared/services/measurand-color.service.ts
+++ frontend/src/app/modules/shared/services/measurand-color.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import * as d3 from "d3";
-import {scaleOrdinal} from "d3";
+import {Injectable} from '@angular/core';
+import * as d3 from 'd3';
+import {scaleOrdinal} from 'd3';
 
 @Injectable({
   providedIn: 'root'
@@ -11,39 +11,43 @@ export class MeasurandColorService {
 
   private static initColors() {
     const loadingTimeColors = [
-        "#1660A7",
-        "#558BBF",
-        "#95b6d7",
-        "#d4e2ef"
+        '#1660A7',
+        '#558BBF',
+        '#95b6d7',
+        '#d4e2ef'
       ],
       countOfRequestColors = [
-        "#E41A1C",
-        "#eb5859",
-        "#f29697",
-        "#fad5d5"
+        '#E41A1C',
+        '#eb5859',
+        '#f29697',
+        '#fad5d5'
       ],
       sizeOfRequestColors = [
-        "#F18F01",
-        "#f4ad46",
-        "#f8cc8b",
-        "#fcead0"
+        '#F18F01',
+        '#f4ad46',
+        '#f8cc8b',
+        '#fcead0'
       ],
       csiColors = [
-        "#59B87A",
-        "#86cb9e",
-        "#b3dec2",
-        "#e0f2e6"
+        '#59B87A',
+        '#86cb9e',
+        '#b3dec2',
+        '#e0f2e6'
       ];
 
     return {
-      "ms": loadingTimeColors,
-      "s": loadingTimeColors,
-      "#": countOfRequestColors,
-      "KB": sizeOfRequestColors,
-      "MB": sizeOfRequestColors,
-      "%": csiColors,
-      "": loadingTimeColors
-    }
+      'ms': loadingTimeColors,
+      's': loadingTimeColors,
+      '#': countOfRequestColors,
+      'KB': sizeOfRequestColors,
+      'MB': sizeOfRequestColors,
+      '%': csiColors,
+      '': loadingTimeColors
+    };
+  }
+
+  private static createDomain(arrayLength: number): string[] {
+    return Array.from({length: arrayLength}, (_, index) => index.toString());
   }
 
   getColorScaleForMeasurandGroup(measurandUnit: string, skipFirst: boolean = false): d3.ScaleOrdinal<string, any> {
@@ -51,19 +55,15 @@ export class MeasurandColorService {
     return d3.scaleOrdinal(colors).domain(MeasurandColorService.createDomain(colors.length));
   }
 
-  private static createDomain(arrayLength: number): string[] {
-    return Array.from({length: arrayLength}, (_, index) => index.toString());
-  }
-
   getColorScaleForTrafficLight() {
     const trafficColors = [
-      "#5cb85c",
-      "#f0ad4e",
-      "#d9534f"
+      '#5cb85c',
+      '#f0ad4e',
+      '#d9534f'
     ];
 
     return scaleOrdinal()
-      .domain(["good", "ok", "bad"])
+      .domain(['good', 'ok', 'bad'])
       .range(trafficColors);
   }
 }
