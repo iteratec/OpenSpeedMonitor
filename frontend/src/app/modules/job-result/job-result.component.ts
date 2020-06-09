f980f710ce3e97755a3bc83582f32a671e9fc0fb
Daniel Steger <daniel.steger@iteratec.com>
2020-06-09 17:13:45 +0200

    Added sorting function for table columns

--- frontend/src/app/modules/job-result/job-result.component.ts
+++ frontend/src/app/modules/job-result/job-result.component.ts
@@ -10,6 +10,7 @@ import {JobResult} from './models/job-result.model';
 export class JobResultComponent implements OnInit {
 
   jobResults: JobResult[] = [];
+  currentSortingRule: { [key: string]: string } = {column: 'date', direction: 'desc'};
 
   constructor(private dataService: JobResultDataService) {
   }
@@ -27,7 +28,44 @@ export class JobResultComponent implements OnInit {
     return test.jobResultStatus === 'Finished' && test.wptStatus === 'COMPLETED (200)' && test.description === 'Ok';
   }
 
-  sort(event: any) {
+  sort(column: string) {
+    if (column === this.currentSortingRule.column) {
+      if (this.currentSortingRule.direction === 'desc') {
+        this.currentSortingRule.direction = 'asc';
+      } else if (this.currentSortingRule.direction === 'asc') {
+        this.currentSortingRule.direction = 'desc';
+      }
+    } else {
+      this.currentSortingRule.column = column;
+      this.currentSortingRule.direction = 'asc';
+    }
+
+    this.jobResults.sort((a: JobResult, b: JobResult) =>
+      this.compareJobResults(a, b, this.currentSortingRule.column, this.currentSortingRule.direction)
+    );
   }
 
+  private compareJobResults(a: JobResult, b: JobResult, column: string, direction: string): number {
+    let directionFactor = 1;
+    if (direction === 'desc') {
+      directionFactor = -1;
+    }
+
+    if (a[column] && b[column]) {
+      if (a[column] < b[column]) {
+        return -directionFactor;
+      }
+      if (a[column] > b[column]) {
+        return directionFactor;
+      }
+    } else {
+      if (a[column]) {
+        return directionFactor;
+      }
+      if (b[column]) {
+        return -directionFactor;
+      }
+    }
+    return 0;
+  }
 }
